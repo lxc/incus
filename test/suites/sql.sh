7d2bc2d77692c9e0a7b14fb4791041c3da0778fd
Stéphane Graber <stgraber@stgraber.org>
2023-08-01 23:43:49 -0400

    tests: Port sql test
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- test/suites/sql.sh
+++ test/suites/sql.sh
@@ -1,41 +1,41 @@
-# Test the lxd sql command.
+# Test the incus sql command.
 test_sql() {
   # Invalid arguments
-  ! lxd sql foo "SELECT * FROM CONFIG" || false
-  ! lxd sql global "" || false
+  ! incus sql foo "SELECT * FROM CONFIG" || false
+  ! incus sql global "" || false
 
   # Local database query
-  lxd sql local "SELECT * FROM config" | grep -qF "core.https_address"
+  incus sql local "SELECT * FROM config" | grep -qF "core.https_address"
 
   # Global database query
-  lxd sql global "SELECT * FROM config" | grep -qF "core.trust_password"
+  incus sql global "SELECT * FROM config" | grep -qF "core.trust_password"
 
   # Global database insert
-  lxd sql global "INSERT INTO config(key,value) VALUES('core.https_allowed_credentials','true')" | grep -qxF "Rows affected: 1"
-  lxd sql global "DELETE FROM config WHERE key='core.https_allowed_credentials'" | grep -qxF "Rows affected: 1"
+  incus sql global "INSERT INTO config(key,value) VALUES('core.https_allowed_credentials','true')" | grep -qxF "Rows affected: 1"
+  incus sql global "DELETE FROM config WHERE key='core.https_allowed_credentials'" | grep -qxF "Rows affected: 1"
 
   # Standard input
-  echo "SELECT * FROM config" | lxd sql global - | grep -qF "core.trust_password"
+  echo "SELECT * FROM config" | incus sql global - | grep -qF "core.trust_password"
 
   # Multiple queries
-  lxd sql global "SELECT * FROM config; SELECT * FROM instances" | grep -qxF "=> Query 0:"
+  incus sql global "SELECT * FROM config; SELECT * FROM instances" | grep -qxF "=> Query 0:"
 
   # Local database dump
   SQLITE_DUMP="${TEST_DIR}/dump.db"
-  lxd sql local .dump | sqlite3 "${SQLITE_DUMP}"
+  incus sql local .dump | sqlite3 "${SQLITE_DUMP}"
   sqlite3 "${SQLITE_DUMP}" "SELECT * FROM patches" | grep -qF "|dnsmasq_entries_include_device_name|"
   rm -f "${SQLITE_DUMP}"
 
   # Local database schema dump
   SQLITE_DUMP="${TEST_DIR}/dump.db"
-  lxd sql local .schema | sqlite3 "${SQLITE_DUMP}"
+  incus sql local .schema | sqlite3 "${SQLITE_DUMP}"
   [ "$(sqlite3 "${SQLITE_DUMP}" 'SELECT * FROM schema' | wc -l)" = "0" ]
   [ "$(sqlite3 "${SQLITE_DUMP}" 'SELECT * FROM patches' | wc -l)" = "0" ]
   rm -f "${SQLITE_DUMP}"
 
   # Global database dump
   SQLITE_DUMP="${TEST_DIR}/dump.db"
-  GLOBAL_DUMP="$(lxd sql global .dump)"
+  GLOBAL_DUMP="$(incus sql global .dump)"
   echo "$GLOBAL_DUMP" | grep -F "CREATE TRIGGER" # ensure triggers are captured.
   echo "$GLOBAL_DUMP" | grep -F "CREATE INDEX"   # ensure indices are captured.
   echo "$GLOBAL_DUMP" | grep -F "CREATE VIEW"    # ensure views are captured.
@@ -45,7 +45,7 @@ test_sql() {
 
   # Global database schema dump
   SQLITE_DUMP="${TEST_DIR}/dump.db"
-  lxd sql global .schema | sqlite3 "${SQLITE_DUMP}"
+  incus sql global .schema | sqlite3 "${SQLITE_DUMP}"
   [ "$(sqlite3 "${SQLITE_DUMP}" 'SELECT * FROM schema' | wc -l)" = "0" ]
   [ "$(sqlite3 "${SQLITE_DUMP}" 'SELECT * FROM profiles' | wc -l)" = "0" ]
   rm -f "${SQLITE_DUMP}"
@@ -53,7 +53,7 @@ test_sql() {
   # Sync the global database to disk
   SQLITE_SYNC="${INCUS_DIR}/database/global/db.bin"
   echo "SYNC ${SQLITE_SYNC}"
-  lxd sql global .sync
+  incus sql global .sync
   sqlite3 "${SQLITE_SYNC}" "SELECT * FROM schema" | grep -q "^1|"
   sqlite3 "${SQLITE_SYNC}" "SELECT * FROM profiles" | grep -qF "|Default LXD profile|"
 }
