bee747218909af15448ba81b7af62ac5f19352b1
Simon Deziel <simon.deziel@canonical.com>
2023-08-07 16:59:58 -0400

    [lxd-import] lxd/endpoints/network: s/sss/ss/
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- internal/server/endpoints/network.go
+++ internal/server/endpoints/network.go
@@ -41,7 +41,7 @@ func (e *Endpoints) NetworkCert() *localtls.CertInfo {
 	return e.cert
 }
 
-// NetworkAddress returns the network addresss of the network endpoint, or an
+// NetworkAddress returns the network address of the network endpoint, or an
 // empty string if there's no network endpoint.
 func (e *Endpoints) NetworkAddress() string {
 	e.mu.RLock()
