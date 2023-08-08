5b75a13fd730e8bb20f72da949a9c2708a049b4c
Ruth Fuchss <ruth.fuchss@canonical.com>
2023-08-08 14:50:24 +0200

    [lxd-import] doc/css: add an orange color to the doc styling
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- doc/conf.py
+++ doc/conf.py
@@ -106,7 +106,8 @@ html_theme_options = {
         "color-highlighted-background": "#EbEbEb",
         "color-link-underline": "var(--color-background-primary)",
         "color-link-underline--hover": "var(--color-background-primary)",
-        "color-version-popup": "#772953"
+        "color-version-popup": "#772953",
+        "color-orange": "#FBDDD2",
     },
     "dark_css_variables": {
         "color-foreground-secondary": "var(--color-foreground-primary)",
@@ -130,7 +131,8 @@ html_theme_options = {
         "color-highlighted-background": "#666",
         "color-link-underline": "var(--color-background-primary)",
         "color-link-underline--hover": "var(--color-background-primary)",
-        "color-version-popup": "#F29879"
+        "color-version-popup": "#F29879",
+        "color-orange": "#E95420",
     },
 }
 
