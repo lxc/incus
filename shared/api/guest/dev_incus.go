0129b229dba144a9ee1b31dead156f31adc37b98
Stéphane Graber <stgraber@stgraber.org>
2023-08-16 13:27:02 -0400

    shared/api: Split and rename /dev/incus API
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/api/guest/dev_incus.go
+++ shared/api/guest/dev_incus.go
@@ -1,15 +1,15 @@
 package api
 
-// DevLXDPut represents the modifiable data.
-type DevLXDPut struct {
+// DevIncusPut represents the modifiable data.
+type DevIncusPut struct {
 	// Instance state
 	// Example: Started
 	State string `json:"state" yaml:"state"`
 }
 
-// DevLXDGet represents the server data which is returned as the root of the devlxd API.
-type DevLXDGet struct {
-	DevLXDPut
+// DevIncusGet represents the server data which is returned as the root of the /dev/incus API.
+type DevIncusGet struct {
+	DevIncusPut
 
 	// API version number
 	// Example: 1.0
@@ -20,6 +20,6 @@ type DevLXDGet struct {
 	InstanceType string `json:"instance_type" yaml:"instance_type"`
 
 	// What cluster member this instance is located on
-	// Example: lxd01
+	// Example: incus01
 	Location string `json:"location" yaml:"location"`
 }
