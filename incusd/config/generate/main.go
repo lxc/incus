3715dfe2d0f756ae643a5426fe5868fafd28aa7a
Stéphane Graber <stgraber@stgraber.org>
2023-08-28 23:21:27 -0400

    incusd/config/generate: Remove LXD mentions
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incusd/config/generate/main.go
+++ incusd/config/generate/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var logger *log.Logger
-var logFilePath string = "/tmp/lxddoc.log"
+var logFilePath string = "/tmp/incusdoc.log"
 
 func init() {
 	file, err := os.Create(logFilePath)
@@ -17,16 +17,16 @@ func init() {
 		log.Fatal(err)
 	}
 
-	logger = log.New(file, "LXDDOC: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logger = log.New(file, "INCUSDOC: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 var exclude []string
 var yamlOutput string
 var txtOutput string
 var rootCmd = &cobra.Command{
-	Use:   "lxd-doc",
-	Short: "lxd-doc - a simple tool to generate documentation for LXD",
-	Long:  "lxd-doc - a simple tool to generate documentation for LXD. It outputs a YAML and a Markdown file that contain the content of all `lxddoc:generate` statements in the project.",
+	Use:   "incus-doc",
+	Short: "incus-doc - a simple tool to generate documentation for Incus",
+	Long:  "incus-doc - a simple tool to generate documentation for Incus. It outputs a YAML and a Markdown file that contain the content of all `gendoc:generate` statements in the project.",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
@@ -48,13 +48,13 @@ var rootCmd = &cobra.Command{
 
 func main() {
 	rootCmd.Flags().StringSliceVarP(&exclude, "exclude", "e", []string{}, "Path to exclude from the process")
-	rootCmd.Flags().StringVarP(&yamlOutput, "yaml", "y", "lxd-doc.yaml", "Output YAML file containing the generated documentation")
-	rootCmd.Flags().StringVarP(&txtOutput, "txt", "t", "lxd-doc.txt", "Output TXT file containing the generated documentation")
+	rootCmd.Flags().StringVarP(&yamlOutput, "yaml", "y", "incus-doc.yaml", "Output YAML file containing the generated documentation")
+	rootCmd.Flags().StringVarP(&txtOutput, "txt", "t", "incus-doc.txt", "Output TXT file containing the generated documentation")
 	err := rootCmd.Execute()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "lxd-doc failed: %v", err)
+		fmt.Fprintf(os.Stderr, "incus-doc failed: %v", err)
 		os.Exit(1)
 	}
 
-	log.Println("lxd-doc finished successfully")
+	log.Println("incus-doc finished successfully")
 }
