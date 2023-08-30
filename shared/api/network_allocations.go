fc25be1a5dbca0d5f0741fc3e5304540d455cfba
Stéphane Graber <stgraber@stgraber.org>
2023-08-29 22:25:49 -0400

    shared/api: Cleanup network_allocations
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/api/network_allocations.go
+++ shared/api/network_allocations.go
@@ -10,12 +10,16 @@ type NetworkAllocations struct {
 	// The network address of the allocation (in CIDR format)
 	// Example: 192.0.2.1/24
 	Address string `json:"addresses" yaml:"addresses"`
-	// Name of the entity consuming the network address
-	UsedBy string `json:"used_by" yaml:"used_by"`
-	// Type of the entity consuming the network address
-	Type string `json:"type" yaml:"type"`
-	// Whether the entity comes from a network that performs egress source NAT
-	NAT bool `json:"nat" yaml:"nat"`
+
 	// Hwaddr is the MAC address of the entity consuming the network address
 	Hwaddr string `json:"hwaddr" yaml:"hwaddr"`
+
+	// Whether the entity comes from a network that performs egress source NAT
+	NAT bool `json:"nat" yaml:"nat"`
+
+	// Type of the entity consuming the network address
+	Type string `json:"type" yaml:"type"`
+
+	// Name of the entity consuming the network address
+	UsedBy string `json:"used_by" yaml:"used_by"`
 }
