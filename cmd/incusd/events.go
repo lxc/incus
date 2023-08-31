b41b6d096a7eb014ae6ce3f951e7a7d7225ef283
Thomas Hipp <thomas.hipp@canonical.com>
2023-08-31 12:40:22 +0200

    [lxd-import] events: Include EventTypeOVN when querying events
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- cmd/incusd/events.go
+++ cmd/incusd/events.go
@@ -19,7 +19,7 @@ import (
 	"github.com/lxc/incus/shared/ws"
 )
 
-var eventTypes = []string{api.EventTypeLogging, api.EventTypeOperation, api.EventTypeLifecycle}
+var eventTypes = []string{api.EventTypeLogging, api.EventTypeOperation, api.EventTypeLifecycle, api.EventTypeOVN}
 var privilegedEventTypes = []string{api.EventTypeLogging}
 
 var eventsCmd = APIEndpoint{
