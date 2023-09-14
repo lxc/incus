8011198706da7fb9387e27eecd8935b3ef414bb9
Stéphane Graber <stgraber@stgraber.org>
2023-09-14 19:35:54 -0400

    internal/linux: Move AvailableStorageDrivers
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/util/storage.go
+++ internal/util/storage.go
@@ -1,10 +1,5 @@
 package util
 
-import (
-	"github.com/lxc/incus/shared"
-	"github.com/lxc/incus/shared/api"
-)
-
 // PoolType represents a type of storage pool (local, remote or any).
 type PoolType string
 
@@ -16,42 +11,3 @@ const PoolTypeLocal PoolType = "local"
 
 // PoolTypeRemote represents remote storage pools.
 const PoolTypeRemote PoolType = "remote"
-
-// AvailableStorageDrivers returns a list of storage drivers that are available.
-func AvailableStorageDrivers(supportedDrivers []api.ServerStorageDriverInfo, poolType PoolType) []string {
-	backingFs, err := Detect(shared.VarPath())
-	if err != nil {
-		backingFs = "dir"
-	}
-
-	drivers := make([]string, 0, len(supportedDrivers))
-
-	// Check available backends.
-	for _, driver := range supportedDrivers {
-		if poolType == PoolTypeRemote && !driver.Remote {
-			continue
-		}
-
-		if poolType == PoolTypeLocal && driver.Remote {
-			continue
-		}
-
-		if poolType == PoolTypeAny && (driver.Name == "cephfs" || driver.Name == "cephobject") {
-			continue
-		}
-
-		if driver.Name == "dir" {
-			drivers = append(drivers, driver.Name)
-			continue
-		}
-
-		// btrfs can work in user namespaces too. (If source=/some/path/on/btrfs is used.)
-		if shared.RunningInUserNS() && (backingFs != "btrfs" || driver.Name != "btrfs") {
-			continue
-		}
-
-		drivers = append(drivers, driver.Name)
-	}
-
-	return drivers
-}
