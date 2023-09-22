898a3be236e631460c3fd2ac0d6483a4ef7112c5
Thomas Hipp <thomas.hipp@canonical.com>
2023-09-22 20:10:19 +0200

    [lxd-import] loki: Add hostname as `instance` label value
    Fixes #12290
    
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- internal/server/loki/loki.go
+++ internal/server/loki/loki.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"reflect"
 	"sort"
 	"strconv"
@@ -233,11 +234,17 @@ func (c *Client) HandleEvent(event api.Event) {
 		return
 	}
 
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "none"
+	}
+
 	entry := entry{
 		labels: LabelSet{
 			"app":      "incus",
 			"type":     event.Type,
 			"location": event.Location,
+			"instance": hostname,
 		},
 		Entry: Entry{
 			Timestamp: event.Timestamp,
