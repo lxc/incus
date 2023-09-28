35a82bf24cb047d04baa516c666c2ef7c8267e3b
sharathsivakumar <mailssr9@gmail.com>
2023-09-28 16:08:33 +0200

    move incusd shutdown to incus admin shutdown
    Signed-off-by: sharathsivakumar <mailssr9@gmail.com>

--- cmd/incus/admin.go
+++ cmd/incus/admin.go
@@ -19,10 +19,13 @@ func (c *cmdAdmin) Command() *cobra.Command {
 	cmd.Short = i18n.G("Manage incus daemon")
 	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
 		`Manage incus daemon`))
-	// init
+	// init sub-command
 	adminInitCmd := cmdAdminInit{global: c.global}
 	cmd.AddCommand(adminInitCmd.Command())
 
+	// shutdown sub-command
+	shutdownCmd := cmdAdminShutdown{global: c.global}
+	cmd.AddCommand(shutdownCmd.Command())
 	// sql
 	adminSqlCmd := cmdAdminSql{global: c.global}
 	cmd.AddCommand(adminSqlCmd.Command())
--- incusd/main.go
+++ incusd/main.go
@@ -181,10 +181,6 @@ func main() {
 	recoverCmd := cmdRecover{global: &globalCmd}
 	app.AddCommand(recoverCmd.Command())
 
-	// shutdown sub-command
-	shutdownCmd := cmdShutdown{global: &globalCmd}
-	app.AddCommand(shutdownCmd.Command())
-
 	// version sub-command
 	versionCmd := cmdVersion{global: &globalCmd}
 	app.AddCommand(versionCmd.Command())
