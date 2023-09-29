c70d37748e7b7ba55945bd9e3736fa7a82587542
Stéphane Graber <stgraber@stgraber.org>
2023-09-29 16:11:56 -0400

    internal/linux: Move systemd functions from incusd/util
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- incusd/util/http.go
+++ incusd/util/http.go
@@ -12,12 +12,9 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"time"
-
-	"golang.org/x/sys/unix"
 
 	"github.com/lxc/incus/internal/ports"
 	"github.com/lxc/incus/shared/api"
@@ -275,54 +272,6 @@ func ListenAddresses(configListenAddress string) ([]string, error) {
 
 	return addresses, nil
 }
-
-// GetListeners returns the socket-activated network listeners, if any.
-//
-// The 'start' parameter must be SystemdListenFDsStart, except in unit tests,
-// see the docstring of SystemdListenFDsStart below.
-func GetListeners(start int) []net.Listener {
-	defer func() {
-		_ = os.Unsetenv("LISTEN_PID")
-		_ = os.Unsetenv("LISTEN_FDS")
-	}()
-
-	pid, err := strconv.Atoi(os.Getenv("LISTEN_PID"))
-	if err != nil {
-		return nil
-	}
-
-	if pid != os.Getpid() {
-		return nil
-	}
-
-	fds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
-	if err != nil {
-		return nil
-	}
-
-	listeners := []net.Listener{}
-
-	for i := start; i < start+fds; i++ {
-		unix.CloseOnExec(i)
-
-		file := os.NewFile(uintptr(i), fmt.Sprintf("inherited-fd%d", i))
-		listener, err := net.FileListener(file)
-		if err != nil {
-			continue
-		}
-
-		listeners = append(listeners, listener)
-	}
-
-	return listeners
-}
-
-// SystemdListenFDsStart is the number of the first file descriptor that might
-// have been opened by systemd when socket activation is enabled. It's always 3
-// in real-world usage (i.e. the first file descriptor opened after stdin,
-// stdout and stderr), so this constant should always be the value passed to
-// GetListeners, except for unit tests.
-const SystemdListenFDsStart = 3
 
 // IsJSONRequest returns true if the content type of the HTTP request is JSON.
 func IsJSONRequest(r *http.Request) bool {
