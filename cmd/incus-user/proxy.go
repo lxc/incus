7d8d6abf14f69c4177c07f1a57dc09b99bb1ffa0
Stéphane Graber <stgraber@stgraber.org>
2023-10-05 15:14:16 -0400

    incus-user: Use users directory under main storage
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/incus-user/proxy.go
+++ cmd/incus-user/proxy.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"net"
 	"os"
-	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
 
@@ -18,7 +17,7 @@ import (
 
 func tlsConfig(uid uint32) (*tls.Config, error) {
 	// Load the client certificate.
-	content, err := os.ReadFile(filepath.Join("users", fmt.Sprintf("%d", uid), "client.crt"))
+	content, err := os.ReadFile(internalUtil.VarPath("users", fmt.Sprintf("%d", uid), "client.crt"))
 	if err != nil {
 		return nil, fmt.Errorf("Unable to open client certificate: %w", err)
 	}
@@ -26,7 +25,7 @@ func tlsConfig(uid uint32) (*tls.Config, error) {
 	tlsClientCert := string(content)
 
 	// Load the client key.
-	content, err = os.ReadFile(filepath.Join("users", fmt.Sprintf("%d", uid), "client.key"))
+	content, err = os.ReadFile(internalUtil.VarPath("users", fmt.Sprintf("%d", uid), "client.key"))
 	if err != nil {
 		return nil, fmt.Errorf("Unable to open client key: %w", err)
 	}
@@ -82,7 +81,7 @@ func proxyConnection(conn *net.UnixConn) {
 	defer logger.Debug("Disconnected")
 
 	// Check if the user was setup.
-	if !util.PathExists(filepath.Join("users", fmt.Sprintf("%d", creds.Uid))) {
+	if !util.PathExists(internalUtil.VarPath("users", fmt.Sprintf("%d", creds.Uid))) {
 		log.Infof("Setting up for uid %d", creds.Uid)
 		err := serverSetupUser(creds.Uid)
 		if err != nil {
