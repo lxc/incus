a815cea3fc6756c3469c0f7c8ef287732b3fb73f
Simon Deziel <simon.deziel@canonical.com>
2023-10-24 12:56:23 -0400

    [lxd-import] test/config: always use -- with `incus exec`
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- test/suites/config.sh
+++ test/suites/config.sh
@@ -69,14 +69,14 @@ testloopmounts() {
   umount -l "${TEST_DIR}/mnt"
   incus start foo
   incus config device add foo mnt disk source="${lpath}" path=/mnt
-  incus exec foo stat /mnt/hello
+  incus exec foo -- stat /mnt/hello
   # Note - we need to add a set_running_config_item to lxc
   # or work around its absence somehow.  Once that's done, we
   # can run the following two lines:
-  #incus exec foo reboot
-  #incus exec foo stat /mnt/hello
+  #incus exec foo -- reboot
+  #incus exec foo -- stat /mnt/hello
   incus restart foo --force
-  incus exec foo stat /mnt/hello
+  incus exec foo -- stat /mnt/hello
   incus config device remove foo mnt
   ensure_fs_unmounted "fs should have been hot-unmounted"
   incus restart foo --force
