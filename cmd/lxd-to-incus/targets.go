73f22c10770ba07ffe55e37480c5d50beb3c0c35
xsoalokinx <soalokin@live.com>
2023-10-29 07:42:02 +0000

    cmd/lxd-to-incus: Add OpenRC target support
    Signed-off-by: xsoalokinx <soalokin@live.com>

--- cmd/lxd-to-incus/targets.go
+++ cmd/lxd-to-incus/targets.go
@@ -16,7 +16,7 @@ type Target interface {
 	Paths() (*DaemonPaths, error)
 }
 
-var targets = []Target{&targetSystemd{}}
+var targets = []Target{&targetSystemd{}, &targetOpenRC{}}
 
 type targetSystemd struct{}
 
@@ -60,4 +60,48 @@ func (s *targetSystemd) Paths() (*DaemonPaths, error) {
 		Logs:   "/var/log/incus/",
 		Cache:  "/var/cache/incus/",
 	}, nil
+}
+
+type targetOpenRC struct{}
+
+func (s *targetOpenRC) Present() bool {
+        if !util.PathExists("/var/lib/incus/") {
+                return false
+        }
+
+        _, err := subprocess.RunCommand("rc-service", "--exists", "incus")
+        if err != nil {
+                return false
+        }
+
+        return true
+}
+
+func (s *targetOpenRC) Stop() error {
+        _, err := subprocess.RunCommand("rc-service", "incus", "stop")
+        return err
+}
+
+func (s *targetOpenRC) Start() error {
+        _, err := subprocess.RunCommand("rc-service", "incus", "start")
+        if err != nil {
+                return err
+        }
+
+        // Wait for the socket to become available.
+        time.Sleep(5 * time.Second)
+
+        return nil
+}
+
+func (s *targetOpenRC) Connect() (incus.InstanceServer, error) {
+        return incus.ConnectIncusUnix("/var/lib/incus/unix.socket", nil)
+}
+
+func (s *targetOpenRC) Paths() (*DaemonPaths, error) {
+        return &DaemonPaths{
+                Daemon: "/var/lib/incus/",
+                Logs:   "/var/log/incus/",
+                Cache:  "/var/cache/incus/",
+        }, nil
 }
