dc8c05ad76b3ada43b864afed94c07f38d453ca8
Stéphane Graber <stgraber@stgraber.org>
2023-10-29 23:03:26 -0400

    lxd-to-incus: Split the targets
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/lxd-to-incus/targets.go
+++ cmd/lxd-to-incus/targets.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"time"
-
 	"github.com/lxc/incus/client"
-	"github.com/lxc/incus/shared/subprocess"
-	"github.com/lxc/incus/shared/util"
 )
 
 type Target interface {
@@ -17,91 +13,3 @@ type Target interface {
 }
 
 var targets = []Target{&targetSystemd{}, &targetOpenRC{}}
-
-type targetSystemd struct{}
-
-func (s *targetSystemd) Present() bool {
-	if !util.PathExists("/var/lib/incus/") {
-		return false
-	}
-
-	_, err := subprocess.RunCommand("systemctl", "list-unit-files", "incus.service")
-	if err != nil {
-		return false
-	}
-
-	return true
-}
-
-func (s *targetSystemd) Stop() error {
-	_, err := subprocess.RunCommand("systemctl", "stop", "incus.service", "incus.socket")
-	return err
-}
-
-func (s *targetSystemd) Start() error {
-	_, err := subprocess.RunCommand("systemctl", "start", "incus.service", "incus.socket")
-	if err != nil {
-		return err
-	}
-
-	// Wait for the socket to become available.
-	time.Sleep(5 * time.Second)
-
-	return nil
-}
-
-func (s *targetSystemd) Connect() (incus.InstanceServer, error) {
-	return incus.ConnectIncusUnix("/var/lib/incus/unix.socket", nil)
-}
-
-func (s *targetSystemd) Paths() (*DaemonPaths, error) {
-	return &DaemonPaths{
-		Daemon: "/var/lib/incus/",
-		Logs:   "/var/log/incus/",
-		Cache:  "/var/cache/incus/",
-	}, nil
-}
-
-type targetOpenRC struct{}
-
-func (s *targetOpenRC) Present() bool {
-        if !util.PathExists("/var/lib/incus/") {
-                return false
-        }
-
-        _, err := subprocess.RunCommand("rc-service", "--exists", "incus")
-        if err != nil {
-                return false
-        }
-
-        return true
-}
-
-func (s *targetOpenRC) Stop() error {
-        _, err := subprocess.RunCommand("rc-service", "incus", "stop")
-        return err
-}
-
-func (s *targetOpenRC) Start() error {
-        _, err := subprocess.RunCommand("rc-service", "incus", "start")
-        if err != nil {
-                return err
-        }
-
-        // Wait for the socket to become available.
-        time.Sleep(5 * time.Second)
-
-        return nil
-}
-
-func (s *targetOpenRC) Connect() (incus.InstanceServer, error) {
-        return incus.ConnectIncusUnix("/var/lib/incus/unix.socket", nil)
-}
-
-func (s *targetOpenRC) Paths() (*DaemonPaths, error) {
-        return &DaemonPaths{
-                Daemon: "/var/lib/incus/",
-                Logs:   "/var/log/incus/",
-                Cache:  "/var/cache/incus/",
-        }, nil
-}
---
+++ cmd/lxd-to-incus/targets_openrc.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"time"
+
+	"github.com/lxc/incus/client"
+	"github.com/lxc/incus/shared/subprocess"
+	"github.com/lxc/incus/shared/util"
+)
+
+type targetOpenRC struct{}
+
+func (s *targetOpenRC) Present() bool {
+	if !util.PathExists("/var/lib/incus/") {
+		return false
+	}
+
+	_, err := subprocess.RunCommand("rc-service", "--exists", "incus")
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (s *targetOpenRC) Stop() error {
+	_, err := subprocess.RunCommand("rc-service", "incus", "stop")
+	return err
+}
+
+func (s *targetOpenRC) Start() error {
+	_, err := subprocess.RunCommand("rc-service", "incus", "start")
+	if err != nil {
+		return err
+	}
+
+	// Wait for the socket to become available.
+	time.Sleep(5 * time.Second)
+
+	return nil
+}
+
+func (s *targetOpenRC) Connect() (incus.InstanceServer, error) {
+	return incus.ConnectIncusUnix("/var/lib/incus/unix.socket", nil)
+}
+
+func (s *targetOpenRC) Paths() (*DaemonPaths, error) {
+	return &DaemonPaths{
+		Daemon: "/var/lib/incus/",
+		Logs:   "/var/log/incus/",
+		Cache:  "/var/cache/incus/",
+	}, nil
+}
