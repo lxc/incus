e0e2793b4639222cdcaa9dceaecf14cd7553e15b
Din Music <din.music@canonical.com>
2023-11-28 11:55:25 +0100

    [lxd-import] tests: Add server-side move tests
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/suites/container_move.sh
+++ test/suites/container_move.sh
@@ -10,50 +10,67 @@ test_container_move() {
   profile="test-profile"
 
   # Setup.
-  incus project create "${project}" --
+  incus project create "${project}"
   incus storage create "${pool2}" "${incus_backend}"
-  incus profile create "${profile}"
-  incus profile device add default root disk pool="${pool2}" path=/ --project "${project}"
+  incus profile create "${profile}" --project "${project}"
+  incus profile device add "${profile}" root disk pool="${pool2}" path=/ --project "${project}"
 
-  # Move project, verify root disk device is retained.
+  # Move to different project with same profile (root disk device is retained).
   incus init "${image}" c1
   incus move c1 --target-project "${project}"
-  [ "$(incus ls --project ${project} --format csv --columns n)" = "c1" ]         # Verify project.
-  [ "$(incus config device get c1 root pool --project ${project})" = "${pool}" ] # Verify pool is retained.
+  [ "$(incus ls --project ${project} --format csv --columns n)" = "c1" ]         # Verify new project.
+  [ "$(incus config device get c1 root pool --project ${project})" = "${pool}" ] # Verify same pool (new local device).
   incus delete -f c1 --project "${project}"
 
-  # Move to different storage pool.
+  # Move to different project with no profiles (root disk device is retained).
   incus init "${image}" c2
-  incus move c2 --storage "${pool2}"
-  [ "$(incus ls --format csv --columns n)" = "c2" ]          # Verify project.
-  [ "$(incus config device get c2 root pool)" = "${pool2}" ] # Verify pool.
-  incus delete -f c2
+  incus move c2 --target-project "${project}" --no-profiles
+  [ "$(incus ls --project ${project} --format csv --columns n)" = "c2" ]         # Verify new project.
+  [ "$(incus config device get c2 root pool --project ${project})" = "${pool}" ] # Verify same pool (new local device).
+  incus delete -f c2 --project "${project}"
 
-  # Move to different storage pool and project.
+  # Move to different project with new profiles (root disk device is retained).
   incus init "${image}" c3
-  incus move c3 --target-project "${project}" --storage "${pool2}"
-  [ "$(incus ls --project ${project} --format csv --columns n)" = "c3" ]          # Verify project.
-  [ "$(incus config device get c3 root pool --project ${project})" = "${pool2}" ] # Verify pool.
+  incus move c3 --target-project "${project}" --profile "${profile}"
+  [ "$(incus ls --project ${project} --format csv --columns n)" = "c3" ]         # Verify new project.
+  [ "$(incus config device get c3 root pool --project ${project})" = "${pool}" ] # Verify same pool (new local device).
+  incus config show c3 -e --project "${project}" | grep -- "- ${profile}"        # Verify new profile.
   incus delete -f c3 --project "${project}"
 
-  # Ensure profile is not retained.
-  incus init "${image}" c4 --profile default --profile "${profile}"
-  ! incus move c4 --target-project "${project}" # Err: Profile not found in target project
+  # Move to different project with non-existing profile.
+  incus init "${image}" c4
+  ! incus move c4 --target-project "${project}" --profile invalid # Err: Profile not found in target project
   incus delete -f c4
 
-  # Create matching profile in target project and ensure it is applied on move.
-  incus profile create "${profile}" --project "${project}"
-  incus profile set "${profile}" user.foo="test" --project "${project}"
-  incus init "${image}" c5 --profile default --profile "${profile}"
-  incus move c5 --target-project "${project}"
-  [ "$(incus ls --project ${project} --format csv --columns n)" = "c5" ] # Verify project.
-  [ "$(incus config get c5 user.foo -e --project ${project})" = "test" ] # Verify pool.
-  incus delete -f c5 --project "${project}"
+  # Move to different storage pool.
+  incus init "${image}" c5
+  incus move c5 --storage "${pool2}"
+  [ "$(incus ls --format csv --columns n)" = "c5" ]          # Verify same project.
+  [ "$(incus config device get c5 root pool)" = "${pool2}" ] # Verify new pool.
+  incus delete -f c5
 
-  # Cleanup.
-  incus profile device remove default root --project "${project}"
+  # Move to different project and storage pool.
+  incus init "${image}" c6
+  incus move c6 --target-project "${project}" --storage "${pool2}"
+  [ "$(incus ls --project ${project} --format csv --columns n)" = "c6" ]          # Verify new project.
+  [ "$(incus config device get c6 root pool --project ${project})" = "${pool2}" ] # Verify new pool.
+  incus delete -f c6 --project "${project}"
+
+  # Move to different project and overwrite storage pool using device entry.
+  incus init "${image}" c7 --storage "${pool}" --no-profiles
+  incus move c7 --target-project "${project}" --device "root,pool=${pool2}"
+  [ "$(incus ls --project ${project} --format csv --columns n)" = "c7" ]          # Verify new project.
+  [ "$(incus config device get c7 root pool --project ${project})" = "${pool2}" ] # Verify new pool.
+  incus delete -f c7 --project "${project}"
+
+  # Move to different project and apply config entry.
+  incus init "${image}" c8
+  incus move c8 --target-project "${project}" --config user.test=success
+  [ "$(incus ls --project ${project} --format csv --columns n)" = "c8" ]  # Verify new project.
+  [ "$(incus config get c8 user.test --project ${project})" = "success" ] # Verify new local config entry.
+  incus delete -f c8 --project "${project}"
+
   incus profile delete "${profile}" --project "${project}"
-  incus profile delete "${profile}"
   incus storage delete "${pool2}"
   incus project delete "${project}"
 }
