6cb220fd46b8dbb61cabd4aa03e535d74b3ce3ea
Stéphane Graber <stgraber@stgraber.org>
2023-12-08 17:46:50 -0500

    incusd/storage: Use Shutdown context for import from backup
    Make it consistent with the ISO import.
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/storage/backend.go
+++ internal/server/storage/backend.go
@@ -6905,7 +6905,7 @@ func (b *backend) CreateCustomVolumeFromBackup(srcBackup backup.Info, srcData io
 		Name: srcBackup.Name,
 	}
 
-	err := b.state.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
+	err := b.state.DB.Cluster.Transaction(b.state.ShutdownCtx, func(ctx context.Context, tx *db.ClusterTx) error {
 		return project.AllowVolumeCreation(tx, srcBackup.Project, req)
 	})
 	if err != nil {
