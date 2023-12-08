3e4ec308d9340c9f091ffc9013db5df2a3849875
Stéphane Graber <stgraber@stgraber.org>
2023-12-08 18:45:01 -0500

    lxd-to-incus: Don't spam the output with command failures
    Closes #287
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/lxd-to-incus/main.go
+++ cmd/lxd-to-incus/main.go
@@ -546,14 +546,22 @@ Instead this tool will be providing specific commands for each of the servers.
 		fmt.Println("=> Running data migration commands")
 		_, _ = logFile.WriteString("Running data migration commands:\n")
 
+		failures := 0
 		for _, cmd := range rewriteCommands {
 			_, _ = logFile.WriteString(fmt.Sprintf(" - %+v\n", cmd))
 
 			_, err := subprocess.RunCommand(cmd[0], cmd[1:]...)
 			if err != nil {
 				_, _ = logFile.WriteString(fmt.Sprintf("Failed to run command: %v\n", err))
-				fmt.Fprintf(os.Stderr, "Failed to run command: %v\n", err)
+				failures++
 			}
+		}
+
+		if failures > 0 {
+			fmt.Printf("==> WARNING: %d commands out of %d succeeded (%d failures)\n", len(rewriteCommands)-failures, len(rewriteCommands), failures)
+			fmt.Println("    Please review the log file for details.")
+			fmt.Println("    Note that in OVN environments, it's normal to see some failures")
+			fmt.Println("    related to Flow Rules and Switch Ports as those often change during the migration.")
 		}
 	}
 
