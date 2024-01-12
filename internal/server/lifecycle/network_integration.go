c8a7ecc6f6ecf2a8b9db969b567c45d94a16530e
Stéphane Graber <stgraber@stgraber.org>
2024-01-11 22:55:10 -0500

    incusd/lifecycle: Add network integration events
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: Luizalabs (https://luizalabs.com)

---
+++ internal/server/lifecycle/network_integration.go
@@ -0,0 +1,29 @@
+package lifecycle
+
+import (
+	"github.com/lxc/incus/internal/version"
+	"github.com/lxc/incus/shared/api"
+)
+
+// NetworkIntegrationAction represents a lifecycle event action for network integrations.
+type NetworkIntegrationAction string
+
+// All supported lifecycle events for network integrations.
+const (
+	NetworkIntegrationCreated = NetworkIntegrationAction(api.EventLifecycleNetworkIntegrationCreated)
+	NetworkIntegrationDeleted = NetworkIntegrationAction(api.EventLifecycleNetworkIntegrationDeleted)
+	NetworkIntegrationUpdated = NetworkIntegrationAction(api.EventLifecycleNetworkIntegrationUpdated)
+	NetworkIntegrationRenamed = NetworkIntegrationAction(api.EventLifecycleNetworkIntegrationRenamed)
+)
+
+// Event creates the lifecycle event for an action on a network integration.
+func (a NetworkIntegrationAction) Event(name string, requestor *api.EventLifecycleRequestor, ctx map[string]any) api.EventLifecycle {
+	u := api.NewURL().Path(version.APIVersion, "network-integrations", name)
+
+	return api.EventLifecycle{
+		Action:    string(a),
+		Source:    u.String(),
+		Context:   ctx,
+		Requestor: requestor,
+	}
+}
