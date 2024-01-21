256400872793e89fa9b499c1e5284b68582e8806
Stéphane Graber <stgraber@stgraber.org>
2024-01-20 23:43:01 -0500

    incusd/migration: Properly forward errors
    Closes #406
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/incusd/migrate_instance.go
+++ cmd/incusd/migrate_instance.go
@@ -149,7 +149,10 @@ func (s *migrationSourceWs) Do(state *state.State, migrateOp *operations.Operati
 	})
 	if err != nil {
 		l.Error("Failed migration on source", logger.Ctx{"err": err})
-		return fmt.Errorf("Failed migration on source: %w", err)
+
+		errMsg := fmt.Errorf("Failed migration on source: %w", err)
+		s.sendControl(errMsg)
+		return errMsg
 	}
 
 	return nil
@@ -277,7 +280,10 @@ func (c *migrationSink) Do(state *state.State, instOp *operationlock.InstanceOpe
 	})
 	if err != nil {
 		l.Error("Failed migration on target", logger.Ctx{"err": err})
-		return fmt.Errorf("Failed migration on target: %w", err)
+
+		errMsg := fmt.Errorf("Failed migration on target: %w", err)
+		c.sendControl(errMsg)
+		return errMsg
 	}
 
 	return nil
