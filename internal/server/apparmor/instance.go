53393deceb3a48924e3f1d28b0f150ad05c3be14
Stéphane Graber <stgraber@stgraber.org>
2024-02-08 16:05:58 +0100

    incusd/apparmor: Support unpriv binfmt_misc
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/apparmor/instance.go
+++ internal/server/apparmor/instance.go
@@ -174,6 +174,7 @@ func instanceProfile(sysOS *sys.OS, inst instance, extraBinaries []string) (stri
 			"feature_cgroup2":  sysOS.CGInfo.Layout == cgroup.CgroupsUnified || sysOS.CGInfo.Layout == cgroup.CgroupsHybrid,
 			"feature_stacking": sysOS.AppArmorStacking && !sysOS.AppArmorStacked,
 			"feature_unix":     unixSupported,
+			"kernel_binfmt":    util.IsFalseOrEmpty(inst.ExpandedConfig()["security.privileged"]) && sysOS.UnprivBinfmt,
 			"name":             InstanceProfileName(inst),
 			"namespace":        InstanceNamespaceName(inst),
 			"nesting":          util.IsTrue(inst.ExpandedConfig()["security.nesting"]),
