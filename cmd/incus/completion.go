1144c5d723e123946d7b7274fa587e62d82d501e
Adam Stephens <adam@valkor.net>
2024-02-12 23:11:36 -0500

    incus/config: add completions
    Signed-off-by: Adam Stephens <adam@valkor.net>

--- cmd/incus/completion.go
+++ cmd/incus/completion.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/lxc/incus/internal/instance"
 	"github.com/spf13/cobra"
 )
 
@@ -42,6 +43,15 @@ func (g *cmdGlobal) cmpImages(toComplete string) ([]string, cobra.ShellCompDirec
 	}
 
 	return results, cobra.ShellCompDirectiveNoFileComp
+}
+
+func (g *cmdGlobal) cmpInstanceAllKeys() ([]string, cobra.ShellCompDirective) {
+	keys := []string{}
+	for k, _ := range instance.InstanceConfigKeysAny {
+		keys = append(keys, k)
+	}
+
+	return keys, cobra.ShellCompDirectiveNoFileComp
 }
 
 func (g *cmdGlobal) cmpInstances(toComplete string) ([]string, cobra.ShellCompDirective) {
@@ -232,5 +242,5 @@ func (g *cmdGlobal) cmpRemotes(includeAll bool) ([]string, cobra.ShellCompDirect
 		results = append(results, fmt.Sprintf("%s:", remoteName))
 	}
 
-	return results, cobra.ShellCompDirectiveNoFileComp
+	return results, cobra.ShellCompDirectiveNoSpace
 }
