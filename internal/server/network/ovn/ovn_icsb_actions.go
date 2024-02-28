a7f9a5aebde1083e82a1edbbd9fe633fe678a06a
Stéphane Graber <stgraber@stgraber.org>
2024-02-27 22:15:54 -0500

    incusd/network/ovn: Add GetGateways to ICSB
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: Luizalabs (https://luizalabs.com)

---
+++ internal/server/network/ovn/ovn_icsb_actions.go
@@ -0,0 +1,39 @@
+package ovn
+
+import (
+	"context"
+
+	ovnICSB "github.com/lxc/incus/internal/server/network/ovn/schema/ovn-ic-sb"
+)
+
+// GetGateways returns a slice of gateways for the specified availability zone.
+func (o *ICSB) GetGateways(ctx context.Context, name string) ([]string, error) {
+	// Get the availability zone.
+	availabilityZone := ovnICSB.AvailabilityZone{
+		Name: name,
+	}
+
+	err := o.client.Get(ctx, &availabilityZone)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the gateways in the availability zone.
+	gateways := []ovnICSB.Gateway{}
+	gateway := ovnICSB.Gateway{
+		AvailabilityZone: availabilityZone.UUID,
+	}
+
+	err = o.client.Where(&gateway).List(ctx, &gateways)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the names.
+	names := make([]string, len(gateways))
+	for _, entry := range gateways {
+		names = append(names, entry.Name)
+	}
+
+	return names, nil
+}
