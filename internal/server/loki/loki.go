f6c7530f2c2ad6824a66f555f768cb15dea26718
Stéphane Graber <stgraber@stgraber.org>
2024-04-17 11:20:33 -0400

    incusd/loki: Allow overriding the location field
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/loki/loki.go
+++ internal/server/loki/loki.go
@@ -41,6 +41,7 @@ type config struct {
 	instance string
 	logLevel string
 	types    []string
+	location string
 
 	timeout time.Duration
 	url     *url.URL
@@ -63,7 +64,7 @@ type Client struct {
 }
 
 // NewClient returns a Client.
-func NewClient(ctx context.Context, u *url.URL, username string, password string, caCert string, instance string, logLevel string, labels []string, types []string) *Client {
+func NewClient(ctx context.Context, u *url.URL, username string, password string, caCert string, instance string, location string, logLevel string, labels []string, types []string) *Client {
 	client := Client{
 		cfg: config{
 			batchSize: 10 * 1024,
@@ -72,6 +73,7 @@ func NewClient(ctx context.Context, u *url.URL, username string, password string
 			username:  username,
 			password:  password,
 			instance:  instance,
+			location:  location,
 			labels:    labels,
 			logLevel:  logLevel,
 			timeout:   10 * time.Second,
@@ -230,11 +232,17 @@ func (c *Client) HandleEvent(event api.Event) {
 		return
 	}
 
+	// Support overriding the location field (used on standalone systems).
+	location := event.Location
+	if c.cfg.location != "" {
+		location = c.cfg.location
+	}
+
 	entry := entry{
 		labels: LabelSet{
 			"app":      "incus",
 			"type":     event.Type,
-			"location": event.Location,
+			"location": location,
 			"instance": c.cfg.instance,
 		},
 		Entry: Entry{
