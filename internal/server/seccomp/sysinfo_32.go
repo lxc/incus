dea0b4c0fed9b4c4541acaddfa2bc3869cc55d19
Stéphane Graber <stgraber@stgraber.org>
2024-05-27 15:15:37 -0400

    incusd/seccomp/sysinfo: Handle 32bit on 64bit
    Closes #881
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/seccomp/sysinfo_32.go
+++ internal/server/seccomp/sysinfo_32.go
@@ -8,12 +8,18 @@ import (
 
 // ToNative fills fields from s into native fields.
 func (s *Sysinfo) ToNative(n *unix.Sysinfo_t) {
-	n.Bufferram = uint32(s.Bufferram)
-	n.Freeram = uint32(s.Freeram)
-	n.Freeswap = uint32(s.Freeswap)
+	n.Bufferram = uint32(s.Bufferram / s.Unit)
+	n.Freeram = uint32(s.Freeram / s.Unit)
+	n.Freeswap = uint32(s.Freeswap / s.Unit)
 	n.Procs = s.Procs
-	n.Sharedram = uint32(s.Sharedram)
-	n.Totalram = uint32(s.Totalram)
-	n.Totalswap = uint32(s.Totalswap)
+	n.Sharedram = uint32(s.Sharedram / s.Unit)
+	n.Totalram = uint32(s.Totalram / s.Unit)
+	n.Totalswap = uint32(s.Totalswap / s.Unit)
 	n.Uptime = int32(s.Uptime)
+	n.Unit = uint32(s.Unit)
+}
+
+// ToNative32 should never be called on 32bit arches.
+func (s *Sysinfo) ToNative32(n unix.Sysinfo_t) *unix.Sysinfo_t {
+	return nil
 }
