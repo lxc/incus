e04dae41592ad46b1ab4085012e27faf2c9b1303
Stéphane Graber <stgraber@stgraber.org>
2024-07-22 23:45:17 -0400

    incusd/auth: Fill missing local volume location
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/auth/authorization_objects.go
+++ internal/server/auth/authorization_objects.go
@@ -168,7 +168,7 @@ func NewObject(objectType ObjectType, projectName string, identifierElements ...
 // Mux vars must be provided in the order that they are found in the endpoint path. If the object
 // requires a project name, this is taken from the project query parameter unless the URL begins
 // with /1.0/projects.
-func ObjectFromRequest(r *http.Request, objectType ObjectType, expandProject func(string) string, expandFingerprint func(string, string) string, muxVars ...string) (Object, error) {
+func ObjectFromRequest(r *http.Request, objectType ObjectType, expandProject func(string) string, expandFingerprint func(string, string) string, expandVolumeLocation func(string, string, string, string) string, muxVars ...string) (Object, error) {
 	// Shortcut for server objects which don't require any arguments.
 	if objectType == ObjectTypeServer {
 		return ObjectServer(), nil
@@ -196,11 +196,15 @@ func ObjectFromRequest(r *http.Request, objectType ObjectType, expandProject fun
 
 		if muxVar == "location" {
 			// Special handling for the location which is not present as a real mux var.
-			if location == "" {
-				continue
+			if location != "" {
+				muxValue = location
+			} else if objectType == ObjectTypeStorageVolume {
+				muxValue = expandVolumeLocation(projectName, vars["poolName"], vars["type"], vars["volumeName"])
 			}
 
-			muxValue = location
+			if muxValue == "" {
+				continue
+			}
 		} else {
 			muxValue, err = url.PathUnescape(vars[muxVar])
 			if err != nil {
