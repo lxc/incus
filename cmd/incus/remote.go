d664a456e1238099a37c537ad6f68e07f768e4ae
HassanAlsamahi <hassanalsamahi12@gmail.com>
2024-08-09 10:40:42 +0200

    incus/remote/list: Add support for column selection
    Part of #1027
    
    Signed-off-by: HassanAlsamahi <hassanalsamahi12@gmail.com>

--- cmd/incus/remote.go
+++ cmd/incus/remote.go
@@ -31,6 +31,11 @@ type cmdRemote struct {
 	global *cmdGlobal
 }
 
+type remoteColumn struct {
+	Name string
+	Data func(string, config.Remote) string
+}
+
 // Command returns a cobra.Command for use with (*cobra.Command).AddCommand.
 func (c *cmdRemote) Command() *cobra.Command {
 	cmd := &cobra.Command{}
@@ -749,6 +754,96 @@ Pre-defined column shorthand chars:
 
 const defaultRemoteColumns = "nupaPsg"
 
+func (c *cmdRemoteList) parseColumns() ([]remoteColumn, error) {
+	columnsShorthandMap := map[rune]remoteColumn{
+		'n': {i18n.G("NAME"), c.remoteNameColumnData},
+		'u': {i18n.G("URL"), c.addrColumnData},
+		'p': {i18n.G("PROTOCOL"), c.protocolColumnData},
+		'a': {i18n.G("AUTH TYPE"), c.authTypeColumnData},
+		'P': {i18n.G("PUBLIC"), c.publicColumnData},
+		's': {i18n.G("STATIC"), c.staticColumnData},
+		'g': {i18n.G("GLOBAL"), c.globalColumnData},
+	}
+
+	columnList := strings.Split(c.flagColumns, ",")
+
+	columns := []remoteColumn{}
+
+	for _, columnEntry := range columnList {
+		if columnEntry == "" {
+			return nil, fmt.Errorf(i18n.G("Empty column entry (redundant, leading or trailing command) in '%s'"), c.flagColumns)
+		}
+
+		for _, columnRune := range columnEntry {
+			column, ok := columnsShorthandMap[columnRune]
+			if !ok {
+				return nil, fmt.Errorf(i18n.G("Unknown column shorthand char '%c' in '%s'"), columnRune, columnEntry)
+			}
+
+			columns = append(columns, column)
+		}
+	}
+
+	return columns, nil
+
+}
+func (c *cmdRemoteList) remoteNameColumnData(name string, rc config.Remote) string {
+	conf := c.global.conf
+
+	strName := name
+	if name == conf.DefaultRemote {
+		strName = fmt.Sprintf("%s (%s)", name, i18n.G("current"))
+	}
+
+	return strName
+}
+
+func (c *cmdRemoteList) addrColumnData(name string, rc config.Remote) string {
+	return rc.Addr
+}
+
+func (c *cmdRemoteList) protocolColumnData(name string, rc config.Remote) string {
+	return rc.Protocol
+}
+
+func (c *cmdRemoteList) authTypeColumnData(name string, rc config.Remote) string {
+	if rc.AuthType == "" {
+		if strings.HasPrefix(rc.Addr, "unix:") {
+			rc.AuthType = "file access"
+		} else if rc.Protocol != "incus" {
+			rc.AuthType = "none"
+		} else {
+			rc.AuthType = api.AuthenticationMethodTLS
+		}
+	}
+	return rc.AuthType
+}
+
+func (c *cmdRemoteList) publicColumnData(name string, rc config.Remote) string {
+	strPublic := i18n.G("NO")
+	if rc.Public {
+		strPublic = i18n.G("YES")
+	}
+
+	return strPublic
+}
+
+func (c *cmdRemoteList) staticColumnData(name string, rc config.Remote) string {
+	strStatic := i18n.G("NO")
+	if rc.Static {
+		strStatic = i18n.G("YES")
+	}
+	return strStatic
+}
+
+func (c *cmdRemoteList) globalColumnData(name string, rc config.Remote) string {
+	strGlobal := i18n.G("NO")
+	if rc.Global {
+		strGlobal = i18n.G("YES")
+	}
+	return strGlobal
+}
+
 // Run is used in the RunE field of the cobra.Command returned by Command.
 func (c *cmdRemoteList) Run(cmd *cobra.Command, args []string) error {
 	conf := c.global.conf
@@ -759,58 +854,30 @@ func (c *cmdRemoteList) Run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	columns, err := c.parseColumns()
+
+	if err != nil {
+		return err
+	}
 	// List the remotes
 	data := [][]string{}
 	for name, rc := range conf.Remotes {
-		strPublic := i18n.G("NO")
-		if rc.Public {
-			strPublic = i18n.G("YES")
+
+		line := []string{}
+		for _, column := range columns {
+			line = append(line, column.Data(name, rc))
 		}
 
-		strStatic := i18n.G("NO")
-		if rc.Static {
-			strStatic = i18n.G("YES")
-		}
+		data = append(data, line)
 
-		strGlobal := i18n.G("NO")
-		if rc.Global {
-			strGlobal = i18n.G("YES")
-		}
-
-		if rc.Protocol == "" {
-			rc.Protocol = "incus"
-		}
-
-		if rc.AuthType == "" {
-			if strings.HasPrefix(rc.Addr, "unix:") {
-				rc.AuthType = "file access"
-			} else if rc.Protocol != "incus" {
-				rc.AuthType = "none"
-			} else {
-				rc.AuthType = api.AuthenticationMethodTLS
-			}
-		}
-
-		strName := name
-		if name == conf.DefaultRemote {
-			strName = fmt.Sprintf("%s (%s)", name, i18n.G("current"))
-		}
-
-		data = append(data, []string{strName, rc.Addr, rc.Protocol, rc.AuthType, strPublic, strStatic, strGlobal})
 	}
 
 	sort.Sort(cli.SortColumnsNaturally(data))
 
-	header := []string{
-		i18n.G("NAME"),
-		i18n.G("URL"),
-		i18n.G("PROTOCOL"),
-		i18n.G("AUTH TYPE"),
-		i18n.G("PUBLIC"),
-		i18n.G("STATIC"),
-		i18n.G("GLOBAL"),
+	header := []string{}
+	for _, column := range columns {
+		header = append(header, column.Name)
 	}
-
 	return cli.RenderTable(c.flagFormat, header, data, conf.Remotes)
 }
 
