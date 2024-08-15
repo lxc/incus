54aff82590a92849100707aac07e716660369510
HassanAlsamahi <hassanalsamahi12@gmail.com>
2024-08-15 19:54:19 +0200

    incus/network/list-allocations: Add support for column selection
    Signed-off-by: HassanAlsamahi <hassanalsamahi12@gmail.com>

--- cmd/incus/network_allocations.go
+++ cmd/incus/network_allocations.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -17,38 +19,38 @@ type cmdNetworkListAllocations struct {
 	flagFormat      string
 	flagProject     string
 	flagAllProjects bool
+	flagColumns     string
 }
 
-func (c *cmdNetworkListAllocations) pretty(allocs []api.NetworkAllocations) error {
-	header := []string{
-		i18n.G("USED BY"),
-		i18n.G("ADDRESS"),
-		i18n.G("TYPE"),
-		i18n.G("NAT"),
-		i18n.G("HARDWARE ADDRESS"),
-	}
-
-	data := [][]string{}
-	for _, alloc := range allocs {
-		row := []string{
-			alloc.UsedBy,
-			alloc.Address,
-			alloc.Type,
-			fmt.Sprint(alloc.NAT),
-			alloc.Hwaddr,
-		}
-
-		data = append(data, row)
-	}
-
-	return cli.RenderTable(c.flagFormat, header, data, allocs)
+type networkAllocationColumn struct {
+	Name string
+	Data func(api.NetworkAllocations) string
 }
 
 func (c *cmdNetworkListAllocations) Command() *cobra.Command {
 	cmd := &cobra.Command{}
 	cmd.Use = usage("list-allocations")
 	cmd.Short = i18n.G("List network allocations in use")
-	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G("List network allocations in use"))
+	cmd.Long = cli.FormatSection(i18n.G("Description"), i18n.G(
+		`List network allocations in use
+Default column layout: uatnm
+
+== Columns ==
+The -c option takes a comma separated list of arguments that control
+which instance attributes to output when displaying in table or csv
+format.
+
+Column arguments are either pre-defined shorthand chars (see below),
+or (extended) config keys.
+
+Commas between consecutive shorthand chars are optional.
+
+Pre-defined column shorthand chars:
+  u - Used by
+  a - Address
+  t - Type
+  n - NAT
+  m - Mac Address`))
 
 	// Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 	cmd.Args = cobra.MaximumNArgs(1)
@@ -57,7 +59,66 @@ func (c *cmdNetworkListAllocations) Command() *cobra.Command {
 	cmd.Flags().StringVarP(&c.flagFormat, "format", "f", "table", i18n.G("Format (csv|json|table|yaml|compact)")+"``")
 	cmd.Flags().StringVarP(&c.flagProject, "project", "p", api.ProjectDefaultName, i18n.G("Run again a specific project"))
 	cmd.Flags().BoolVar(&c.flagAllProjects, "all-projects", false, i18n.G("Run against all projects"))
+	cmd.Flags().StringVarP(&c.flagColumns, "columns", "c", defaultNetworkAllocationColumns, i18n.G("Columns")+"``")
+
 	return cmd
+}
+
+const defaultNetworkAllocationColumns = "uatnm"
+
+func (c *cmdNetworkListAllocations) parseColumns() ([]networkAllocationColumn, error) {
+	columnsShorthandMap := map[rune]networkAllocationColumn{
+		'u': {i18n.G("USED BY"), c.usedByColumnData},
+		'a': {i18n.G("ADDRESS"), c.addressColumnData},
+		't': {i18n.G("TYPE"), c.typeColumnData},
+		'n': {i18n.G("NAT"), c.natColumnData},
+		'm': {i18n.G("MAC ADDRESS"), c.macAddressColumnData},
+	}
+
+	columnList := strings.Split(c.flagColumns, ",")
+	columns := []networkAllocationColumn{}
+
+	for _, columnEntry := range columnList {
+		if columnEntry == "" {
+			return nil, fmt.Errorf(i18n.G("Empty column entry (redundant, leading or trailing command) in '%s'"), c.flagColumns)
+		}
+
+		for _, columnRune := range columnEntry {
+			column, ok := columnsShorthandMap[columnRune]
+			if !ok {
+				return nil, fmt.Errorf(i18n.G("Unknown column shorthand char '%c' in '%s'"), columnRune, columnEntry)
+			}
+
+			columns = append(columns, column)
+		}
+	}
+
+	return columns, nil
+}
+
+func (c *cmdNetworkListAllocations) usedByColumnData(alloc api.NetworkAllocations) string {
+	return alloc.UsedBy
+}
+
+func (c *cmdNetworkListAllocations) addressColumnData(alloc api.NetworkAllocations) string {
+	return alloc.Address
+}
+
+func (c *cmdNetworkListAllocations) typeColumnData(alloc api.NetworkAllocations) string {
+	return alloc.Type
+}
+
+func (c *cmdNetworkListAllocations) natColumnData(alloc api.NetworkAllocations) string {
+	strNat := "NO"
+	if alloc.NAT {
+		strNat = "YES"
+	}
+
+	return strNat
+}
+
+func (c *cmdNetworkListAllocations) macAddressColumnData(alloc api.NetworkAllocations) string {
+	return alloc.Hwaddr
 }
 
 func (c *cmdNetworkListAllocations) Run(cmd *cobra.Command, args []string) error {
@@ -74,19 +135,40 @@ func (c *cmdNetworkListAllocations) Run(cmd *cobra.Command, args []string) error
 	resource := resources[0]
 	server := resource.server.UseProject(c.flagProject)
 
+	var addresses []api.NetworkAllocations
 	if c.flagAllProjects {
-		addresses, err := server.GetNetworkAllocationsAllProjects()
+		addresses, err = server.GetNetworkAllocationsAllProjects()
 		if err != nil {
 			return err
 		}
-
-		return c.pretty(addresses)
+	} else {
+		addresses, err = server.GetNetworkAllocations()
+		if err != nil {
+			return err
+		}
 	}
 
-	addresses, err := server.GetNetworkAllocations()
+	columns, err := c.parseColumns()
 	if err != nil {
 		return err
 	}
 
-	return c.pretty(addresses)
+	data := [][]string{}
+	for _, address := range addresses {
+		line := []string{}
+		for _, column := range columns {
+			line = append(line, column.Data(address))
+		}
+
+		data = append(data, line)
+	}
+
+	sort.Sort(cli.SortColumnsNaturally(data))
+
+	header := []string{}
+	for _, column := range columns {
+		header = append(header, column.Name)
+	}
+
+	return cli.RenderTable(c.flagFormat, header, data, addresses)
 }
