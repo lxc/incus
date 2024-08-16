55edc0ab09f58ca1c6b235841d79b7ea9d7a7e42
Stéphane Graber <stgraber@stgraber.org>
2024-08-15 20:02:02 -0400

    incusd/network/ovn: Add healthcheck support in LoadBalancer
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: Luizalabs (https://luizalabs.com)

--- internal/server/network/ovn/ovn_nb_actions.go
+++ internal/server/network/ovn/ovn_nb_actions.go
@@ -143,8 +143,20 @@ type OVNLoadBalancerTarget struct {
 	Port    uint64
 }
 
+// OVNLoadBalancerHealthCheck represents an OVN load balancer health checker.
+type OVNLoadBalancerHealthCheck struct {
+	Interval     int
+	Timeout      int
+	SuccessCount int
+	FailureCount int
+
+	CheckerIPV4 net.IP
+	CheckerIPV6 net.IP
+}
+
 // OVNLoadBalancerVIP represents a OVN load balancer Virtual IP entry.
 type OVNLoadBalancerVIP struct {
+	HealthCheck   *OVNLoadBalancerHealthCheck
 	Protocol      string // Either "tcp" or "udp". But only applies to port based VIPs.
 	ListenAddress net.IP
 	ListenPort    uint64
@@ -2958,6 +2970,9 @@ func (o *NB) CreateLoadBalancer(ctx context.Context, loadBalancerName OVNLoadBal
 		Protocol: &ovnNB.LoadBalancerProtocolUDP,
 	}
 
+	// Keep track of health check settings.
+	healthChecks := map[string]*OVNLoadBalancerHealthCheck{}
+
 	// Build up the commands to add VIPs to the load balancer.
 	for _, r := range vips {
 		if r.ListenAddress == nil {
@@ -3003,13 +3018,146 @@ func (o *NB) CreateLoadBalancer(ctx context.Context, loadBalancerName OVNLoadBal
 			}
 
 			lb.Vips[listenAddress] = strings.Join(targetAddresses, ",")
+
+			if r.HealthCheck != nil {
+				healthChecks[listenAddress] = r.HealthCheck
+			}
 		}
 	}
 
 	// Create any used load-balancer.
+	lbhcCount := 0
 	for _, lb := range []*ovnNB.LoadBalancer{lbtcp, lbudp} {
 		if len(lb.Vips) == 0 {
 			continue
+		}
+
+		// Create healthcheck records.
+		lb.HealthCheck = []string{}
+		lb.IPPortMappings = map[string]string{}
+
+		for vip, targets := range lb.Vips {
+			// Check if an health check exists for the VIP.
+			healthCheck := healthChecks[vip]
+			if healthCheck == nil {
+				continue
+			}
+
+			// Create the record.
+			lbhc := &ovnNB.LoadBalancerHealthCheck{
+				UUID:    fmt.Sprintf("lbhc%d", lbhcCount),
+				Options: map[string]string{},
+				Vip:     vip,
+			}
+
+			// Set some defaults.
+			if healthCheck.FailureCount == 0 {
+				healthCheck.FailureCount = 3
+			}
+
+			if healthCheck.SuccessCount == 0 {
+				healthCheck.SuccessCount = 3
+			}
+
+			if healthCheck.Interval == 0 {
+				healthCheck.Interval = 10
+			}
+
+			if healthCheck.Timeout == 0 {
+				healthCheck.Timeout = 30
+			}
+
+			// Apply the settings.
+			lbhc.Options = map[string]string{
+				"failure_count": fmt.Sprintf("%d", healthCheck.FailureCount),
+				"success_count": fmt.Sprintf("%d", healthCheck.SuccessCount),
+				"interval":      fmt.Sprintf("%d", healthCheck.Interval),
+				"timeout":       fmt.Sprintf("%d", healthCheck.Timeout),
+			}
+
+			// Create the load balancer health checker.
+			createOps, err := o.client.Create(lbhc)
+			if err != nil {
+				return err
+			}
+
+			operations = append(operations, createOps...)
+
+			// Link LB to LBHC.
+			lb.HealthCheck = append(lb.HealthCheck, lbhc.UUID)
+
+			// Set up the port bindings.
+			for _, target := range strings.Split(targets, ",") {
+				// Split host and port.
+				host, _, err := net.SplitHostPort(target)
+				if err == nil {
+					target = host
+				}
+
+				// Skip existing entries.
+				_, ok := lb.IPPortMappings[target]
+				if ok {
+					continue
+				}
+
+				// Lookup the logical switch port.
+				var lspName string
+				for _, port := range ls.Ports {
+					lsp := ovnNB.LogicalSwitchPort{
+						UUID: port,
+					}
+
+					err = o.get(ctx, &lsp)
+					if err != nil {
+						return err
+					}
+
+					if lsp.ExternalIDs == nil {
+						continue
+					}
+
+					if OVNSwitch(lsp.ExternalIDs[ovnExtIDIncusSwitch]) != switchName {
+						continue
+					}
+
+					if lsp.DynamicAddresses != nil {
+						fields := strings.Split(*lsp.DynamicAddresses, " ")
+						if slices.Contains(fields, target) {
+							lspName = lsp.Name
+							break
+						}
+					}
+
+					for _, address := range lsp.Addresses {
+						fields := strings.Split(address, " ")
+						if slices.Contains(fields, target) {
+							lspName = lsp.Name
+							break
+						}
+					}
+				}
+
+				if lspName == "" {
+					return fmt.Errorf("Couldn't find a logical switch port for %q", target)
+				}
+
+				ip := net.ParseIP(target)
+				if ip.To4() == nil {
+					if healthCheck.CheckerIPV6 == nil {
+						return fmt.Errorf("No IPv6 address for load balancer health check")
+					}
+
+					lb.IPPortMappings[fmt.Sprintf("[%s]", target)] = fmt.Sprintf("%s:[%s]", lspName, healthCheck.CheckerIPV6.String())
+				} else {
+					if healthCheck.CheckerIPV4 == nil {
+						return fmt.Errorf("No IPv4 address for load balancer health check")
+					}
+
+					lb.IPPortMappings[target] = fmt.Sprintf("%s:%s", lspName, healthCheck.CheckerIPV4.String())
+				}
+			}
+
+			lbhcCount++
 		}
 
 		// Create the record.
