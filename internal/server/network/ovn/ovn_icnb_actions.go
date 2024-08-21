25bae07eaef20eb59978423c738ba73505b6a036
Stéphane Graber <stgraber@stgraber.org>
2024-08-21 00:53:25 -0400

    incusd/network/ovn: Record transit subnets
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: Luizalabs (https://luizalabs.com)

--- internal/server/network/ovn/ovn_icnb_actions.go
+++ internal/server/network/ovn/ovn_icnb_actions.go
@@ -2,6 +2,10 @@ package ovn
 
 import (
 	"context"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"net"
 
 	ovsdbClient "github.com/ovn-org/libovsdb/client"
 	"github.com/ovn-org/libovsdb/ovsdb"
@@ -30,8 +34,20 @@ func (o *ICNB) CreateTransitSwitch(ctx context.Context, name string, mayExist bo
 		return nil
 	}
 
+	// Generate a random IPv4 subnet (/28).
+	buf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(buf, rand.Uint32())
+	buf[0] = 169
+	buf[1] = 254
+	ipv4 := net.IP(buf)
+	ipv4Net := net.IPNet{IP: ipv4.Mask(net.CIDRMask(28, 32)), Mask: net.CIDRMask(28, 32)}
+
 	// Mark new switches as managed by Incus.
-	transitSwitch.ExternalIDs = map[string]string{"incus-managed": "true"}
+	transitSwitch.ExternalIDs = map[string]string{
+		"incus-managed":     "true",
+		"incus-subnet-ipv4": ipv4Net.String(),
+		"incus-subnet-ipv6": fmt.Sprintf("fd42:%x:%x:%x::/64", rand.Intn(65535), rand.Intn(65535), rand.Intn(65535)),
+	}
 
 	// Create the switch.
 	ops, err := o.client.Create(&transitSwitch)
