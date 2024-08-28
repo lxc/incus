62f2f961997e5c639ef63f7402204ded49be9294
Benjamin Somers <benjamin.somers@imt-atlantique.fr>
2024-08-28 23:04:45 +0000

    incusd: Move QEMU default values to a subpackage
    This commit breaks a dependency cycle when using scriptlets in the QEMU
    driver, as the instance placement scriptlet depends on the QEMU default
    values.
    
    Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

---
+++ internal/server/instance/drivers/qemudefault/default.go
@@ -0,0 +1,7 @@
+package qemudefault
+
+// CPUCores defines the default number of cores a VM will get if no limit specified.
+const CPUCores = 1
+
+// MemSize is the default memory size for VMs if no limit specified.
+const MemSize = "1GiB"
