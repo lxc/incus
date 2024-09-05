9618965175c74b5771f8aa722d0a6d42903be1d5
Stéphane Graber <stgraber@stgraber.org>
2024-09-05 13:28:56 -0400

    incusd/device/config: Fix comment
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/device/config/devices.go
+++ internal/server/device/config/devices.go
@@ -27,7 +27,7 @@ func (device Device) Validate(rules map[string]func(value string) error) error {
 	checkedFields := map[string]struct{}{}
 
 	for k, validator := range rules {
-		checkedFields[k] = struct{}{} //Mark field as checked.
+		checkedFields[k] = struct{}{} // Mark field as checked.
 		err := validator(device[k])
 		if err != nil {
 			return fmt.Errorf("Invalid value for device option %q: %w", k, err)
