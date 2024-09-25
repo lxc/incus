5f081010a6d93e6f79e70acd6aa2b9c459e94b99
Mathias Gibbens <mathias.gibbens@futurfusion.io>
2024-09-25 12:09:50 -0600

    internal/server/instance/drivers/qmp: Ensure that the device passed to RingbufRead() is a ring buffer
    Signed-off-by: Mathias Gibbens <mathias.gibbens@futurfusion.io>

--- internal/server/instance/drivers/qmp/errors.go
+++ internal/server/instance/drivers/qmp/errors.go
@@ -7,5 +7,8 @@ import (
 // ErrMonitorDisconnect is returned when interacting with a disconnected Monitor.
 var ErrMonitorDisconnect = fmt.Errorf("Monitor is disconnected")
 
-// ErrMonitorBadConsole is retuned when the requested console doesn't exist.
+// ErrMonitorBadConsole is returned when the requested console doesn't exist.
 var ErrMonitorBadConsole = fmt.Errorf("Requested console couldn't be found")
+
+// ErrNotARingbuf is returned when the requested device isn't a ring buffer.
+var ErrNotARingbuf = fmt.Errorf("Requested device isn't a ring buffer")
