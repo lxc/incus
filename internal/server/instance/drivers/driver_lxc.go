c91ab37a8d6074472a43f67aa7cc36331d3760cc
Stéphane Graber <stgraber@stgraber.org>
2024-10-21 15:37:23 -0400

    incusd/instance/lxc: Simplify findIdmap
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/instance/drivers/driver_lxc.go
+++ internal/server/instance/drivers/driver_lxc.go
@@ -271,7 +271,7 @@ func lxcCreate(s *state.State, args db.InstanceArgs, p api.Project, op *operatio
 		idmapSet, base, err = findIdmap(
 			s,
 			args.Name,
-			d.expandedConfig["security.idmap.isolated"],
+			util.IsTrue(d.expandedConfig["security.idmap.isolated"]),
 			d.expandedConfig["security.idmap.base"],
 			d.expandedConfig["security.idmap.size"],
 			d.expandedConfig["raw.idmap"],
@@ -471,12 +471,7 @@ func idmapSize(state *state.State, isolated bool, size string) (int64, error) {
 
 var idmapLock sync.Mutex
 
-func findIdmap(s *state.State, cName string, isolatedStr string, configBase string, configSize string, rawIdmap string) (*idmap.Set, int64, error) {
-	isolated := false
-	if util.IsTrue(isolatedStr) {
-		isolated = true
-	}
-
+func findIdmap(s *state.State, cName string, isolated bool, configBase string, configSize string, rawIdmap string) (*idmap.Set, int64, error) {
 	rawMaps, err := idmap.NewSetFromIncusIDMap(rawIdmap)
 	if err != nil {
 		return nil, 0, err
@@ -1929,7 +1924,7 @@ func (d *lxc) startCommon() (string, []func() error, error) {
 			idmapSet, base, err := findIdmap(
 				d.state,
 				d.Name(),
-				d.expandedConfig["security.idmap.isolated"],
+				util.IsTrue(d.expandedConfig["security.idmap.isolated"]),
 				d.expandedConfig["security.idmap.base"],
 				d.expandedConfig["security.idmap.size"],
 				d.expandedConfig["raw.idmap"],
@@ -4510,7 +4505,7 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 			idmapSet, base, err = findIdmap(
 				d.state,
 				d.Name(),
-				d.expandedConfig["security.idmap.isolated"],
+				util.IsTrue(d.expandedConfig["security.idmap.isolated"]),
 				d.expandedConfig["security.idmap.base"],
 				d.expandedConfig["security.idmap.size"],
 				d.expandedConfig["raw.idmap"],
