56c29ed3204c7fc8d8fe3b169657bd23c98b6bc7
Benjamin Somers <benjamin.somers@imt-atlantique.fr>
2024-10-29 22:04:53 +0000

    incusd/scriptlet: Add useful QMP functions
    Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

--- internal/server/scriptlet/qemu.go
+++ internal/server/scriptlet/qemu.go
@@ -3,6 +3,7 @@ package scriptlet
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"go.starlark.net/starlark"
 
@@ -46,13 +47,110 @@ func QEMURun(l logger.Logger, m *qmp.Monitor, instance string, stage string) err
 		return rv, nil
 	}
 
+	runCommandFromKwargs := func(funName string, kwargs []starlark.Tuple) (starlark.Value, error) {
+		qmpArgs := make(map[string]any)
+		for _, kwarg := range kwargs {
+			key, err := StarlarkUnmarshal(kwarg.Index(0))
+			if err != nil {
+				return nil, err
+			}
+
+			value, err := StarlarkUnmarshal(kwarg.Index(1))
+			if err != nil {
+				return nil, err
+			}
+
+			qmpArgs[key.(string)] = value
+		}
+
+		var resp struct {
+			Return any
+		}
+
+		err := m.Run(funName, qmpArgs, &resp)
+		if err != nil {
+			return nil, err
+		}
+
+		// Extract the return value
+		rv, err := StarlarkMarshal(resp.Return)
+		if err != nil {
+			return nil, fmt.Errorf("Marshalling QMP response failed: %w", err)
+		}
+
+		return rv, nil
+	}
+
+	runCommandFunc := func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		frame := thread.CallFrame(1)
+		errPrefix := fmt.Sprintf("run_command (%d:%d):", frame.Pos.Line, frame.Pos.Col)
+
+		argsLen := args.Len()
+		if argsLen != 1 {
+			return nil, fmt.Errorf("%s Expected exactly one positional argument, got %d", errPrefix, argsLen)
+		}
+
+		arg, err := StarlarkUnmarshal(args.Index(0))
+		if err != nil {
+			return nil, err
+		}
+
+		funName, ok := arg.(string)
+		if !ok {
+			return nil, fmt.Errorf("%s The positional argument must be a string representing a QMP function", errPrefix)
+		}
+
+		rv, err := runCommandFromKwargs(funName, kwargs)
+		if err != nil {
+			return nil, err
+		}
+
+		return rv, nil
+	}
+
+	makeQOM := func(funName string) *starlark.Builtin {
+		fun := func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+			frame := thread.CallFrame(1)
+			errPrefix := fmt.Sprintf("%s (%d:%d):", strings.ReplaceAll(funName, "-", "_"), frame.Pos.Line, frame.Pos.Col)
+
+			argsLen := args.Len()
+			if argsLen != 0 {
+				return nil, fmt.Errorf("%s Expected only keyword arguments, got %d positional argument(s)", errPrefix, argsLen)
+			}
+
+			rv, err := runCommandFromKwargs(funName, kwargs)
+			if err != nil {
+				return nil, err
+			}
+
+			return rv, nil
+		}
+
+		return starlark.NewBuiltin(funName, fun)
+	}
+
 	// Remember to match the entries in scriptletLoad.QEMUCompile() with this list so Starlark can
 	// perform compile time validation of functions used.
 	env := starlark.StringDict{
-		"log_info":  starlark.NewBuiltin("log_info", logFunc),
-		"log_warn":  starlark.NewBuiltin("log_warn", logFunc),
-		"log_error": starlark.NewBuiltin("log_error", logFunc),
-		"run_qmp":   starlark.NewBuiltin("run_qmp", runQMPFunc),
+		"log_info":       starlark.NewBuiltin("log_info", logFunc),
+		"log_warn":       starlark.NewBuiltin("log_warn", logFunc),
+		"log_error":      starlark.NewBuiltin("log_error", logFunc),
+		"run_qmp":        starlark.NewBuiltin("run_qmp", runQMPFunc),
+		"run_command":    starlark.NewBuiltin("run_command", runCommandFunc),
+		"blockdev_add":   makeQOM("blockdev-add"),
+		"blockdev_del":   makeQOM("blockdev-del"),
+		"chardev_add":    makeQOM("chardev-add"),
+		"chardev_change": makeQOM("chardev-change"),
+		"chardev_remove": makeQOM("chardev-remove"),
+		"device_add":     makeQOM("device_add"),
+		"device_del":     makeQOM("device_del"),
+		"netdev_add":     makeQOM("netdev_add"),
+		"netdev_del":     makeQOM("netdev_del"),
+		"object_add":     makeQOM("object-add"),
+		"object_del":     makeQOM("object-del"),
+		"qom_get":        makeQOM("qom-get"),
+		"qom_list":       makeQOM("qom-list"),
+		"qom_set":        makeQOM("qom-set"),
 	}
 
 	prog, thread, err := scriptletLoad.QEMUProgram(instance)
