8116662b112e6c46b1f194c2d9934d466832fb7d
Benjamin Somers <benjamin.somers@imt-atlantique.fr>
2024-11-22 16:09:10 +0000

    incusd/scriptlet: Add project and instance authorization getters
    Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

--- internal/server/scriptlet/auth/auth.go
+++ internal/server/scriptlet/auth/auth.go
@@ -9,6 +9,7 @@ import (
 	scriptletLoad "github.com/lxc/incus/v6/internal/server/scriptlet/load"
 	"github.com/lxc/incus/v6/internal/server/scriptlet/log"
 	"github.com/lxc/incus/v6/internal/server/scriptlet/marshal"
+	"github.com/lxc/incus/v6/shared/api"
 	"github.com/lxc/incus/v6/shared/logger"
 )
 
@@ -69,4 +70,90 @@ func AuthorizationRun(l logger.Logger, details *common.RequestDetails, object st
 	}
 
 	return bool(v.(starlark.Bool)), nil
+}
+
+func getAccess(l logger.Logger, fun string, args []starlark.Tuple) (*api.Access, error) {
+	access := &api.Access{}
+	emptyAccess := &api.Access{}
+	logFunc := log.CreateLogger(l, fmt.Sprintf("Authorization scriptlet (%s)", fun))
+
+	// Remember to match the entries in scriptletLoad.AuthorizationCompile() with this list so Starlark can
+	// perform compile time validation of functions used.
+	env := starlark.StringDict{
+		"log_info":  starlark.NewBuiltin("log_info", logFunc),
+		"log_warn":  starlark.NewBuiltin("log_warn", logFunc),
+		"log_error": starlark.NewBuiltin("log_error", logFunc),
+	}
+
+	prog, thread, err := scriptletLoad.AuthorizationProgram()
+	if err != nil {
+		return emptyAccess, err
+	}
+
+	globals, err := prog.Init(thread, env)
+	if err != nil {
+		return emptyAccess, fmt.Errorf("Failed initializing: %w", err)
+	}
+
+	globals.Freeze()
+
+	// Retrieve a global variable from starlark environment.
+	getter := globals[fun]
+	if getter == nil {
+		return emptyAccess, nil
+	}
+
+	// Call starlark function from Go.
+	v, err := starlark.Call(thread, getter, nil, args)
+	if err != nil {
+		return emptyAccess, fmt.Errorf("Failed to run: %w", err)
+	}
+
+	value, err := marshal.StarlarkUnmarshal(v)
+	if err != nil {
+		return emptyAccess, err
+	}
+
+	identifiers, ok := value.([]any)
+	if !ok {
+		return emptyAccess, fmt.Errorf("Failed with unexpected return value: %v", v)
+	}
+
+	for _, id := range identifiers {
+		identifier, ok := id.(string)
+		if !ok {
+			return emptyAccess, fmt.Errorf("Failed with unexpected return value: %v", v)
+		}
+
+		*access = append(*access, api.AccessEntry{
+			Identifier: identifier,
+			Role:       "unknown",
+			Provider:   "scriptlet",
+		})
+	}
+
+	return access, nil
+}
+
+// GetInstanceAccessRun runs the optional get_instance_access scriptlet function.
+func GetInstanceAccessRun(l logger.Logger, projectName string, instanceName string) (*api.Access, error) {
+	return getAccess(l, "get_instance_access", []starlark.Tuple{
+		{
+			starlark.String("project_name"),
+			starlark.String(projectName),
+		}, {
+			starlark.String("instance_name"),
+			starlark.String(instanceName),
+		},
+	})
+}
+
+// GetProjectAccessRun runs the optional get_project_access scriptlet function.
+func GetProjectAccessRun(l logger.Logger, projectName string) (*api.Access, error) {
+	return getAccess(l, "get_project_access", []starlark.Tuple{
+		{
+			starlark.String("project_name"),
+			starlark.String(projectName),
+		},
+	})
 }
