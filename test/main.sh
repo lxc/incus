cb8e7da0045e7736eb4428dfc54558e530657faf
Piotr Resztak <piotr.resztak@futurfusion.io>
2024-11-27 23:19:45 +0100

    tests: Add a reuse flag test for the 'incus image import'
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- test/main.sh
+++ test/main.sh
@@ -296,6 +296,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_image_auto_update "image auto-update"
     run_test test_image_prefer_cached "image prefer cached"
     run_test test_image_import_dir "import image from directory"
+    run_test test_image_import_with_reuse "import image with reuse flag"
     run_test test_image_refresh "image refresh"
     run_test test_image_acl "image acl"
     run_test test_cloud_init "cloud-init"
