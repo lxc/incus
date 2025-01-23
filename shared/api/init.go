369b8338f23c9a68fea16a43dc8a6cf568d5effe
Meghea Iulian <iulian.meghea@gmail.com>
2025-01-23 18:07:07 -0500

    shared/api: Add storage volume initialization
    Signed-off-by: Meghea Iulian <iulian.meghea@gmail.com>

--- shared/api/init.go
+++ shared/api/init.go
@@ -26,6 +26,12 @@ type InitLocalPreseed struct {
 	// Example: local dir storage pool
 	StoragePools []StoragePoolsPost `json:"storage_pools" yaml:"storage_pools"`
 
+	// Storage Volumes to add
+	// Example: local dir storage volume
+	//
+	// API extension: init_preseed_storage_volumes.
+	StorageVolumes []InitStorageVolumesProjectPost `json:"storage_volumes" yaml:"storage_volumes"`
+
 	// Profiles to add
 	// Example: "default" profile with a root disk device
 	Profiles []ProfilesPost `json:"profiles" yaml:"profiles"`
@@ -44,6 +50,23 @@ type InitNetworksProjectPost struct {
 	NetworksPost `yaml:",inline"`
 
 	// Project in which the network will reside
+	// Example: "default"
+	Project string
+}
+
+// InitStorageVolumesProjectPost represents the fields of a new storage volume along with its associated pool.
+//
+// swagger:model
+//
+// API extension: init_preseed_storage_volumes.
+type InitStorageVolumesProjectPost struct {
+	StorageVolumesPost `yaml:",inline"`
+
+	// Storage pool in which the volume will reside
+	// Example: "default"
+	Pool string
+
+	// Project in which the volume will reside
 	// Example: "default"
 	Project string
 }
