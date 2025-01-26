4f64aa56224b8aa4eefcba2ebdd0c0e52f3f7346
Meghea Iulian <iulian.meghea@gmail.com>
2025-01-26 09:20:55 +0200

    incus/admin/init: use api.InitProfileProjectPost
    Signed-off-by: Meghea Iulian <iulian.meghea@gmail.com>

--- cmd/incus/admin_init_auto.go
+++ cmd/incus/admin_init_auto.go
@@ -109,17 +109,20 @@ func (c *cmdAdminInit) RunAuto(cmd *cobra.Command, args []string, d incus.Instan
 		config.StoragePools = []api.StoragePoolsPost{pool}
 
 		// Profile entry
-		config.Profiles = []api.ProfilesPost{{
-			Name: "default",
-			ProfilePut: api.ProfilePut{
-				Devices: map[string]map[string]string{
-					"root": {
-						"type": "disk",
-						"path": "/",
-						"pool": pool.Name,
+		config.Profiles = []api.InitProfileProjectPost{{
+			ProfilesPost: api.ProfilesPost{
+				Name: "default",
+				ProfilePut: api.ProfilePut{
+					Devices: map[string]map[string]string{
+						"root": {
+							"type": "disk",
+							"path": "/",
+							"pool": pool.Name,
+						},
 					},
 				},
 			},
+			Project: api.ProjectDefaultName,
 		}}
 	}
 
@@ -170,17 +173,20 @@ func (c *cmdAdminInit) RunAuto(cmd *cobra.Command, args []string, d incus.Instan
 
 		// Add it to the profile
 		if config.Profiles == nil {
-			config.Profiles = []api.ProfilesPost{{
-				Name: "default",
-				ProfilePut: api.ProfilePut{
-					Devices: map[string]map[string]string{
-						"eth0": {
-							"type":    "nic",
-							"network": network.Name,
-							"name":    "eth0",
+			config.Profiles = []api.InitProfileProjectPost{{
+				ProfilesPost: api.ProfilesPost{
+					Name: "default",
+					ProfilePut: api.ProfilePut{
+						Devices: map[string]map[string]string{
+							"eth0": {
+								"type":    "nic",
+								"network": network.Name,
+								"name":    "eth0",
+							},
 						},
 					},
 				},
+				Project: api.ProjectDefaultName,
 			}}
 		} else {
 			config.Profiles[0].Devices["eth0"] = map[string]string{
