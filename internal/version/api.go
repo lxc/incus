f239b7817622555a63f56b0504dc0076325f63a3
Stéphane Graber <stgraber@stgraber.org>
2025-01-27 13:35:42 -0500

    api: init_preseed_profile_project
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/version/api.go
+++ internal/version/api.go
@@ -457,6 +457,7 @@ var APIExtensions = []string{
 	"network_bridge_acl_devices",
 	"instance_debug_memory",
 	"init_preseed_storage_volumes",
+	"init_preseed_profile_project",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
