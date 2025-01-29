f3b6008711990d82407d7e65da7aaaf752d68422
Jakob Mueller <me@jakobm.de>
2025-01-29 15:14:25 +0100

    incus/server/ip: Add `Master` parameter for veth and tuntap
    Signed-off-by: Jakob Mueller <me@jakobm.de>

--- internal/server/ip/link_veth.go
+++ internal/server/ip/link_veth.go
@@ -1,12 +1,30 @@
 package ip
 
+import (
+	"github.com/lxc/incus/v6/shared/subprocess"
+)
+
 // Veth represents arguments for link of type veth.
 type Veth struct {
 	Link
-	Peer Link
+	Peer   Link
+	Master string
 }
 
 // Add adds new virtual link.
 func (veth *Veth) Add() error {
-	return veth.Link.add("veth", append([]string{"peer"}, veth.Peer.args()...))
+	err := veth.Link.add("veth", append([]string{"peer"}, veth.Peer.args()...))
+
+	if err != nil {
+		return err
+	}
+
+	if veth.Master != "" {
+		_, err := subprocess.RunCommand("ip", "link", "set", veth.Name, "master", veth.Master)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
