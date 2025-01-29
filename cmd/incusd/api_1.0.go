81d48942b362ab501717429eb1a1dd1e6a3089e6
Luís Simas <luis.nascimento@luizalabs.com>
2025-01-29 10:32:45 -0300

    cmd/incusd: Trigger Linstor client setup on server config changes
    Signed-off-by: Luís Simas <luis.nascimento@luizalabs.com>

--- cmd/incusd/api_1.0.go
+++ cmd/incusd/api_1.0.go
@@ -794,6 +794,7 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 	oidcChanged := false
 	openFGAChanged := false
 	ovnChanged := false
+	linstorChanged := false
 	ovsChanged := false
 	syslogChanged := false
 
@@ -837,6 +838,9 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 
 		case "openfga.api.url", "openfga.api.token", "openfga.store.id":
 			openFGAChanged = true
+
+		case "storage.linstor.controller_connection", "storage.linstor.ca_cert", "storage.linstor.client_cert", "storage.linstor.client_key":
+			linstorChanged = true
 		}
 	}
 
@@ -999,6 +1003,13 @@ func doApi10UpdateTriggers(d *Daemon, nodeChanged, clusterChanged map[string]str
 
 	if syslogChanged {
 		err := d.setupSyslogSocket(nodeConfig.SyslogSocket())
+		if err != nil {
+			return err
+		}
+	}
+
+	if linstorChanged {
+		err := d.setupLinstor()
 		if err != nil {
 			return err
 		}
