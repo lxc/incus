83a4ab5b9674376d7c95771642624fba976d565d
Winicius Silva <winiciusab12@gmail.com>
2025-01-30 00:07:46 -0300

    incus/storage: implement the Filler function for Linstor volumes
    Signed-off-by: Winicius Silva <winiciusab12@gmail.com>
    Co-authored-by: Luís Simas <luis.nascimento@luizalabs.com>
    Co-authored-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

--- internal/server/storage/drivers/driver_linstor_volumes.go
+++ internal/server/storage/drivers/driver_linstor_volumes.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/lxc/incus/v6/internal/server/operations"
 	"github.com/lxc/incus/v6/shared/logger"
+	"github.com/lxc/incus/v6/shared/revert"
 	"github.com/lxc/incus/v6/shared/units"
 )
 
@@ -24,6 +25,11 @@ func (d *linstor) ValidateVolume(vol Volume, removeUnknownKeys bool) error {
 // CreateVolume creates an empty volume and can optionally fill it by executing the supplied
 // filler function.
 func (d *linstor) CreateVolume(vol Volume, filler *VolumeFiller, op *operations.Operation) error {
+	l := d.logger.AddContext(logger.Ctx{"volume": vol.Name()})
+	l.Debug("Creating a new Linstor volume")
+	rev := revert.New()
+	defer rev.Fail()
+
 	linstor, err := d.state.Linstor()
 	if err != nil {
 		return err
@@ -66,8 +72,69 @@ func (d *linstor) CreateVolume(vol Volume, filler *VolumeFiller, op *operations.
 		return fmt.Errorf("Unable to spawn from resource group: %w", err)
 	}
 
-	// TODO: run filler function if supplied
+	l.Debug("Spawned a new Linstor resource definition for volume", logger.Ctx{"resourceDefinitionName": resourceDefinitionName})
+	rev.Add(func() { _ = d.DeleteVolume(vol, op) })
 
+	err = vol.MountTask(func(mountPath string, op *operations.Operation) error {
+		// Run the volume filler function if supplied.
+		if filler != nil && filler.Fill != nil {
+			var err error
+			var devPath string
+
+			if IsContentBlock(vol.contentType) {
+				// Get the device path.
+				devPath, err = d.GetVolumeDiskPath(vol)
+				if err != nil {
+					return err
+				}
+			}
+
+			allowUnsafeResize := false
+			if vol.volType == VolumeTypeImage {
+				// Allow filler to resize initial image volume as needed.
+				// Some storage drivers don't normally allow image volumes to be resized due to
+				// them having read-only snapshots that cannot be resized. However when creating
+				// the initial image volume and filling it before the snapshot is taken resizing
+				// can be allowed and is required in order to support unpacking images larger than
+				// the default volume size. The filler function is still expected to obey any
+				// volume size restrictions configured on the pool.
+				// Unsafe resize is also needed to disable filesystem resize safety checks.
+				// This is safe because if for some reason an error occurs the volume will be
+				// discarded rather than leaving a corrupt filesystem.
+				allowUnsafeResize = true
+			}
+
+			// Run the filler.
+			err = d.runFiller(vol, devPath, filler, allowUnsafeResize)
+			if err != nil {
+				return err
+			}
+
+			// Move the GPT alt header to end of disk if needed.
+			if vol.IsVMBlock() {
+				err = d.moveGPTAltHeader(devPath)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		if vol.contentType == ContentTypeFS {
+			// Run EnsureMountPath again after mounting and filling to ensure the mount directory has
+			// the correct permissions set.
+			err = vol.EnsureMountPath()
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}, op)
+	if err != nil {
+		return nil
+	}
+
+	rev.Success()
 	return nil
 }
 
@@ -122,4 +189,14 @@ func (d *linstor) HasVolume(vol Volume) (bool, error) {
 	}
 
 	return true, nil
+}
+
+// GetVolumeDiskPath returns the location of a root disk block device.
+func (d *linstor) GetVolumeDiskPath(vol Volume) (string, error) {
+	if vol.IsVMBlock() || (vol.volType == VolumeTypeCustom && IsContentBlock(vol.contentType)) {
+		devPath, err := d.getLinstorDevPath(vol)
+		return devPath, err
+	}
+
+	return "", ErrNotSupported
 }
