0c5960dd122e4ef3954f0192ba96a57e87e3b44d
Benjamin Somers <benjamin.somers@imt-atlantique.fr>
2025-02-11 17:43:00 +0000

    test: Add standalone LINSTOR tests
    Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

---
+++ test/backends/linstor.sh
@@ -0,0 +1,51 @@
+# shellcheck shell=sh
+
+linstor_setup() {
+  # shellcheck disable=2039,3043
+  local INCUS_DIR
+
+  INCUS_DIR=$1
+
+  echo "==> Setting up LINSTOR backend in ${INCUS_DIR}"
+}
+
+linstor_preconfigure() {
+  # shellcheck disable=2039,3043
+  local INCUS_DIR
+
+  INCUS_DIR=$1
+
+  incus config set storage.linstor.controller_connection "${INCUS_LINSTOR_CLUSTER}"
+  if [ -n "${INCUS_LINSTOR_LOCAL_SATELLITE:-}" ]; then
+    incus config set storage.linstor.satellite.name "${INCUS_LINSTOR_LOCAL_SATELLITE}"
+  fi
+}
+
+linstor_configure() {
+  # shellcheck disable=2039,3043
+  local INCUS_DIR
+
+  INCUS_DIR=$1
+
+  echo "==> Configuring LINSTOR backend in ${INCUS_DIR}"
+
+  linstor_preconfigure "${INCUS_DIR}"
+  driver_config="linstor.resource_group.place_count=1"
+
+  if [ -n "${LINSTOR_PREFIX_OVERRIDE:-}" ]; then
+    driver_config="${driver_config} linstor.volume.prefix=${LINSTOR_PREFIX_OVERRIDE}"
+  fi
+
+  # shellcheck disable=SC2086
+  incus storage create "incustest-$(basename "${INCUS_DIR}")" linstor ${driver_config}
+  incus profile device add default root disk path="/" pool="incustest-$(basename "${INCUS_DIR}")"
+}
+
+linstor_teardown() {
+  # shellcheck disable=2039,3043
+  local INCUS_DIR
+
+  INCUS_DIR=$1
+
+  echo "==> Tearing down LINSTOR backend in ${INCUS_DIR}"
+}
