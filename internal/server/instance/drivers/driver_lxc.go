0ca0d3b6fbecb8da4b774d04f983be403211bd60
Luís Simas <luis.nascimento@luizalabs.com>
2025-02-27 15:41:11 -0300

    incusd/instance/drivers: Populate StorageMove and StoragePool when migrating lxc instances
    Signed-off-by: Luís Simas <luis.nascimento@luizalabs.com>

--- internal/server/instance/drivers/driver_lxc.go
+++ internal/server/instance/drivers/driver_lxc.go
@@ -5615,6 +5615,7 @@ func (d *lxc) MigrateSend(args instance.MigrateSendArgs) error {
 		VolumeOnly:         !args.Snapshots,
 		Info:               &localMigration.Info{Config: srcConfig},
 		ClusterMove:        args.ClusterMoveSourceName != "",
+		StorageMove:        args.StoragePool != "",
 	}
 
 	// Only send the snapshots that the target requests when refreshing.
@@ -6378,6 +6379,7 @@ func (d *lxc) MigrateReceive(args instance.MigrateReceiveArgs) error {
 			VolumeSize:            offerHeader.GetVolumeSize(), // Block size setting override.
 			VolumeOnly:            !args.Snapshots,
 			ClusterMoveSourceName: args.ClusterMoveSourceName,
+			StoragePool:           args.StoragePool,
 		}
 
 		// At this point we have already figured out the parent container's root
