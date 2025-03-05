0eae5da9ec4075a33d865b4f8051109ef0f659fb
JUN JIE NAN <nanjunjie@gmail.com>
2025-03-05 18:35:31 +0800

    incusd/instance/edk2: Fix EDK2 path overwrite issue
    Current INCUS_EDK2_PATH once set, no matter it's directory exists or not, it
    will overwrite the buildin EDK2 Path. The fix added two conditions to overwrite:
    1. buildit EDK2 Path directory not exist, and
    2. INCUS_EDK2_PATH directory exists.
    
    And a function naming issue is fixed too(GetAchitectureFirmwarePairs -> GetArchitectureFirmwarePairs).
    
    Signed-off-by: JUN JIE NAN <nanjunjie@gmail.com>

---
+++ internal/server/instance/drivers/edk2/driver_edk2_test.go
@@ -0,0 +1,88 @@
+package edk2
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/lxc/incus/v6/internal/server/util"
+)
+
+func tSetup(t *testing.T) func() {
+	t.Helper()
+	tmpdir, err := os.MkdirTemp("", "edk2*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	incusEdk2Path := filepath.Join(tmpdir, "ovmf")
+	err = os.MkdirAll(incusEdk2Path, 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, fn := range []string{"OVMF_CODE.fd", "OVMF.fd", "OVMF_VARS.fd"} {
+		f, err := os.Create(filepath.Join(incusEdk2Path, fn))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = f.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err = os.Setenv("INCUS_EDK2_PATH", incusEdk2Path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return func() {
+		err = os.Unsetenv("INCUS_EDK2_PATH")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = os.RemoveAll(tmpdir)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestGetEdk2Path(t *testing.T) {
+	incusEDK2Path, err := GetenvEdk2Path()
+	if err != nil || incusEDK2Path != "" {
+		t.Fatal(err, incusEDK2Path)
+	}
+
+	rb := tSetup(t)
+	defer rb()
+	incusEDK2Path, err = GetenvEdk2Path()
+	if err != nil || incusEDK2Path == "" {
+		t.Fatal(incusEDK2Path)
+	}
+}
+
+func TestArchitectureFirmwarePairs(t *testing.T) {
+	rb := tSetup(t)
+	defer rb()
+	architectures, err := util.GetArchitectures()
+	if err != nil || len(architectures) == 0 {
+		t.Fatal(err)
+	}
+
+	pairs := GetArchitectureFirmwarePairs(architectures[0])
+	if len(pairs) == 0 {
+		t.Fatal(pairs)
+	}
+
+	pair1 := pairs[0]
+	code, vars := pair1.Code, pair1.Vars
+	if !strings.HasSuffix(code, "ovmf/OVMF_CODE.fd") ||
+		!strings.HasSuffix(vars, "ovmf/OVMF_VARS.fd") {
+		t.Fatal(pair1)
+	}
+}
