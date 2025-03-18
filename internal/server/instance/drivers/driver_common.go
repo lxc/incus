35943f8baba68c5639c3420804ce88909daa2e63
JUN JIE NAN <nanjunjie@gmail.com>
2025-03-18 17:42:43 +0800

    incusd/instance/common: Check PID validity
    For pid = -1 skip Stat file not found error.
    
    Signed-off-by: JUN JIE NAN <nanjunjie@gmail.com>

--- internal/server/instance/drivers/driver_common.go
+++ internal/server/instance/drivers/driver_common.go
@@ -1597,6 +1597,10 @@ func (d *common) balanceNUMANodes() error {
 
 // Gets the process starting time.
 func (d *common) processStartedAt(pid int) (time.Time, error) {
+	if pid < 1 {
+		return time.Time{}, fmt.Errorf("Invalid PID %q", pid)
+	}
+
 	file, err := os.Stat(fmt.Sprintf("/proc/%d", pid))
 	if err != nil {
 		return time.Time{}, err
