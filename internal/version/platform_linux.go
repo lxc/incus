3e476a0febf8f4558d168f97572b9020920e34c9
Stéphane Graber <stgraber@stgraber.org>
2025-03-19 02:15:37 -0400

    internal/version: Update UserAgent logic for osarch change
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/version/platform_linux.go
+++ internal/version/platform_linux.go
@@ -3,18 +3,16 @@
 package version
 
 import (
-	"os"
 	"strings"
 
 	"github.com/lxc/incus/v6/internal/linux"
 	"github.com/lxc/incus/v6/shared/osarch"
-	"github.com/lxc/incus/v6/shared/util"
 )
 
 func getPlatformVersionStrings() []string {
 	versions := []string{}
 
-	// Add kernel version
+	// Add kernel version.
 	uname, err := linux.Uname()
 	if err != nil {
 		return versions
@@ -22,23 +20,13 @@ func getPlatformVersionStrings() []string {
 
 	versions = append(versions, strings.Split(uname.Release, "-")[0])
 
-	// Add distribution info
-	lsbRelease, err := osarch.GetLSBRelease()
-	if err == nil {
-		for _, key := range []string{"NAME", "VERSION_ID"} {
-			value, ok := lsbRelease[key]
-			if ok {
-				versions = append(versions, value)
-			}
-		}
-	}
+	// Add distribution info.
+	osRelease, err := osarch.GetOSRelease()
+	if err == nil && osRelease["NAME"] != "" {
+		versions = append(versions, osRelease["NAME"])
 
-	// Add chromebook info
-	if len(versions) == 1 && util.PathExists("/run/cros_milestone") {
-		content, err := os.ReadFile("/run/cros_milestone")
-		if err == nil {
-			versions = append(versions, "Chrome OS")
-			versions = append(versions, strings.TrimSpace(string(content)))
+		if osRelease["VERSION_ID"] != "" {
+			versions = append(versions, osRelease["VERSION_ID"])
 		}
 	}
 
