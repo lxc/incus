bd110fad3e684798339b45a53a55a097d84ae17e
Stéphane Graber <stgraber@stgraber.org>
2025-03-19 02:14:53 -0400

    shared/osarch: Extend OS detection logic
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/osarch/release.go
+++ shared/osarch/release.go
@@ -6,19 +6,65 @@ import (
 	"io/fs"
 	"os"
 	"strings"
+
+	"github.com/lxc/incus/v6/shared/util"
 )
 
-// GetLSBRelease returns a map with Linux distribution information.
-func GetLSBRelease() (map[string]string, error) {
-	osRelease, err := getLSBRelease("/etc/os-release")
-	if errors.Is(err, fs.ErrNotExist) {
-		return getLSBRelease("/usr/lib/os-release")
+// GetOSRelease returns a map with Linux distribution information.
+func GetOSRelease() (map[string]string, error) {
+	// Handle TrueNAS.
+	if util.PathExists("/usr/share/truenas") {
+		content, err := os.ReadFile("/etc/version")
+		if err == nil {
+			return map[string]string{
+				"NAME":       "TrueNAS Scale",
+				"VERSION_ID": strings.TrimSpace(string(content)),
+			}, nil
+		}
 	}
 
-	return osRelease, err
+	// Add chromebook info
+	if util.PathExists("/run/cros_milestone") {
+		content, err := os.ReadFile("/run/cros_milestone")
+		if err == nil {
+			return map[string]string{
+				"NAME":       "Chrome OS",
+				"VERSION_ID": strings.TrimSpace(string(content)),
+			}, nil
+		}
+	}
+
+	// Parse OS release files.
+	for _, osPath := range []string{"/etc/os-release", "/usr/lib/os-release"} {
+		osRelease, err := getOSRelease(osPath)
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				continue
+			}
+
+			return nil, err
+		}
+
+		// Check if we have what we need.
+		if osRelease["IMAGE_ID"] != "" {
+			return map[string]string{
+				"NAME":       osRelease["IMAGE_ID"],
+				"VERSION_ID": osRelease["IMAGE_VERSION"],
+			}, nil
+		}
+
+		if osRelease["NAME"] != "" {
+			return map[string]string{
+				"NAME":       osRelease["NAME"],
+				"VERSION_ID": osRelease["VERSION_ID"],
+			}, nil
+		}
+	}
+
+	return map[string]string{}, nil
 }
 
-func getLSBRelease(filename string) (map[string]string, error) {
+func getOSRelease(filename string) (map[string]string, error) {
 	osRelease := make(map[string]string)
 
 	data, err := os.ReadFile(filename)
