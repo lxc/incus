de51332500c0b07ef0acce5174f718368deceae2
Max Asnaashari <max.asna@futurfusion.io>
2025-03-21 11:14:04 -0700

    internal/server/db/cluster: Update generated code
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/server/db/cluster/config.interface.mapper.go
+++ internal/server/db/cluster/config.interface.mapper.go
@@ -8,17 +8,17 @@ import "context"
 type ConfigGenerated interface {
 	// GetConfig returns all available config.
 	// generator: config GetMany
-	GetConfig(ctx context.Context, db dbtx, parent string, filters ...ConfigFilter) (map[int]map[string]string, error)
+	GetConfig(ctx context.Context, db dbtx, parentTablePrefix string, parentColumnPrefix string, filters ...ConfigFilter) (map[int]map[string]string, error)
 
 	// CreateConfig adds a new config to the database.
 	// generator: config Create
-	CreateConfig(ctx context.Context, db dbtx, parent string, object Config) error
+	CreateConfig(ctx context.Context, db dbtx, parentTablePrefix string, parentColumnPrefix string, object Config) error
 
 	// UpdateConfig updates the config matching the given key parameters.
 	// generator: config Update
-	UpdateConfig(ctx context.Context, db dbtx, parent string, referenceID int, config map[string]string) error
+	UpdateConfig(ctx context.Context, db dbtx, parentTablePrefix string, parentColumnPrefix string, referenceID int, config map[string]string) error
 
 	// DeleteConfig deletes the config matching the given key parameters.
 	// generator: config DeleteMany
-	DeleteConfig(ctx context.Context, db dbtx, parent string, referenceID int) error
+	DeleteConfig(ctx context.Context, db dbtx, parentTablePrefix string, parentColumnPrefix string, referenceID int) error
 }
