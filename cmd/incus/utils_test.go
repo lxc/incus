3d2483423871fc1ed6b32953bcf0602faf5c30e4
Piotr Resztak <piotr.resztak@futurfusion.io>
2025-03-31 22:11:29 +0200

    tests: Added/Fixed tests for server-side filtering
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- cmd/incus/utils_test.go
+++ cmd/incus/utils_test.go
@@ -78,11 +78,15 @@ func (s *utilsTestSuite) TestGetServerSupportedFilters() {
 		"foo", "type=container", "user.blah=a", "status=running,stopped",
 	}
 
-	supportedFilters, unsupportedFilters := getServerSupportedFilters(filters, api.InstanceFull{}, false)
-	s.Equal([]string{"type=container"}, supportedFilters)
-	s.Equal([]string{"foo", "user.blah=a", "status=running,stopped"}, unsupportedFilters)
+	supportedFilters, unsupportedFilters := getServerSupportedFilters(filters, []string{}, false)
+	s.Equal([]string{"type=container", "user.blah=a", "status=running,stopped"}, supportedFilters)
+	s.Equal([]string{"foo"}, unsupportedFilters)
 
-	supportedFilters, unsupportedFilters = getServerSupportedFilters(filters, api.InstanceFull{}, true)
-	s.Equal([]string{"foo", "type=container"}, supportedFilters)
-	s.Equal([]string{"user.blah=a", "status=running,stopped"}, unsupportedFilters)
+	supportedFilters, unsupportedFilters = getServerSupportedFilters(filters, []string{}, true)
+	s.Equal([]string{"foo", "type=container", "user.blah=a", "status=running,stopped"}, supportedFilters)
+	s.Equal([]string{}, unsupportedFilters)
+
+	supportedFilters, unsupportedFilters = getServerSupportedFilters(filters, []string{"type", "status"}, true)
+	s.Equal([]string{"foo", "user.blah=a"}, supportedFilters)
+	s.Equal([]string{"type=container", "status=running,stopped"}, unsupportedFilters)
 }
