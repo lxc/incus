0f2976a7223c54d6ab9747d56eab348c64027417
Stéphane Graber <stgraber@stgraber.org>
2025-03-30 22:58:02 -0400

    shared/revert: Make golangci-lint clean
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/revert/revert_test.go
+++ shared/revert/revert_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func ExampleReverter_fail() {
-	revert := revert.New()
-	defer revert.Fail()
+	reverter := revert.New()
+	defer reverter.Fail()
 
-	revert.Add(func() { fmt.Println("1st step") })
-	revert.Add(func() { fmt.Println("2nd step") })
+	reverter.Add(func() { fmt.Println("1st step") })
+	reverter.Add(func() { fmt.Println("2nd step") })
 
 	// Revert functions are run in reverse order on return.
 	// Output: 2nd step
@@ -19,12 +19,12 @@ func ExampleReverter_fail() {
 }
 
 func ExampleReverter_success() {
-	revert := revert.New()
-	defer revert.Fail()
+	reverter := revert.New()
+	defer reverter.Fail()
 
-	revert.Add(func() { fmt.Println("1st step") })
-	revert.Add(func() { fmt.Println("2nd step") })
+	reverter.Add(func() { fmt.Println("1st step") })
+	reverter.Add(func() { fmt.Println("2nd step") })
 
-	revert.Success() // Revert functions added are not run on return.
+	reverter.Success() // Revert functions added are not run on return.
 	// Output:
 }
