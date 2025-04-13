a7c33301738aede3c035063e973b1d885d9bac7c
Stéphane Graber <stgraber@stgraber.org>
2025-04-13 00:32:04 -0400

    incusd/firewall: Add basic rules on nftables
    This aligns the behavior with OVN where the most basic connectivity for
    the purpose of obtaining IP addresses and DNS should be allowed.
    
    Closes #1919
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/firewall/drivers/drivers_xtables.go
+++ internal/server/firewall/drivers/drivers_xtables.go
@@ -811,7 +811,7 @@ func (d Xtables) instanceDeviceIPTablesComment(projectName string, instanceName 
 // If the parent bridge is managed by Incus then parentManaged argument should be true so that the rules added can
 // use the iptablesChainACLFilterPrefix chain. If not they are added to the main filter chains directly (which only
 // works for unmanaged bridges because those don't support ACLs).
-func (d Xtables) InstanceSetupBridgeFilter(projectName string, instanceName string, deviceName string, parentName string, hostName string, hwAddr string, IPv4Nets []*net.IPNet, IPv6Nets []*net.IPNet, parentManaged bool, macFiltering bool, aclRules []ACLRule) error {
+func (d Xtables) InstanceSetupBridgeFilter(projectName string, instanceName string, deviceName string, parentName string, hostName string, hwAddr string, IPv4Nets []*net.IPNet, IPv6Nets []*net.IPNet, IPv4DNS []string, IPv6DNS []string, parentManaged bool, macFiltering bool, aclRules []ACLRule) error {
 	if len(aclRules) > 0 {
 		return fmt.Errorf("ACL rules not supported for xtables bridge filtering")
 	}
