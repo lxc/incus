d08c303b0edc9264eed5f0a4bf9eb8d067b9ffd6
Stéphane Graber <stgraber@stgraber.org>
2025-04-13 21:17:03 -0400

    api: network_forward_snat
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    Sponsored-by: ActivePort (https://www.activeport.com.au)

--- internal/version/api.go
+++ internal/version/api.go
@@ -478,6 +478,7 @@ var APIExtensions = []string{
 	"instance_oci_entrypoint",
 	"network_address_set",
 	"server_logging",
+	"network_forward_snat",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
