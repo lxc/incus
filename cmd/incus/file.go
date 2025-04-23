b13778a89ee29f14f87e514836cca7e4f7ab8602
Stéphane Graber <stgraber@stgraber.org>
2025-04-22 21:02:10 -0400

    incus/file: Handle Windows
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/incus/file.go
+++ cmd/incus/file.go
@@ -969,6 +969,12 @@ func (c *cmdFilePush) Run(cmd *cobra.Command, args []string) error {
 }
 
 func (c *cmdFile) setOwnerMode(sftpConn *sftp.Client, targetPath string, args incus.InstanceFileArgs) error {
+	// Skip if not on UNIX.
+	_, err := sftpConn.StatVFS("/")
+	if err != nil {
+		return nil
+	}
+
 	// Get the current stat information.
 	st, err := sftpConn.Stat(targetPath)
 	if err != nil {
