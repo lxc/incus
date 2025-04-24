ad0163aa3b017d646016551ce240e1a5c61fd1c6
Gwendolyn <me@gwendolyn.dev>
2025-04-24 11:25:49 +0200

    incusd/ip/link: Switch to netlink
    Signed-off-by: Gwendolyn <me@gwendolyn.dev>

--- internal/server/ip/link_veth.go
+++ internal/server/ip/link_veth.go
@@ -1,29 +1,38 @@
 package ip
 
 import (
-	"github.com/lxc/incus/v6/shared/subprocess"
+	"github.com/vishvananda/netlink"
 )
 
 // Veth represents arguments for link of type veth.
 type Veth struct {
 	Link
 	Peer   Link
-	Master string
+	Master string // TODO: Link already has a master, and the way this one is used does not suggest that it is different
 }
 
 // Add adds new virtual link.
 func (veth *Veth) Add() error {
-	err := veth.Link.add("veth", append([]string{"peer"}, veth.Peer.args()...))
+	veth.Link.Master = veth.Master // TODO: this is just to see if it works
+	attrs, err := veth.netlinkAttrs()
 	if err != nil {
 		return err
 	}
 
-	if veth.Master != "" {
-		_, err := subprocess.RunCommand("ip", "link", "set", veth.Name, "master", veth.Master)
-		if err != nil {
-			return err
-		}
+	link := netlink.NewVeth(attrs)
+
+	peerAttrs, err := veth.Peer.netlinkAttrs()
+	if err != nil {
+		return err
 	}
 
-	return nil
+	link.PeerMTU = uint32(peerAttrs.MTU)
+	link.PeerName = peerAttrs.Name
+	link.PeerNamespace = peerAttrs.Namespace
+	link.PeerNumTxQueues = uint32(peerAttrs.NumTxQueues)
+	link.PeerNumRxQueues = uint32(peerAttrs.NumRxQueues)
+	link.PeerTxQLen = peerAttrs.TxQLen
+	link.PeerHardwareAddr = peerAttrs.HardwareAddr
+
+	return netlink.LinkAdd(link)
 }
