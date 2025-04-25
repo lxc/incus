3cef6ab6fc573a6003d7b5aa3dbd86fd988efa5f
Gwendolyn <me@gwendolyn.dev>
2025-04-25 15:10:15 +0200

    incusd/ip/tuntap: Switch to netlink
    Signed-off-by: Gwendolyn <me@gwendolyn.dev>

--- internal/server/ip/tuntap.go
+++ internal/server/ip/tuntap.go
@@ -1,7 +1,9 @@
 package ip
 
 import (
-	"github.com/lxc/incus/v6/shared/subprocess"
+	"fmt"
+
+	"github.com/vishvananda/netlink"
 )
 
 // Tuntap represents arguments for tuntap manipulation.
@@ -14,21 +16,36 @@ type Tuntap struct {
 
 // Add adds new tuntap interface.
 func (t *Tuntap) Add() error {
-	cmd := []string{"tuntap", "add", "name", t.Name, "mode", t.Mode}
+	var mode netlink.TuntapMode
+
+	switch t.Mode {
+	case "tun":
+		mode = netlink.TUNTAP_MODE_TUN
+	case "tap":
+		mode = netlink.TUNTAP_MODE_TAP
+	default:
+		return fmt.Errorf("Invalid tuntap mode %q", t.Mode)
+	}
+
+	var flags netlink.TuntapFlag
+
 	if t.MultiQueue {
-		cmd = append(cmd, "multi_queue")
+		flags = netlink.TUNTAP_MULTI_QUEUE_DEFAULTS
+	} else {
+		flags = netlink.TUNTAP_DEFAULTS
 	}
 
-	_, err := subprocess.RunCommand("ip", cmd...)
+	tuntap := &netlink.Tuntap{
+		LinkAttrs: netlink.LinkAttrs{
+			Name: t.Name,
+		},
+		Mode:  mode,
+		Flags: flags,
+	}
+
+	err := netlink.LinkAdd(tuntap)
 	if err != nil {
-		return err
-	}
-
-	if t.Master != "" {
-		_, err := subprocess.RunCommand("ip", "link", "set", t.Name, "master", t.Master)
-		if err != nil {
-			return err
-		}
+		return fmt.Errorf("Failed to create tuntap %q: %w", t.Name, err)
 	}
 
 	return nil
