94d317af385409bf628d9b860e61730aaca02743
karo <karolin.kostial@gmail.com>
2025-04-27 10:30:34 +0200

    incusd: Introduce patchRun type
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- cmd/incusd/patches.go
+++ cmd/incusd/patches.go
@@ -89,10 +89,12 @@ var patches = []patch{
 	{name: "auth_openfga_network_address_set", stage: patchPostNetworks, run: patchGenericAuthorization},
 }
 
+type patchRun func(name string, d *Daemon) error
+
 type patch struct {
 	name  string
 	stage patchStage
-	run   func(name string, d *Daemon) error
+	run   patchRun
 }
 
 func (p *patch) apply(d *Daemon) error {
