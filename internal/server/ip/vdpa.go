828bb6956b43ab41806726b051e334fe3f7e514f
Gwendolyn <me@gwendolyn.dev>
2025-04-27 10:26:46 +0200

    incusd/ip/vdpa: Remove unused
    Signed-off-by: Gwendolyn <me@gwendolyn.dev>

--- internal/server/ip/vdpa.go
+++ internal/server/ip/vdpa.go
@@ -5,14 +5,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"syscall"
 
 	"github.com/vishvananda/netlink"
-)
-
-// Base flags passed to all Netlink requests.
-const (
-	commonNLFlags = syscall.NLM_F_REQUEST | syscall.NLM_F_ACK
 )
 
 // MAX_VQP is the maximum number of VQPs supported by the vDPA device and is always the same as of now.
@@ -32,20 +26,12 @@ type VhostVDPA struct {
 	Path string
 }
 
-// MgmtVDPADev represents the vDPA management device information.
-type MgmtVDPADev struct {
-	BusName string // e.g. "pci"
-	DevName string // e.g. "0000:00:08.2"
-}
-
 // VDPADev represents the vDPA device information.
 type VDPADev struct {
 	// Name of the vDPA created device. e.g. "vdpa0" (note: the iproute2 associated command would look like `vdpa dev add mgmtdev pci/<PCI_SLOT_NAME> name vdpa0 max_vqp <MAX_VQP>`).
 	Name string
 	// Max VQs supported by the vDPA device.
 	MaxVQs uint32
-	// Associated vDPA management device.
-	MgmtDev *MgmtVDPADev
 	// Associated vhost-vdpa device.
 	VhostVDPA *VhostVDPA
 }
@@ -84,49 +70,6 @@ func getVhostVDPADevInPath(parentPath string) (*VhostVDPA, error) {
 	}
 
 	return nil, fmt.Errorf("No vhost-vdpa device found in %s", parentPath)
-}
-
-// ListVDPAMgmtDevices returns the list of all vDPA management devices.
-func ListVDPAMgmtDevices() ([]*MgmtVDPADev, error) {
-	netlinkDevices, err := netlink.VDPAGetMGMTDevList()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get vdpa mgmt dev list: %w", err)
-	}
-
-	devices := make([]*MgmtVDPADev, 0, len(netlinkDevices))
-	for _, dev := range netlinkDevices {
-		devices = append(devices, &MgmtVDPADev{
-			BusName: dev.BusName,
-			DevName: dev.DevName,
-		})
-	}
-
-	return devices, nil
-}
-
-// ListVDPADevices returns the list of all vDPA devices.
-func ListVDPADevices() ([]*VDPADev, error) {
-	netlinkDevices, err := netlink.VDPAGetDevList()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get vdpa dev list: %w", err)
-	}
-
-	devices := make([]*VDPADev, 0, len(netlinkDevices))
-	for _, dev := range netlinkDevices {
-		vhostVDPA, err := getVhostVDPADevInPath(filepath.Join(vdpaBusDevDir, dev.Name))
-		if err != nil {
-			return nil, err
-		}
-
-		devices = append(devices, &VDPADev{
-			Name:      dev.Name,
-			MaxVQs:    dev.MaxVQS,
-			MgmtDev:   nil, // TODO: the netlink library does not expose the associated management device information
-			VhostVDPA: vhostVDPA,
-		})
-	}
-
-	return devices, nil
 }
 
 // AddVDPADevice adds a new vDPA device.
@@ -176,7 +119,6 @@ func AddVDPADevice(pciDevSlotName string, volatile map[string]string) (*VDPADev,
 	return &VDPADev{
 		Name:      dev.Name,
 		MaxVQs:    dev.MaxVQS,
-		MgmtDev:   nil, // TODO: the netlink library does not expose the associated management device information
 		VhostVDPA: vhostVDPA,
 	}, nil
 }
