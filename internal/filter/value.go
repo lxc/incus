22a820edbca984e4ccf4002f1f31d456ed037b42
karo <karolin.kostial@gmail.com>
2025-04-30 18:30:36 +0200

    internal/filter: internalize the default case into the switch so it covers all iota constants
    Moves the external return into the switch statements, so that the switch over the iota covers all the cases internally.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- internal/filter/value.go
+++ internal/filter/value.go
@@ -41,7 +41,9 @@ func ValueOf(obj any, field string) any {
 					return v
 				}
 			}
+
 			return m[field]
+
 		case reflect.Map:
 			for _, entry := range value.MapKeys() {
 				if entry.Interface() != key {
@@ -51,8 +53,12 @@ func ValueOf(obj any, field string) any {
 				m := value.MapIndex(entry)
 				return ValueOf(m.Interface(), rest)
 			}
+
+			return nil
+
+		default:
+			return nil
 		}
-		return nil
 	}
 
 	for i := 0; i < value.NumField(); i++ {
