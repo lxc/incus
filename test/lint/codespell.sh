59268f12cb6f83c7fcd869c41e7a94d51a68d196
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-05-01 16:55:32 +0200

    lint: Exclude generated manpages from codespell
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- test/lint/codespell.sh
+++ test/lint/codespell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -eu
-codespell -S 'internal/server/apparmor/*.profile.go' -S 'doc/html/*' -I .codespell-ignore client cmd doc grafana internal scripts shared test
+codespell -S 'internal/server/apparmor/*.profile.go' -S 'doc/html/*' -S 'doc/reference/manpages/*' -I .codespell-ignore client cmd doc grafana internal scripts shared test
