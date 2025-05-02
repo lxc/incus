6ce7802cd177bc38071d83ecc4a37ac43bf45dde
Akshat Kumar <akaman.boss@gmail.com>
2025-05-02 22:00:48 +0000

    shared/api: Added s3 target field to InstanceBackupsPost for changes to shared/api/
    Signed-off-by: Akshat Kumar <akaman.boss@gmail.com>

--- shared/api/instance_backup.go
+++ shared/api/instance_backup.go
@@ -31,6 +31,10 @@ type InstanceBackupsPost struct {
 	//
 	// API extension: backup_compression_algorithm
 	CompressionAlgorithm string `json:"compression_algorithm" yaml:"compression_algorithm"`
+
+	// Where to store the backup if needed. Uses a presigned S3 URL
+	// Example: "https://<bucket-name>.s3.<region>.amazonaws.com/<object-key>?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=<credential>&X-Amz-Date=<timestamp>&X-Amz-Expires=<expiration>&X-Amz-SignedHeaders=host&X-Amz-Signature=<signature>"
+	S3BackupTarget string `json:"s3_backup_target,omitempty" yaml:"s3_backup_target,omitempty"`
 }
 
 // InstanceBackup represents an instance backup.
