4308282a2af5c142610572a15b652a973f352bfb
Stéphane Graber <stgraber@stgraber.org>
2025-05-05 23:32:08 -0400

    incusd: Switch to new RenameNetworkACL
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/db/network_acls.go
+++ internal/server/db/network_acls.go
@@ -120,19 +120,3 @@ func (c *ClusterTx) UpdateNetworkACL(ctx context.Context, id int64, put *api.Net
 
 	return nil
 }
-
-// RenameNetworkACL renames a Network ACL.
-func (c *ClusterTx) RenameNetworkACL(ctx context.Context, id int64, newName string) error {
-	idInt := int(id)
-	filter := cluster.NetworkACLFilter{ID: &idInt}
-	acls, err := cluster.GetNetworkACLs(ctx, c.tx, filter)
-	if err != nil {
-		return err
-	}
-
-	if len(acls) == 0 {
-		return api.StatusErrorf(http.StatusNotFound, "Network ACL not found")
-	}
-
-	return cluster.RenameNetworkACL(ctx, c.tx, acls[0].Project, acls[0].Name, newName)
-}
