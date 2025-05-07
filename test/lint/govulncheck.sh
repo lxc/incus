24a0f8d22d5c5b94fdf6b4cf67863bb6c53a30ab
Stéphane Graber <stgraber@stgraber.org>
2025-05-06 21:20:11 -0400

    lint/govulncheck: Don't test stdlib
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- test/lint/govulncheck.sh
+++ test/lint/govulncheck.sh
@@ -2,4 +2,8 @@
 
 echo "Checking for vulnerabilities in Go dependencies using govulncheck..."
 
+GOVERSION=$(go version | cut -d' ' -f3 | sed "s/go//g")
+cp go.mod go.mod.bak
+sed "s/^go 1.*/go ${GOVERSION}/" -i go.mod
 govulncheck ./...
+mv go.mod.bak go.mod
