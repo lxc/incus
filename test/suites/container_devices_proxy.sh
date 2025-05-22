lxc/incus

commit 4494bad12c572d069042cf169aa07d8fd82f4b3b

incusd/main_forkproxy: Join all namespaces at once
When dealing with an OCI container where PID1 runs as a non-root user,
the current logic which relies on all nsfd files belonging to root in
the container falls apart and results in permission errors.

To avoid the issue and simplify the joining logic, attach to all
namespaces at once.

Closes #1508

Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

cmd/incusd/main_forkproxy.go
@@ -120,6 +120,7 @@ void forkproxy(void)
 
 	if (pid == 0) {
 		int listen_nsfd;
+		int setns_flags;
 
 		whoami = FORKPROXY_CHILD;
 
@@ -134,19 +135,23 @@ void forkproxy(void)
 			_exit(EXIT_FAILURE);
 		}
 
-		// Attach to the user namespace of the listener
-		attach_userns_fd(listen_nsfd);
+		// Attach to the namespaces of the listener
+		setns_flags = CLONE_NEWNET;
 
-		// Attach to the network namespace of the listener
-		if (!change_namespaces(listen_pidfd, listen_nsfd, CLONE_NEWNET)) {
-			fprintf(stderr, "Error: %m - Failed setns to listener network namespace\n");
+		if (in_same_namespace(getpid(), listen_nsfd, "user") > 0)
+			setns_flags |= CLONE_NEWUSER;
+
+		if (needs_mntns & CONNECT_NEEDS_MNTNS)
+			setns_flags |= CLONE_NEWNS;
+
+		if (!change_namespaces(listen_pidfd, listen_nsfd, setns_flags)) {
+			fprintf(stderr, "Error: %m - Failed setns to listener namespaces\n");
 			_exit(EXIT_FAILURE);
 		}
 
-		if ((needs_mntns & LISTEN_NEEDS_MNTNS) && !change_namespaces(listen_pidfd, listen_nsfd, CLONE_NEWNS)) {
-			fprintf(stderr, "Error: %m - Failed setns to listener mount namespace\n");
-			_exit(EXIT_FAILURE);
-		}
+		// Complete switch to the user namespace of the connector
+		if (setns_flags & CLONE_NEWUSER)
+			finalize_userns();
 
 		close_prot_errno_disarm(listen_nsfd);
 		close_prot_errno_disarm(listen_pidfd);
@@ -166,6 +171,7 @@ void forkproxy(void)
 	} else {
 		pthread_t thread;
 		int connect_nsfd;
+		int setns_flags;
 
 		whoami = FORKPROXY_PARENT;
 
@@ -180,20 +186,23 @@ void forkproxy(void)
 			_exit(EXIT_FAILURE);
 		}
 
-		// Attach to the user namespace of the connector
-		attach_userns_fd(connect_nsfd);
+		// Attach to the namespaces of the connector
+		setns_flags = CLONE_NEWNET;
 
-		// Attach to the network namespace of the connector
-		if (!change_namespaces(connect_pidfd, connect_nsfd, CLONE_NEWNET)) {
-			fprintf(stderr, "Error: %m - Failed setns to connector network namespace\n");
+		if (in_same_namespace(getpid(), connect_nsfd, "user") > 0)
+			setns_flags |= CLONE_NEWUSER;
+
+		if (needs_mntns & CONNECT_NEEDS_MNTNS)
+			setns_flags |= CLONE_NEWNS;
+
+		if (!change_namespaces(connect_pidfd, connect_nsfd, setns_flags)) {
+			fprintf(stderr, "Error: %m - Failed setns to connector namespaces\n");
 			_exit(EXIT_FAILURE);
 		}
 
-		// Attach to the mount namespace of the connector
-		if ((needs_mntns & CONNECT_NEEDS_MNTNS) && !change_namespaces(connect_pidfd, connect_nsfd, CLONE_NEWNS)) {
-			fprintf(stderr, "Error: %m - Failed setns to connector mount namespace\n");
-			_exit(EXIT_FAILURE);
-		}
+		// Complete switch to the user namespace of the connector
+		if (setns_flags & CLONE_NEWUSER)
+			finalize_userns();
 
 		close_prot_errno_disarm(connect_nsfd);
 		close_prot_errno_disarm(connect_pidfd);

commit 8de0cde00a2798a74e3b498af96d6c5b6ef7f9c0

tests: add tests for bind=container with proxy device
As far as I can tell there were no tests for bind=container, which
caused us to miss the subtle breakage caused by commit 4494bad12c57
("incusd/main_forkproxy: Join all namespaces at once").

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

test/suites/container_devices_proxy.sh
@@ -351,6 +351,43 @@ container_devices_proxy_unix() {
 
   rm -f "${HOST_SOCK}"
 
+  # Switch to bind=container.
+  (
+    umask 0000
+    exec socat unix-listen:"${HOST_SOCK}",unlink-early exec:/bin/cat
+  ) &
+  SOCAT_PID=$!
+
+  # We need to swap connect= and listen= so it's simpler to just remove the
+  # device and re-add it with the right configuration.
+  incus config device remove proxyTester proxyDev
+  incus config device add proxyTester proxyDev proxy \
+    bind=container connect="unix:${HOST_SOCK}" listen="unix:/tmp/incustest-$(basename "${INCUS_DIR}").sock" \
+    mode=0777 uid=1234 gid=1234 security.uid=1234 security.gid=1234
+  sleep 0.5
+
+  PID="$(incus query /1.0/instances/proxyTester/state | jq .pid)"
+  CTR_SOCK="/proc/${PID}/root/tmp/incustest-$(basename "${INCUS_DIR}").sock"
+  # Double-check that the listen socket exists.
+  # <https://github.com/lxc/incus/pull/2136>
+  if ! [ -S "${CTR_SOCK}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not create a container-side listen socket"
+    false
+  fi
+
+  ECHO="$( ( echo "${MESSAGE}"; sleep 0.5 ) | nsenter -n -U -t "${PID}" -- socat - unix:"${CTR_SOCK}")"
+  kill "${SOCAT_PID}" 2>/dev/null || true
+  wait "${SOCAT_PID}" 2>/dev/null || true
+
+  if [ "${ECHO}" != "${MESSAGE}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not properly send data from container to host"
+    false
+  fi
+
+  rm -f "${HOST_SOCK}"
+
   # Cleanup
   incus delete -f proxyTester
 }
@@ -430,6 +467,39 @@ container_devices_proxy_tcp_unix() {
     false
   fi
 
+  # Switch to bind=container.
+  HOST_TCP_PORT2=$(local_tcp_port)
+  socat tcp-listen:"${HOST_TCP_PORT2}" exec:/bin/cat &
+  SOCAT_PID=$!
+
+  # We need to swap connect= and listen= so it's simpler to just remove the
+  # device and re-add it with the right configuration.
+  incus config device remove proxyTester proxyDev
+  incus config device add proxyTester proxyDev proxy \
+    bind=container connect="tcp:127.0.0.1:${HOST_TCP_PORT2}" listen="unix:/tmp/incustest-$(basename "${INCUS_DIR}").sock" \
+    mode=0777 uid=1234 gid=1234 security.uid=1234 security.gid=1234
+  sleep 0.5
+
+  PID="$(incus query /1.0/instances/proxyTester/state | jq .pid)"
+  CTR_SOCK="/proc/${PID}/root/tmp/incustest-$(basename "${INCUS_DIR}").sock"
+  # Double-check that the listen socket exists.
+  # <https://github.com/lxc/incus/pull/2136>
+  if ! [ -S "${CTR_SOCK}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not create a container-side listen socket"
+    false
+  fi
+
+  ECHO="$( ( echo "${MESSAGE}"; sleep 0.5 ) | nsenter -n -U -t "${PID}" -- socat - unix:"${CTR_SOCK}")"
+  kill "${SOCAT_PID}" 2>/dev/null || true
+  wait "${SOCAT_PID}" 2>/dev/null || true
+
+  if [ "${ECHO}" != "${MESSAGE}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not properly send data from container to host"
+    false
+  fi
+
   # Cleanup
   incus delete -f proxyTester
 }
@@ -498,6 +568,34 @@ container_devices_proxy_unix_tcp() {
 
   rm -f "${HOST_SOCK}"
 
+  # Switch to bind=container.
+  (
+    umask 0000
+    exec socat unix-listen:"${HOST_SOCK}",unlink-early exec:/bin/cat
+  ) &
+  SOCAT_PID=$!
+
+  # We need to swap connect= and listen= so it's simpler to just remove the
+  # device and re-add it with the right configuration.
+  incus config device remove proxyTester proxyDev
+  incus config device add proxyTester proxyDev proxy \
+    bind=container connect="unix:${HOST_SOCK}" listen="tcp:0.0.0.0:4321" \
+    security.uid=1234 security.gid=1234
+  sleep 0.5
+
+  PID="$(incus query /1.0/instances/proxyTester/state | jq .pid)"
+  ECHO="$( ( echo "${MESSAGE}"; sleep 0.5 ) | nsenter -n -U -t "${PID}" -- socat - tcp:127.0.0.1:4321)"
+  kill "${SOCAT_PID}" 2>/dev/null || true
+  wait "${SOCAT_PID}" 2>/dev/null || true
+
+  if [ "${ECHO}" != "${MESSAGE}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not properly send data from container to host"
+    false
+  fi
+
+  rm -f "${HOST_SOCK}"
+
   # Cleanup
   incus delete -f proxyTester
 }
@@ -557,6 +655,30 @@ container_devices_proxy_udp() {
   if [ "${ECHO}" != "${MESSAGE}" ]; then
     cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
     echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  # Switch to bind=container.
+  HOST_UDP_PORT2=$(local_tcp_port)
+  socat udp-listen:"${HOST_UDP_PORT2}" exec:/bin/cat &
+  SOCAT_PID=$!
+
+  # We need to swap connect= and listen= so it's simpler to just remove the
+  # device and re-add it with the right configuration.
+  incus config device remove proxyTester proxyDev
+  incus config device add proxyTester proxyDev proxy \
+    bind=container connect="udp:127.0.0.1:${HOST_UDP_PORT2}" listen="udp:127.0.0.1:4321" \
+    security.uid=1234 security.gid=1234
+  sleep 0.5
+
+  PID="$(incus query /1.0/instances/proxyTester/state | jq .pid)"
+  ECHO="$( ( echo "${MESSAGE}"; sleep 0.5 ) | nsenter -n -U -t "${PID}" -- socat - udp:127.0.0.1:4321)"
+  kill "${SOCAT_PID}" 2>/dev/null || true
+  wait "${SOCAT_PID}" 2>/dev/null || true
+
+  if [ "${ECHO}" != "${MESSAGE}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not properly send data from container to host"
     false
   fi
 
@@ -628,6 +750,40 @@ container_devices_proxy_unix_udp() {
 
   rm -f "${HOST_SOCK}"
 
+  # Switch to bind=container (we use connect=udp for the host because
+  # listen=udp doesn't work with non-udp connect= proxies).
+  HOST_UDP_PORT=$(local_tcp_port)
+  socat udp-listen:"${HOST_UDP_PORT}" exec:/bin/cat &
+  SOCAT_PID=$!
+
+  # We need to swap connect= and listen= so it's simpler to just remove the
+  # device and re-add it with the right configuration.
+  incus config device remove proxyTester proxyDev
+  incus config device add proxyTester proxyDev proxy \
+    bind=container connect="udp:127.0.0.1:${HOST_UDP_PORT}" listen="unix:/tmp/incustest-$(basename "${INCUS_DIR}").sock" \
+    mode=0777 uid=1234 gid=1234 security.uid=1234 security.gid=1234
+  sleep 0.5
+
+  PID="$(incus query /1.0/instances/proxyTester/state | jq .pid)"
+  CTR_SOCK="/proc/${PID}/root/tmp/incustest-$(basename "${INCUS_DIR}").sock"
+  # Double-check that the listen socket exists.
+  # <https://github.com/lxc/incus/pull/2136>
+  if ! [ -S "${CTR_SOCK}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not create a container-side listen socket"
+    false
+  fi
+
+  ECHO="$( ( echo "${MESSAGE}"; sleep 0.5 ) | nsenter -n -U -t "${PID}" -- socat - unix:"${CTR_SOCK}")"
+  kill "${SOCAT_PID}" 2>/dev/null || true
+  wait "${SOCAT_PID}" 2>/dev/null || true
+
+  if [ "${ECHO}" != "${MESSAGE}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not properly send data from container to host"
+    false
+  fi
+
   # Cleanup
   incus delete -f proxyTester
 }
@@ -687,6 +843,31 @@ container_devices_proxy_tcp_udp() {
   if [ "${ECHO}" != "${MESSAGE}" ]; then
     cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
     echo "Proxy device did not properly restart when config was updated"
+    false
+  fi
+
+  # Switch to bind=container (we use connect=udp for the host because
+  # listen=udp doesn't work with non-udp connect= proxies).
+  HOST_UDP_PORT=$(local_tcp_port)
+  socat udp-listen:"${HOST_UDP_PORT}" exec:/bin/cat &
+  SOCAT_PID=$!
+
+  # We need to swap connect= and listen= so it's simpler to just remove the
+  # device and re-add it with the right configuration.
+  incus config device remove proxyTester proxyDev
+  incus config device add proxyTester proxyDev proxy \
+    bind=container connect="udp:127.0.0.1:${HOST_UDP_PORT}" listen="tcp:127.0.0.1:4321" \
+    security.uid=1234 security.gid=1234
+  sleep 0.5
+
+  PID="$(incus query /1.0/instances/proxyTester/state | jq .pid)"
+  ECHO="$( ( echo "${MESSAGE}"; sleep 0.5 ) | nsenter -n -U -t "${PID}" -- socat - tcp:127.0.0.1:4321)"
+  kill "${SOCAT_PID}" 2>/dev/null || true
+  wait "${SOCAT_PID}" 2>/dev/null || true
+
+  if [ "${ECHO}" != "${MESSAGE}" ]; then
+    cat "${INCUS_DIR}/logs/proxyTester/proxy.proxyDev.log"
+    echo "Proxy device with bind=container did not properly send data from container to host"
     false
   fi
 