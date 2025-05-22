lxc/incus

commit 4494bad12c572d069042cf169aa07d8fd82f4b3b

incusd/main_forkproxy: Join all namespaces at once
When dealing with an OCI container where PID1 runs as a non-root user,
the current logic which relies on all nsfd files belonging to root in
the container falls apart and results in permission errors.

To avoid the issue and simplify the joining logic, attach to all
namespaces at once.

Closes #1508

Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

cmd/incusd/main_forkproxy.go
@@ -120,6 +120,7 @@ void forkproxy(void)
 
 	if (pid == 0) {
 		int listen_nsfd;
+		int setns_flags;
 
 		whoami = FORKPROXY_CHILD;
 
@@ -134,19 +135,23 @@ void forkproxy(void)
 			_exit(EXIT_FAILURE);
 		}
 
-		// Attach to the user namespace of the listener
-		attach_userns_fd(listen_nsfd);
+		// Attach to the namespaces of the listener
+		setns_flags = CLONE_NEWNET;
 
-		// Attach to the network namespace of the listener
-		if (!change_namespaces(listen_pidfd, listen_nsfd, CLONE_NEWNET)) {
-			fprintf(stderr, "Error: %m - Failed setns to listener network namespace\n");
+		if (in_same_namespace(getpid(), listen_nsfd, "user") > 0)
+			setns_flags |= CLONE_NEWUSER;
+
+		if (needs_mntns & CONNECT_NEEDS_MNTNS)
+			setns_flags |= CLONE_NEWNS;
+
+		if (!change_namespaces(listen_pidfd, listen_nsfd, setns_flags)) {
+			fprintf(stderr, "Error: %m - Failed setns to listener namespaces\n");
 			_exit(EXIT_FAILURE);
 		}
 
-		if ((needs_mntns & LISTEN_NEEDS_MNTNS) && !change_namespaces(listen_pidfd, listen_nsfd, CLONE_NEWNS)) {
-			fprintf(stderr, "Error: %m - Failed setns to listener mount namespace\n");
-			_exit(EXIT_FAILURE);
-		}
+		// Complete switch to the user namespace of the connector
+		if (setns_flags & CLONE_NEWUSER)
+			finalize_userns();
 
 		close_prot_errno_disarm(listen_nsfd);
 		close_prot_errno_disarm(listen_pidfd);
@@ -166,6 +171,7 @@ void forkproxy(void)
 	} else {
 		pthread_t thread;
 		int connect_nsfd;
+		int setns_flags;
 
 		whoami = FORKPROXY_PARENT;
 
@@ -180,20 +186,23 @@ void forkproxy(void)
 			_exit(EXIT_FAILURE);
 		}
 
-		// Attach to the user namespace of the connector
-		attach_userns_fd(connect_nsfd);
+		// Attach to the namespaces of the connector
+		setns_flags = CLONE_NEWNET;
 
-		// Attach to the network namespace of the connector
-		if (!change_namespaces(connect_pidfd, connect_nsfd, CLONE_NEWNET)) {
-			fprintf(stderr, "Error: %m - Failed setns to connector network namespace\n");
+		if (in_same_namespace(getpid(), connect_nsfd, "user") > 0)
+			setns_flags |= CLONE_NEWUSER;
+
+		if (needs_mntns & CONNECT_NEEDS_MNTNS)
+			setns_flags |= CLONE_NEWNS;
+
+		if (!change_namespaces(connect_pidfd, connect_nsfd, setns_flags)) {
+			fprintf(stderr, "Error: %m - Failed setns to connector namespaces\n");
 			_exit(EXIT_FAILURE);
 		}
 
-		// Attach to the mount namespace of the connector
-		if ((needs_mntns & CONNECT_NEEDS_MNTNS) && !change_namespaces(connect_pidfd, connect_nsfd, CLONE_NEWNS)) {
-			fprintf(stderr, "Error: %m - Failed setns to connector mount namespace\n");
-			_exit(EXIT_FAILURE);
-		}
+		// Complete switch to the user namespace of the connector
+		if (setns_flags & CLONE_NEWUSER)
+			finalize_userns();
 
 		close_prot_errno_disarm(connect_nsfd);
 		close_prot_errno_disarm(connect_pidfd);

commit edd79a6d1c4ac3283b6816d795fe6d975e5af5e3

incusd/forkproxy: join the correct mntns for listen
This was a copy-paste error introduced in commit 4494bad12c57
("incusd/main_forkproxy: Join all namespaces at once"). The net result
was that container-binding proxy devices with a unix listener would be
in the wrong mount namespace and the unix socket would accidentally be
created on the host.

Fixes: 4494bad12c57 ("incusd/main_forkproxy: Join all namespaces at once")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

cmd/incusd/main_forkproxy.go
@@ -141,7 +141,7 @@ void forkproxy(void)
 		if (in_same_namespace(getpid(), listen_nsfd, "user") > 0)
 			setns_flags |= CLONE_NEWUSER;
 
-		if (needs_mntns & CONNECT_NEEDS_MNTNS)
+		if (needs_mntns & LISTEN_NEEDS_MNTNS)
 			setns_flags |= CLONE_NEWNS;
 
 		if (!change_namespaces(listen_pidfd, listen_nsfd, setns_flags)) {