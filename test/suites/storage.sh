ac6a6866b43e83aef80f136ad79da3ac003c37a9
Stuart Espey <stuart.espey@mactrix.com>
2025-06-02 07:24:37 +0000

    tests/truenas: delint: no trailing spaces in shell scripts
    Signed-off-by: Stuart Espey <stuart.espey@mactrix.com>

--- test/suites/storage.sh
+++ test/suites/storage.sh
@@ -892,7 +892,7 @@ test_storage() {
       incus storage create "$storage_pool" "$incus_backend" linstor.resource_group.place_count=1
   elif [ "${incus_backend}" = "truenas" ]; then
     incus storage create "$storage_pool" "$incus_backend" "$(truenas_source_uuid)" "$(truenas_api_key)"
-  else 
+  else
     incus storage create "$storage_pool" "$incus_backend"
   fi
   incus init -s "${storage_pool}" testimage c1
