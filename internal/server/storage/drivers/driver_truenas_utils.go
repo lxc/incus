6041a4bc8dcf2ef378d3151e53a49f644e5bcbed
Stuart Espey <stuart.espey@mactrix.com>
2025-06-05 11:40:29 +0000

    incusd/storage/truenas: work-around of iscsiadm race condition
    when stopping/deleteing/renaming an instance its possible for it to be deactivated in close sucession
    
    iscsiadm has a TOCTOU when checking if a node needs to be deactivated.
    
    Signed-off-by: Stuart Espey <stuart.espey@mactrix.com>

--- internal/server/storage/drivers/driver_truenas_utils.go
+++ internal/server/storage/drivers/driver_truenas_utils.go
@@ -503,6 +503,9 @@ func (d *truenas) activateIscsiDataset(dataset string) (string, error) {
 func (d *truenas) deactivateIscsiDatasetIfActive(dataset string) (bool, error) {
 	statusPath, err := d.runIscsiCmd("locate", "--deactivate", "--parsable", dataset)
 	if err != nil {
+		if strings.Contains(err.Error(), "iscsiadm: Could not stat") {
+			return false, nil // this error seems to be a race in iscsiadm. it basically means its alrady deactivate.
+		}
 		return false, err
 	}
 
