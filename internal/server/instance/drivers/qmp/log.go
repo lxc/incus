b005faa3e49e4b1400d9bf490239840e3ccedc23
JUN JIE NAN <nanjunjie@gmail.com>
2025-06-07 12:21:12 +0800

    incusd/instance/qmp add qmp log implementation
    Signed-off-by: JUN JIE NAN <nanjunjie@gmail.com>

---
+++ internal/server/instance/drivers/qmp/log.go
@@ -0,0 +1,61 @@
+package qmp
+
+import (
+	"os"
+	"sync"
+)
+
+type qmpLog struct {
+	logFile string
+	log     *os.File
+	mu      sync.Mutex
+}
+
+func newQmpLog(logFile string) (*qmpLog, error) {
+	ql := &qmpLog{
+		logFile: logFile,
+	}
+
+	err := ql.open()
+	if err != nil {
+		return nil, err
+	}
+
+	return ql, nil
+}
+
+func (ql *qmpLog) open() error {
+	if ql.log == nil {
+		ql.mu.Lock()
+		defer ql.mu.Unlock()
+		log, err := os.OpenFile(ql.logFile,
+			os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+		if err != nil {
+			return err
+		}
+
+		ql.log = log
+	}
+
+	return nil
+}
+
+// Write writes len(b) bytes from b to the channel.
+func (ql *qmpLog) Write(p []byte) (n int, err error) {
+	ql.mu.Lock()
+	defer ql.mu.Unlock()
+	return ql.log.Write(p)
+}
+
+// Close closes the log and wait the channel clean.
+func (ql *qmpLog) Close() error {
+	if ql.log != nil {
+		ql.mu.Lock()
+		defer ql.mu.Unlock()
+		err := ql.log.Close()
+		ql.log = nil
+		return err
+	}
+
+	return nil
+}
