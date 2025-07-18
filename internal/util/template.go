122dab49103917eb3e492db40f1ddf126e73384a
Stéphane Graber <stgraber@stgraber.org>
2025-07-18 02:30:50 +0200

    internal/util: Tweak common pongo2 parser to block dangerous functions
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/util/template.go
+++ internal/util/template.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/flosch/pongo2/v6"
@@ -10,10 +11,21 @@ import (
 // RenderTemplate renders a pongo2 template with nesting support.
 // This supports up to 3 levels of nesting (to avoid loops).
 func RenderTemplate(template string, ctx pongo2.Context) (string, error) {
+	// Prepare a custom set.
+	custom := pongo2.NewSet("render-template", pongo2.DefaultLoader)
+
+	// Block the use of some tags.
+	for _, tag := range []string{"extends", "import", "include", "ssi"} {
+		err := custom.BanTag(tag)
+		if err != nil {
+			return "", fmt.Errorf("Failed to configure custom pongo2 parser: Failed to block tag tag %q: %w", tag, err)
+		}
+	}
+
 	// Limit recursion to 3 levels.
 	for range 3 {
 		// Load template from string
-		tpl, err := pongo2.FromString("{% autoescape off %}" + template + "{% endautoescape %}")
+		tpl, err := custom.FromString("{% autoescape off %}" + template + "{% endautoescape %}")
 		if err != nil {
 			return "", err
 		}
