7e1186ab368257c0bdeb0465c9c693596f04e12f
Stéphane Graber <stgraber@stgraber.org>
2025-07-18 02:27:25 +0200

    internal/util: Add recursion limit to RenderTemplate
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/util/template.go
+++ internal/util/template.go
@@ -1,29 +1,37 @@
 package util
 
 import (
+	"errors"
 	"strings"
 
 	"github.com/flosch/pongo2/v6"
 )
 
-// RenderTemplate renders a pongo2 template.
+// RenderTemplate renders a pongo2 template with nesting support.
+// This supports up to 3 levels of nesting (to avoid loops).
 func RenderTemplate(template string, ctx pongo2.Context) (string, error) {
-	// Load template from string
-	tpl, err := pongo2.FromString("{% autoescape off %}" + template + "{% endautoescape %}")
-	if err != nil {
-		return "", err
+	// Limit recursion to 3 levels.
+	for range 3 {
+		// Load template from string
+		tpl, err := pongo2.FromString("{% autoescape off %}" + template + "{% endautoescape %}")
+		if err != nil {
+			return "", err
+		}
+
+		// Get rendered template
+		ret, err := tpl.Execute(ctx)
+		if err != nil {
+			return ret, err
+		}
+
+		// Check if another pass is needed.
+		if !strings.Contains(ret, "{{") && !strings.Contains(ret, "{%") {
+			return ret, nil
+		}
+
+		// Prepare for another run.
+		template = ret
 	}
 
-	// Get rendered template
-	ret, err := tpl.Execute(ctx)
-	if err != nil {
-		return ret, err
-	}
-
-	// Looks like we're nesting templates so run pongo again
-	if strings.Contains(ret, "{{") || strings.Contains(ret, "{%") {
-		return RenderTemplate(ret, ctx)
-	}
-
-	return ret, err
+	return "", errors.New("Maximum template recursion limit reached")
 }
