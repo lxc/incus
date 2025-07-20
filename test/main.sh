cbf311afd4688d1a270c18cc0b16a98c3b168cde
karo <karolin.kostial@gmail.com>
2025-07-20 13:45:22 +0200

    tests: only run tests if ovn is available
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- test/main.sh
+++ test/main.sh
@@ -319,6 +319,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
     run_test test_filemanip "file manipulations"
     run_test test_network "network management"
     run_test test_network_dhcp_routes "network dhcp routes"
+    run_test test_network_peers "network peers"
     run_test test_network_acl "network ACL management"
     run_test test_network_forward "network address forwards"
     run_test test_network_zone "network DNS zones"
