533954ca5de799361d472145fbce3e359643d3ad
Angelos Kolaitis <neoaggelos@gmail.com>
2025-09-06 20:58:54 +0300

    client/oci: Allow OCI image names with a pinned hash
    Enables specifying OCI images as `IMAGE:TAG@HASH`
    
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- client/oci_images.go
+++ client/oci_images.go
@@ -395,6 +395,12 @@ func (r *ProtocolOCI) runSkopeo(action string, image string, args ...string) (st
 
 // GetImageAlias returns an existing alias as an ImageAliasesEntry struct.
 func (r *ProtocolOCI) GetImageAlias(name string) (*api.ImageAliasesEntry, string, error) {
+	// If image name is "IMAGE:TAG@HASH", drop ":TAG" so that skopeo uses the pinned hash instead.
+	if image, hash, ok := strings.Cut(name, "@"); ok {
+		imageWithoutTag, _, _ := strings.Cut(image, ":")
+		name = fmt.Sprintf("%s@%s", imageWithoutTag, hash)
+	}
+
 	// Get the image information from skopeo.
 	stdout, err := r.runSkopeo("inspect", name)
 	if err != nil {
