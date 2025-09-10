0ff38d7eacf81664df47f1bff603387f8d481508
AJRepo <AJRepo@users.noreply.github.com>
2025-09-09 23:23:58 -0500

    gci write linting fix
    Signed-off-by: AJRepo <AJRepo@users.noreply.github.com>

--- cmd/incus/export.go
+++ cmd/incus/export.go
@@ -8,12 +8,13 @@ import (
 	"path"
 	"time"
 
+	"github.com/spf13/cobra"
+
 	incus "github.com/lxc/incus/v6/client"
 	cli "github.com/lxc/incus/v6/internal/cmd"
 	"github.com/lxc/incus/v6/internal/i18n"
 	"github.com/lxc/incus/v6/shared/api"
 	"github.com/lxc/incus/v6/shared/archive"
-	"github.com/spf13/cobra"
 )
 
 type cmdExport struct {
