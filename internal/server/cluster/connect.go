3ad0773fc2ec4a132de4a5e6721e0066dc4aad9d
Stéphane Graber <stgraber@stgraber.org>
2025-09-25 20:01:52 -0400

    incusd/cluster: Make use of updated tlsTransport
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/cluster/connect.go
+++ internal/server/cluster/connect.go
@@ -2,7 +2,6 @@ package cluster
 
 import (
 	"context"
-	"crypto/tls"
 	"errors"
 	"fmt"
 	"net"
@@ -295,6 +294,7 @@ func UpdateTrust(serverCert *localtls.CertInfo, serverName string, targetAddress
 
 // HasConnectivity probes the member with the given address for connectivity.
 func HasConnectivity(networkCert *localtls.CertInfo, serverCert *localtls.CertInfo, address string, apiCheck bool) bool {
+	// Check if the main server endpoint is functional.
 	if apiCheck {
 		c, err := Connect(address, networkCert, serverCert, nil, true)
 		if err != nil {
@@ -305,14 +305,19 @@ func HasConnectivity(networkCert *localtls.CertInfo, serverCert *localtls.CertIn
 		return err == nil
 	}
 
-	config, err := tlsClientConfig(networkCert, serverCert)
+	// Get the transport.
+	transport, cleanup, err := tlsTransport(networkCert, serverCert)
 	if err != nil {
 		return false
 	}
 
+	defer cleanup()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
 	var conn net.Conn
-	dialer := &net.Dialer{Timeout: time.Second}
-	conn, err = tls.DialWithDialer(dialer, "tcp", address, config)
+	conn, err = transport.DialTLSContext(ctx, "tcp", address)
 	if err == nil {
 		_ = conn.Close()
 		return true
