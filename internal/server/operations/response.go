b118c800c5d8ee40f0133e6c45c91385a2cd9485
Stéphane Graber <stgraber@stgraber.org>
2025-10-28 09:48:43 -0400

    incusd: Remove remaining project remnants from operations
    Follow-up on https://github.com/lxc/incus/pull/2588
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/operations/response.go
+++ internal/server/operations/response.go
@@ -70,16 +70,14 @@ func (r *operationResponse) Code() int {
 //
 // Returned when the operation has been created on another node.
 type forwardedOperationResponse struct {
-	op      *api.Operation
-	project string
+	op *api.Operation
 }
 
 // ForwardedOperationResponse creates a response that forwards the metadata of
 // an operation created on another node.
-func ForwardedOperationResponse(project string, op *api.Operation) response.Response {
+func ForwardedOperationResponse(op *api.Operation) response.Response {
 	return &forwardedOperationResponse{
-		op:      op,
-		project: project,
+		op: op,
 	}
 }
 
