c5dbb01330ee3a757320015e61e1e42e747ff79a
Denys Mosiiuk <dmos@dector.space>
2025-11-10 13:43:59 +0100

    incus/instance: Add missing godocs
    Adding missing docs for exported symbols inside `internal/instance` package.
    
    Signed-off-by: Denys Mosiiuk <dmos@dector.space>

--- internal/instance/snapshot.go
+++ internal/instance/snapshot.go
@@ -4,8 +4,10 @@ import (
 	"strings"
 )
 
+// SnapshotDelimiter is used to separate instance name from snapshot name.
 const SnapshotDelimiter = "/"
 
+// IsSnapshot checks if provided name is a snapshot name.
 func IsSnapshot(name string) bool {
 	return strings.Contains(name, SnapshotDelimiter)
 }
