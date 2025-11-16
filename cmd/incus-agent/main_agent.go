599a1734f534164f53af38ac98f9ff118d2ee03d
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2025-11-16 09:13:20 +0900

    cmd/incus-agent: address os.Exit being called outside of main and init
    Signed-off-by: Olivier Lechevalier <olivier.lechevalier@gmail.com>

--- cmd/incus-agent/main_agent.go
+++ cmd/incus-agent/main_agent.go
@@ -49,7 +49,7 @@ func (c *cmdAgent) Run(cmd *cobra.Command, args []string) error {
 	// Setup logger.
 	err := logger.InitLogger("", "", c.global.flagLogVerbose, c.global.flagLogDebug, nil)
 	if err != nil {
-		os.Exit(1)
+		return fmt.Errorf("could not initialize logger: %w", err)
 	}
 
 	logger.Info("Starting")
@@ -174,19 +174,17 @@ func (c *cmdAgent) Run(cmd *cobra.Command, args []string) error {
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, osShutdownSignal)
 
-	exitStatus := 0
-
 	select {
 	case <-chSignal:
 	case err := <-errChan:
-		fmt.Fprintln(os.Stderr, err)
-		exitStatus = 1
+		cancelStatusNotifier() // Ensure STOPPED status is written to QEMU status ringbuffer.
+		cancelFunc()
+
+		return err
 	}
 
 	cancelStatusNotifier() // Ensure STOPPED status is written to QEMU status ringbuffer.
 	cancelFunc()
-
-	os.Exit(exitStatus)
 
 	return nil
 }
