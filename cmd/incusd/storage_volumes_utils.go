d777c7b8802b9b24dcc4ab826c130d87daa07985
Helder Ardachnikoff Gomes <helder.versatti@gmail.com>
2025-12-01 22:28:04 +0000

    cmd/incusd: remove snapshot from instance args
    Signed-off-by: Helder Ardachnikoff Gomes <helder.versatti@gmail.com>

--- cmd/incusd/storage_volumes_utils.go
+++ cmd/incusd/storage_volumes_utils.go
@@ -45,7 +45,6 @@ func storagePoolVolumeUpdateUsers(ctx context.Context, s *state.State, projectNa
 			Profiles:     inst.Profiles(),
 			Project:      inst.Project().Name,
 			Type:         inst.Type(),
-			Snapshot:     inst.IsSnapshot(),
 		}
 
 		err = inst.Update(args, false)
