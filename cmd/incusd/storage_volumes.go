215b366cdd3457660b419ebadb966182a7f0c336
Helder Gomes <helder.gomes@luizalabs.com>
2026-01-28 16:03:51 -0300

    internal/server: change if to switch case due to static-check
    Signed-off-by: Helder Ardachnikoff Gomes <helder.versatti@gmail.com>

--- cmd/incusd/storage_volumes.go
+++ cmd/incusd/storage_volumes.go
@@ -2010,12 +2010,23 @@ func getVolumeFull(ctx context.Context, s *state.State, poolName string, vol api
 	// Add the state.
 	var usage *storagePools.VolumeUsage
 
-	if volType == db.StoragePoolVolumeTypeCustom {
+	switch volType {
+	case db.StoragePoolVolumeTypeCustom:
 		usage, err = pool.GetCustomVolumeUsage(vol.Project, vol.Name)
 		if err != nil && !errors.Is(err, storageDrivers.ErrNotSupported) {
 			return nil, err
 		}
-	} else if volType == db.StoragePoolVolumeTypeContainer || volType == db.StoragePoolVolumeTypeVM {
+	case db.StoragePoolVolumeTypeContainer:
+		inst, err := instance.LoadByProjectAndName(s, vol.Project, vol.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		usage, err = pool.GetInstanceUsage(inst)
+		if err != nil && !errors.Is(err, storageDrivers.ErrNotSupported) {
+			return nil, err
+		}
+	case db.StoragePoolVolumeTypeVM:
 		inst, err := instance.LoadByProjectAndName(s, vol.Project, vol.Name)
 		if err != nil {
 			return nil, err
