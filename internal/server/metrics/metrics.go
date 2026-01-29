5c1d31243effe253c9e83671b87eb2fe374bd4f2
Thorsten Lusser <tlusser@gmail.com>
2026-01-29 14:27:31 +0100

    feat: Implement incus_node_boot_time_seconds and incus_node_time_seconds in metrics
    Signed-off-by: Thorsten Lusser <tlusser@gmail.com>

--- internal/server/metrics/metrics.go
+++ internal/server/metrics/metrics.go
@@ -298,5 +298,9 @@ func MetricSetFromAPI(metrics *Metrics, labels map[string]string) (*MetricSet, e
 	// Procs stats
 	set.AddSamples(ProcsTotal, Sample{Value: float64(metrics.ProcessesTotal)})
 
+	// NodeBootTimeSeconds & NodeTimeSeconds
+	set.AddSamples(NodeBootTimeSeconds, Sample{Value: float64(metrics.NodeBootTimeSeconds)})
+	set.AddSamples(NodeTimeSeconds, Sample{Value: float64(metrics.NodeTimeSeconds)})
+
 	return set, nil
 }
