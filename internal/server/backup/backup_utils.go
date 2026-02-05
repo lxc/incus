2a45853371f02357f25002d399a0453bc4f0b6a0
Benjamin Somers <benjamin.somers@imt-atlantique.fr>
2026-02-05 19:13:15 +0000

    incusd/backup: Refactor S3 upload
    Signed-off-by: Benjamin Somers <benjamin.somers@imt-atlantique.fr>

--- internal/server/backup/backup_utils.go
+++ internal/server/backup/backup_utils.go
@@ -3,10 +3,19 @@ package backup
 import (
 	"archive/tar"
 	"context"
+	"crypto/tls"
 	"errors"
+	"fmt"
 	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 
 	"github.com/lxc/incus/v6/internal/server/sys"
+	"github.com/lxc/incus/v6/shared/api"
 	"github.com/lxc/incus/v6/shared/archive"
 )
 
@@ -32,4 +41,50 @@ func TarReader(r io.ReadSeeker, sysOS *sys.OS, outputPath string) (*tar.Reader, 
 	}
 
 	return tr, cancelFunc, nil
+}
+
+// Upload handles backup uploads.
+func Upload(reader *io.PipeReader, req *api.BackupTarget) error {
+	// We want to close the reader as soon as something bad occurs, ensuring that we don't hang on a
+	// pipe that's unable to consume anything.
+	defer func() { _ = reader.Close() }()
+
+	if req.Protocol != "s3" {
+		return fmt.Errorf("Unsupported backup target protocol %q", req.Protocol)
+	}
+
+	// Set up an S3 client.
+	uri, err := url.Parse(req.URL)
+	if err != nil {
+		return err
+	}
+
+	creds := credentials.NewStaticV4(req.AccessKey, req.SecretKey, "")
+
+	ts := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    30 * time.Second,
+		DisableCompression: true,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         tls.VersionTLS12,
+		},
+	}
+
+	client, err := minio.New(uri.Host, &minio.Options{
+		BucketLookup: minio.BucketLookupPath,
+		Creds:        creds,
+		Secure:       uri.Scheme == "https",
+		Transport:    ts,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = client.PutObject(context.Background(), req.BucketName, req.Path, reader, -1, minio.PutObjectOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
