a3ed8b7a9fa25ba0234f1880800981764e96424d
Piotr Resztak <piotr.resztak@futurfusion.io>
2026-02-23 11:33:00 +0100

    incusd/instance/drivers: Support detaching qcow2 custom volumes
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- internal/server/instance/drivers/driver_qemu.go
+++ internal/server/instance/drivers/driver_qemu.go
@@ -2642,9 +2642,17 @@ func (d *qemu) deviceDetachBlockDevice(deviceName string, rawConfig deviceConfig
 	deviceID := fmt.Sprintf("%s%s", qemuDeviceIDPrefix, escapedDeviceName)
 	blockDevName := d.blockNodeName(escapedDeviceName)
 
-	err = monitor.RemoveFDFromFDSet(blockDevName)
+	blockDevs, err := d.fetchBlockDeviceChain(monitor, blockDevName)
 	if err != nil {
 		return err
+	}
+
+	for i := len(blockDevs); i > 0; i-- {
+		blockDev := blockDevs[i-1]
+		err = monitor.RemoveFDFromFDSet(blockDev)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = monitor.RemoveDevice(deviceID)
@@ -2660,10 +2668,22 @@ func (d *qemu) deviceDetachBlockDevice(deviceName string, rawConfig deviceConfig
 		}
 	}
 
+	for i := len(blockDevs); i > 0; i-- {
+		blockDev := blockDevs[i-1]
+		err = d.detachBlockDeviceAndWait(monitor, blockDev)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (d *qemu) detachBlockDeviceAndWait(m *qmp.Monitor, blockDevName string) error {
 	waitDuration := time.Duration(time.Second * time.Duration(10))
 	waitUntil := time.Now().Add(waitDuration)
 	for {
-		err = monitor.RemoveBlockDevice(blockDevName)
+		err := m.RemoveBlockDevice(blockDevName)
 		if err == nil {
 			break
 		}
@@ -7648,7 +7668,7 @@ func (d *qemu) migrateSendLive(ctx context.Context, pool storagePools.Pool, clus
 	}
 
 	// Selects all block devices related to this instance (backing, root disk, overlays).
-	blockDevs, err := d.fetchQcow2Blockdevs(monitor)
+	blockDevs, err := d.fetchRootBlockDeviceChain(monitor)
 	if err != nil {
 		return err
 	}
@@ -10596,7 +10616,7 @@ func (d *qemu) CreateQcow2Snapshot(snapshotName string, backingFilename string) 
 	defer func() { _ = f.Close() }()
 
 	// Select all block devices related to a qcow2 backing chain.
-	blockDevs, err := d.fetchQcow2Blockdevs(monitor)
+	blockDevs, err := d.fetchRootBlockDeviceChain(monitor)
 	if err != nil {
 		return err
 	}
@@ -10651,14 +10671,21 @@ func (d *qemu) CreateQcow2Snapshot(snapshotName string, backingFilename string) 
 	return nil
 }
 
-// fetchQcow2Blockdevs selects block devices related to a qcow2 backing chain.
-func (d *qemu) fetchQcow2Blockdevs(m *qmp.Monitor) ([]string, error) {
+// fetchBlockDeviceChain returns the ordered list of block device names
+// required to load a volume, including any dependent layers.
+func (d *qemu) fetchBlockDeviceChain(m *qmp.Monitor, blockDevName string) ([]string, error) {
 	// Fetch information about block devices.
 	blockdevNames, err := m.QueryNamedBlockNodes()
 	if err != nil {
 		return nil, fmt.Errorf("Failed fetching block nodes names: %w", err)
 	}
 
+	return filterAndSortQcow2Blockdevs(blockdevNames, blockDevName), nil
+}
+
+// fetchRootBlockDeviceChain returns the ordered list of block device names
+// required to load the root disk device, including any dependent layers.
+func (d *qemu) fetchRootBlockDeviceChain(m *qmp.Monitor) ([]string, error) {
 	rootDevName, _, err := internalInstance.GetRootDiskDevice(d.expandedDevices.CloneNative())
 	if err != nil {
 		return nil, fmt.Errorf("Failed getting instance root disk: %w", err)
@@ -10667,7 +10694,7 @@ func (d *qemu) fetchQcow2Blockdevs(m *qmp.Monitor) ([]string, error) {
 	escapedDeviceName := linux.PathNameEncode(rootDevName)
 	rootNodeName := d.blockNodeName(escapedDeviceName)
 
-	return filterAndSortQcow2Blockdevs(blockdevNames, rootNodeName), nil
+	return d.fetchBlockDeviceChain(m, rootNodeName)
 }
 
 // DeleteQcow2Snapshot deletes a qcow2 snapshot for a running instance.
@@ -10678,7 +10705,7 @@ func (d *qemu) DeleteQcow2Snapshot(snapshotIndex int, backingFilename string) er
 	}
 
 	// Select all block devices related to a qcow2 backing chain.
-	blockDevs, err := d.fetchQcow2Blockdevs(monitor)
+	blockDevs, err := d.fetchRootBlockDeviceChain(monitor)
 	if err != nil {
 		return err
 	}
@@ -10762,7 +10789,7 @@ func (d *qemu) ExportQcow2Block(blockIndex int) (func(), string, error) {
 	}
 
 	// Selects all block devices related to this instance (backing, root disk, overlays).
-	blockDevs, err := d.fetchQcow2Blockdevs(monitor)
+	blockDevs, err := d.fetchRootBlockDeviceChain(monitor)
 	if err != nil {
 		return nil, "", err
 	}
