2544af0f4dedfc1def952bd95488ab6a5ef93ca9
Stéphane Graber <stgraber@stgraber.org>
2026-03-04 22:17:47 -0500

    incusd/instance/debug: Implement instance repair API
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/incusd/instance_debug.go
+++ cmd/incusd/instance_debug.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"os"
+	"slices"
 
 	"github.com/gorilla/mux"
 
 	internalInstance "github.com/lxc/incus/v6/internal/instance"
 	"github.com/lxc/incus/v6/internal/server/instance"
 	"github.com/lxc/incus/v6/internal/server/instance/instancetype"
+	"github.com/lxc/incus/v6/internal/server/operations"
+	"github.com/lxc/incus/v6/internal/server/project"
 	"github.com/lxc/incus/v6/internal/server/request"
 	"github.com/lxc/incus/v6/internal/server/response"
+	"github.com/lxc/incus/v6/internal/server/state"
+	storagePools "github.com/lxc/incus/v6/internal/server/storage"
+	storageDrivers "github.com/lxc/incus/v6/internal/server/storage/drivers"
+	"github.com/lxc/incus/v6/shared/api"
+	"github.com/lxc/incus/v6/shared/subprocess"
 )
 
 // swagger:operation GET /1.0/instances/{name}/debug/memory instances instance_debug_memory_get
@@ -130,4 +140,154 @@ func instanceDebugMemoryGet(d *Daemon, r *http.Request) response.Response {
 
 		return nil
 	})
+}
+
+// swagger:operation GET /1.0/instances/{name}/debug/repair instances instance_debug_repair_post
+//
+//	Trigger a repair action on the instance.
+//
+//	Runs an internal repair action on the instance.
+//
+//	---
+//	parameters:
+//	  - in: query
+//	    name: project
+//	    description: Project name
+//	    type: string
+//	    example: default
+//	  - in: body
+//	    name: state
+//	    description: State
+//	    required: false
+//	    schema:
+//	      $ref: "#/definitions/InstanceDebugRepairPost"
+//	responses:
+//	  "200":
+//	    $ref: "#/responses/EmptySyncResponse"
+//	  "400":
+//	    $ref: "#/responses/BadRequest"
+//	  "403":
+//	    $ref: "#/responses/Forbidden"
+//	  "404":
+//	    $ref: "#/responses/NotFound"
+//	  "500":
+//	    $ref: "#/responses/InternalServerError"
+func instanceDebugRepairPost(d *Daemon, r *http.Request) response.Response {
+	s := d.State()
+
+	projectName := request.ProjectParam(r)
+	name, err := url.PathUnescape(mux.Vars(r)["name"])
+	if err != nil {
+		return response.SmartError(err)
+	}
+
+	if internalInstance.IsSnapshot(name) {
+		return response.BadRequest(errors.New("Invalid instance name"))
+	}
+
+	// Handle requests targeted to an instance on a different node
+	resp, err := forwardedResponseIfInstanceIsRemote(s, r, projectName, name)
+	if err != nil {
+		return response.SmartError(err)
+	}
+
+	if resp != nil {
+		return resp
+	}
+
+	// Parse the request.
+	req := api.InstanceDebugRepairPost{}
+
+	err = json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return response.BadRequest(err)
+	}
+
+	// Validate the repair action.
+	if !slices.Contains([]string{"rebuild-config-volume"}, req.Action) {
+		return response.BadRequest(fmt.Errorf("Invalid repair action %q", req.Action))
+	}
+
+	// Load the instance.
+	inst, err := instance.LoadByProjectAndName(s, projectName, name)
+	if err != nil {
+		return response.SmartError(err)
+	}
+
+	// Running the action.
+	switch req.Action {
+	case "rebuild-config-volume":
+		err := instanceDebugRepairRebuildConfigVolume(s, inst)
+		if err != nil {
+			return response.SmartError(err)
+		}
+	}
+
+	return response.EmptySyncResponse
+}
+
+func instanceDebugRepairRebuildConfigVolume(s *state.State, inst instance.Instance) error {
+	// Initial validation.
+	if inst.Type() != instancetype.VM {
+		return errors.New("Config volume rebuild is only possible on VMs")
+	}
+
+	if inst.IsRunning() {
+		return errors.New("Config volume rebuild is only possible on stopped VMs")
+	}
+
+	// Load the storage pool.
+	pool, err := storagePools.LoadByInstance(s, inst)
+	if err != nil {
+		return err
+	}
+
+	// Load the volume.
+	dbVol, err := storagePools.VolumeDBGet(pool, inst.Project().Name, inst.Name(), storageDrivers.VolumeTypeVM)
+	if err != nil {
+		return err
+	}
+
+	if dbVol.Config["block.type"] != "qcow2" {
+		return errors.New("Config volume rebuild is only possible on QCOW2 backed VMs")
+	}
+
+	volStorageName := project.Instance(inst.Project().Name, inst.Name())
+	vol := pool.GetVolume(storageDrivers.VolumeTypeVM, storageDrivers.ContentTypeFS, volStorageName, dbVol.Config)
+
+	// Re-create the filesystem.
+	err = pool.Driver().ActivateTask(vol, func(devPath string, op *operations.Operation) error {
+		_, err = subprocess.RunCommand("mkfs.btrfs", "-f", devPath)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	// Re-configure the sub-volumes.
+	err = storageDrivers.Qcow2CreateConfig(vol, nil)
+	if err != nil {
+		return err
+	}
+
+	snaps, err := inst.Snapshots()
+	if err != nil {
+		return err
+	}
+
+	for _, snap := range snaps {
+		snapVolStorageName := project.Instance(snap.Project().Name, snap.Name())
+		snapVol := pool.GetVolume(storageDrivers.VolumeTypeVM, storageDrivers.ContentTypeFS, snapVolStorageName, dbVol.Config)
+
+		err = storageDrivers.Qcow2CreateConfigSnapshot(vol, snapVol, nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
