fb4f305586a95ae739d29487f9282bb7f08fda4e
Piotr Resztak <piotr.resztak@futurfusion.io>
2026-03-09 23:12:05 +0100

    incusd/backup/config: Add DependentVolumes field to Config struct
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- internal/server/backup/config/backup_config.go
+++ internal/server/backup/config/backup_config.go
@@ -6,12 +6,13 @@ import (
 
 // Config represents the config of a backup that can be stored in a backup.yaml file (or embedded in index.yaml).
 type Config struct {
-	Container       *api.Instance                `yaml:"container,omitempty"` // Used by VM backups too.
-	Snapshots       []*api.InstanceSnapshot      `yaml:"snapshots,omitempty"`
-	Pool            *api.StoragePool             `yaml:"pool,omitempty"`
-	Profiles        []*api.Profile               `yaml:"profiles,omitempty"`
-	Volume          *api.StorageVolume           `yaml:"volume,omitempty"`
-	VolumeSnapshots []*api.StorageVolumeSnapshot `yaml:"volume_snapshots,omitempty"`
-	Bucket          *api.StorageBucket           `yaml:"bucket,omitempty"`
-	BucketKeys      []*api.StorageBucketKey      `yaml:"bucket_keys,omitempty"`
+	Container        *api.Instance                `yaml:"container,omitempty"` // Used by VM backups too.
+	Snapshots        []*api.InstanceSnapshot      `yaml:"snapshots,omitempty"`
+	Pool             *api.StoragePool             `yaml:"pool,omitempty"`
+	Profiles         []*api.Profile               `yaml:"profiles,omitempty"`
+	Volume           *api.StorageVolume           `yaml:"volume,omitempty"`
+	VolumeSnapshots  []*api.StorageVolumeSnapshot `yaml:"volume_snapshots,omitempty"`
+	DependentVolumes []*Config                    `yaml:"dependent_volumes,omitempty"`
+	Bucket           *api.StorageBucket           `yaml:"bucket,omitempty"`
+	BucketKeys       []*api.StorageBucketKey      `yaml:"bucket_keys,omitempty"`
 }
