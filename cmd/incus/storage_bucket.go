fa2e5f2c961f2fe79149d5fe3edbe0e3f68a386f
kazan417 <kazan417@mail.ru>
2026-04-30 20:29:59 +0700

    Update storage_bucket.go
    fix typo
    
    Signed-off-by: kazan417 <kazan417@mail.ru>

--- cmd/incus/storage_bucket.go
+++ cmd/incus/storage_bucket.go
@@ -840,7 +840,7 @@ Default column layout: ndr
 
 == Columns ==
 The -c option takes a comma separated list of arguments that control
-which network zone attributes to output when displaying in table or csv
+which storage bucket keys attributes to output when displaying in table or csv
 format.
 
 Column arguments are either pre-defined shorthand chars (see below),
