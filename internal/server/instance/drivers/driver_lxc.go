019eb3f596b1be0846baddd974b0340219b20896
Stéphane Graber <stgraber@stgraber.org>
2026-05-25 03:49:54 +0000

    incusd/instance/drivers: Remove unnecessary blank line at end of block (whitespace)
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/instance/drivers/driver_lxc.go
+++ internal/server/instance/drivers/driver_lxc.go
@@ -5197,7 +5197,6 @@ func (d *lxc) Update(args db.InstanceArgs, userRequested bool) error {
 						}
 					}
 				}
-
 			} else if key == "limits.cpu" || key == "limits.cpu.nodes" {
 				// Clear the "volatile.cpu.nodes" if needed.
 				d.ClearLimitsCPUNodes(changedConfig)
