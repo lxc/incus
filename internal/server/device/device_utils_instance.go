d9d69e3e98746a6372ce650dbbc41ad8485f7213
Stéphane Graber <stgraber@stgraber.org>
2026-06-23 21:11:13 -0400

    incusd/device: Apply OCI static network configuration on NICs
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/device/device_utils_instance.go
+++ internal/server/device/device_utils_instance.go
@@ -1,9 +1,12 @@
 package device
 
 import (
+	"path/filepath"
 	"slices"
 
+	"github.com/lxc/incus/v7/internal/server/instance"
 	"github.com/lxc/incus/v7/internal/server/instance/instancetype"
+	"github.com/lxc/incus/v7/shared/util"
 )
 
 // instanceSupported is a helper function to check instance type is supported for validation.
@@ -15,4 +18,18 @@ func instanceSupported(instType instancetype.Type, supportedTypes ...instancetyp
 	}
 
 	return slices.Contains(supportedTypes, instType)
+}
+
+// instanceIsOCI reports whether the instance is an OCI application container.
+func instanceIsOCI(inst instance.Instance) bool {
+	if inst == nil {
+		return false
+	}
+
+	// Rely on the volatile marker when set, falling back to the on-disk OCI config in case it's missing.
+	if util.IsTrue(inst.ExpandedConfig()["volatile.container.oci"]) {
+		return true
+	}
+
+	return util.PathExists(filepath.Join(inst.Path(), "config.json"))
 }
