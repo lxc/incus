a02f89b3af42f3fd26a4cf6ab05ff496b4f007a5
Max Asnaashari <max.asna@futurfusion.io>
2026-06-24 17:17:04 -0700

    internal/server/instance/drivers: Add OS version specific QEMU options
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- internal/server/instance/drivers/driver_qemu_machine.go
+++ internal/server/instance/drivers/driver_qemu_machine.go
@@ -13,6 +13,7 @@ import (
 	"github.com/lxc/incus/v7/internal/linux"
 	"github.com/lxc/incus/v7/internal/server/db"
 	dbCluster "github.com/lxc/incus/v7/internal/server/db/cluster"
+	"github.com/lxc/incus/v7/internal/server/instance/drivers/cfg"
 	"github.com/lxc/incus/v7/internal/server/instance/drivers/qemudefault"
 	"github.com/lxc/incus/v7/internal/server/instance/instancetype"
 	"github.com/lxc/incus/v7/shared/osarch"
@@ -290,6 +291,11 @@ func (d *qemu) cpuType(bs *qemuBootState) (string, error) {
 		cpuExtensions = append(cpuExtensions, "migratable=no", "+invtsc")
 	}
 
+	// Some older OSes will kernel panic with SMAP enabled, so explicitly disable it if we detect those versions.
+	osType, distro, version := osinfo.DetermineOSDetails(d.expandedConfig["image.os"], d.expandedConfig["image.release"])
+	_, _, disableFlags := osinfo.GetOSQemuCompatibility(osType, distro, version)
+	cpuExtensions = append(cpuExtensions, disableFlags...)
+
 	if len(cpuExtensions) > 0 {
 		cpuType += "," + strings.Join(cpuExtensions, ",")
 	}
@@ -399,4 +405,52 @@ func (d *qemu) memoryTopology(bs *qemuBootState) (*qemuMemoryTopology, error) {
 	memInfo.Max = maxMemoryBytes
 
 	return memInfo, nil
+}
+
+func (d *qemu) osVersionSpecificOptions() []cfg.Section {
+	imageOS := d.expandedConfig["image.os"]
+	imageRelease := d.expandedConfig["image.release"]
+
+	osType, distro, version := osinfo.DetermineOSDetails(imageOS, imageRelease)
+
+	supportsVioSCSI, supportsModernVioNet, _ := osinfo.GetOSQemuCompatibility(osType, distro, version)
+
+	var conf []cfg.Section
+	if !supportsModernVioNet {
+		conf = append(conf, cfg.Section{
+			Name: "global",
+			Entries: map[string]string{
+				"driver":   "virtio-net-pci",
+				"property": "disable-legacy",
+				"value":    "off",
+			},
+		})
+	}
+
+	if !supportsVioSCSI {
+		conf = append(conf, cfg.Section{
+			Name: "global",
+			Entries: map[string]string{
+				"driver":   "virtio-blk-pci",
+				"property": "disable-legacy",
+				"value":    "off",
+			},
+		})
+	}
+
+	if osType == osinfo.Windows {
+		versionCode, _ := osinfo.MapWindowsVersionToAbbrev(version)
+		if versionCode == "2k3" || versionCode == "xp" {
+			conf = append(conf, cfg.Section{
+				Name: "global",
+				Entries: map[string]string{
+					"driver":   "q35-pcihost",
+					"property": "x-pci-hole64-fix",
+					"value":    "off",
+				},
+			})
+		}
+	}
+
+	return conf
 }
