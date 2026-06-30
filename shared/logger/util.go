31de4be43c17eafa6c9aa516f9f5a99cea1569c6
Stéphane Graber <stgraber@stgraber.org>
2026-06-30 09:19:14 -0400

    shared/logger: Add WarnOnErrorExcept helper
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/logger/util.go
+++ shared/logger/util.go
@@ -1,12 +1,28 @@
 package logger
 
+import (
+	"errors"
+)
+
 // WarnOnError calls the provided function and, if it returns an error, logs
 // that error as a warning together with the provided message and optional
 // context.
 func WarnOnError(f func() error, msg string, ctx ...Ctx) {
+	WarnOnErrorExcept(f, nil, msg, ctx...)
+}
+
+// WarnOnErrorExcept behaves like WarnOnError but doesn't warn for any error
+// matching (via errors.Is) one of the provided ignored errors.
+func WarnOnErrorExcept(f func() error, ignore []error, msg string, ctx ...Ctx) {
 	err := f()
 	if err == nil {
 		return
+	}
+
+	for _, target := range ignore {
+		if errors.Is(err, target) {
+			return
+		}
 	}
 
 	ctx = append(ctx, Ctx{"err": err})
