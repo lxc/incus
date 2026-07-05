719c09d87267228109f92e60769f0e4cc5faceb8
Stéphane Graber <stgraber@stgraber.org>
2026-07-04 20:01:38 -0400

    incusd: Limit websocket control message size
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/ws/upgrader.go
+++ shared/ws/upgrader.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// ControlMessageMaxSize is the maximum size of a websocket control message.
+const ControlMessageMaxSize = 1024 * 32
+
 // Upgrader is a websocket upgrader which ignores the request Origin.
 var Upgrader = websocket.Upgrader{
 	HandshakeTimeout: time.Second * 5,
