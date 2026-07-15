d155ba6a8a1ee405440bb613cdb7198f8bc8c94e
Leon Schoch <lschoch@anexia.com>
2026-07-15 13:50:31 +0200

    incus/server/storage/driver/ceph: Flatten image in background during instance creation
    Signed-off-by: Leon Schoch <lschoch@anexia.com>

--- internal/server/storage/drivers/driver_ceph_volumes.go
+++ internal/server/storage/drivers/driver_ceph_volumes.go
@@ -383,6 +383,17 @@ func (d *ceph) CreateVolumeFromCopy(vol Volume, srcVol Volume, copySnapshots boo
 			return err
 		}
 
+		if util.IsFalse(d.config["ceph.rbd.clone_copy"]) {
+			// If clone_copy is false, flatten image to detach it from source snapshot
+			// Missing the error on failure is acceptable, as instance creation can still continue
+			go func() {
+				err := d.flattenImage(context.TODO(), d.getRBDVolumeName(vol, "", true))
+				if err != nil {
+					logger.Warnf("Failed to flatten image %s: %v", d.getRBDVolumeName(vol, "", true), err)
+				}
+			}()
+		}
+
 		return nil
 	}
 
@@ -410,63 +421,39 @@ func (d *ceph) CreateVolumeFromCopy(vol Volume, srcVol Volume, copySnapshots boo
 
 	// Copy without snapshots.
 	if !copySnapshots || len(snapshots) == 0 {
-		// If lightweight clone mode isn't enabled, perform a full copy of the volume.
-		if util.IsFalse(d.config["ceph.rbd.clone_copy"]) {
-			_, err = subprocess.RunCommand(
-				"rbd",
-				"--id", d.config["ceph.user.name"],
-				"--cluster", d.config["ceph.cluster_name"],
-				"cp",
-				d.getRBDVolumeName(srcVol, "", true),
-				d.getRBDVolumeName(vol, "", true),
-			)
-			if err != nil {
-				return err
-			}
+		parentVol := srcVol
+		snapshotName := "readonly"
 
-			reverter.Add(func() { _ = d.DeleteVolume(vol, op) })
+		if srcVol.volType != VolumeTypeImage {
+			snapshotName = fmt.Sprintf("zombie_snapshot_%s", uuid.New().String())
 
-			_, err = d.rbdMapVolume(vol)
-			if err != nil {
-				return err
-			}
-
-			reverter.Add(func() { _ = d.rbdUnmapVolume(vol, true) })
-		} else {
-			parentVol := srcVol
-			snapshotName := "readonly"
-
-			if srcVol.volType != VolumeTypeImage {
-				snapshotName = fmt.Sprintf("zombie_snapshot_%s", uuid.New().String())
-
-				if srcVol.IsSnapshot() {
-					srcParentName, srcSnapOnlyName, _ := api.GetParentAndSnapshotName(srcVol.name)
-					snapshotName = fmt.Sprintf("snapshot_%s", srcSnapOnlyName)
-					parentVol = NewVolume(d, d.name, srcVol.volType, srcVol.contentType, srcParentName, nil, nil)
-				} else {
-					// Create snapshot.
-					err := d.rbdCreateVolumeSnapshot(srcVol, snapshotName)
-					if err != nil {
-						return err
-					}
-				}
-
-				// Protect volume so we can create clones of it.
-				err = d.rbdProtectVolumeSnapshot(parentVol, snapshotName)
+			if srcVol.IsSnapshot() {
+				srcParentName, srcSnapOnlyName, _ := api.GetParentAndSnapshotName(srcVol.name)
+				snapshotName = fmt.Sprintf("snapshot_%s", srcSnapOnlyName)
+				parentVol = NewVolume(d, d.name, srcVol.volType, srcVol.contentType, srcParentName, nil, nil)
+			} else {
+				// Create snapshot.
+				err := d.rbdCreateVolumeSnapshot(srcVol, snapshotName)
 				if err != nil {
 					return err
 				}
-
-				reverter.Add(func() { _ = d.rbdUnprotectVolumeSnapshot(parentVol, snapshotName) })
 			}
 
-			err = d.rbdCreateClone(parentVol, snapshotName, vol)
+			// Protect volume so we can create clones of it.
+			err = d.rbdProtectVolumeSnapshot(parentVol, snapshotName)
 			if err != nil {
 				return err
 			}
 
-			reverter.Add(func() { _ = d.DeleteVolume(vol, op) })
+			reverter.Add(func() { _ = d.rbdUnprotectVolumeSnapshot(parentVol, snapshotName) })
 		}
+
+		err = d.rbdCreateClone(parentVol, snapshotName, vol)
+		if err != nil {
+			return err
+		}
+
+		reverter.Add(func() { _ = d.DeleteVolume(vol, op) })
 
 		err = postCreateTasks(vol)
 		if err != nil {
