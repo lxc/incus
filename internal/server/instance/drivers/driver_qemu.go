2e2e2f3f6748c7c92afeed408627c848a077257b
Stéphane Graber <stgraber@stgraber.org>
2026-07-24 11:45:40 -0400

    incusd/instance/qemu: Confine template access to instance root
    The template traversal guard from CVE-2026-48752 was only applied to the
    LXC driver. Apply the same checks to the QEMU driver.
    
    This addresses GHSA-4qxq-p5hm-3q3p (CVE pending)
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/instance/drivers/driver_qemu.go
+++ internal/server/instance/drivers/driver_qemu.go
@@ -3479,6 +3479,29 @@ func (d *qemu) templateApplyNow(trigger instance.TemplateTrigger, path string) e
 				return nil
 			}
 
+			// Perform some early security checks on the template itself.
+			if filepath.Base(tpl.Template) != tpl.Template {
+				return errors.New("Template path is attempting to read outside of template directory")
+			}
+
+			tplDirStat, err := os.Lstat(d.TemplatesPath())
+			if err != nil {
+				return fmt.Errorf("Couldn't access template directory: %w", err)
+			}
+
+			if !tplDirStat.IsDir() {
+				return errors.New("Template directory isn't a regular directory")
+			}
+
+			tplFileStat, err := os.Lstat(filepath.Join(d.TemplatesPath(), tpl.Template))
+			if err != nil {
+				return fmt.Errorf("Couldn't access template file: %w", err)
+			}
+
+			if tplFileStat.Mode()&os.ModeSymlink == os.ModeSymlink {
+				return errors.New("Template file is a symlink")
+			}
+
 			// Create the file itself.
 			w, err = os.Create(filepath.Join(path, fmt.Sprintf("%s.out", tpl.Template)))
 			if err != nil {
