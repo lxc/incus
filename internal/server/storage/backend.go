dc00ebaadef92d3112313e36267efd38bc1b8ec6
Piotr Resztak <piotr.resztak@futurfusion.io>
2026-08-04 16:33:25 +0200

    incusd/storage: Add cleanupDependencies argument to DeleteInstanceSnapshot
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- internal/server/storage/backend.go
+++ internal/server/storage/backend.go
@@ -3681,7 +3681,7 @@ func (b *backend) RenameInstanceSnapshot(inst instance.Instance, newName string,
 }
 
 // DeleteInstanceSnapshot removes the snapshot volume for the supplied snapshot instance.
-func (b *backend) DeleteInstanceSnapshot(inst instance.Instance, op *operations.Operation) error {
+func (b *backend) DeleteInstanceSnapshot(inst instance.Instance, cleanupDependencies bool, op *operations.Operation) error {
 	l := b.logger.AddContext(logger.Ctx{"project": inst.Project().Name, "instance": inst.Name()})
 	l.Debug("DeleteInstanceSnapshot started")
 	defer l.Debug("DeleteInstanceSnapshot finished")
@@ -3779,6 +3779,10 @@ func (b *backend) DeleteInstanceSnapshot(inst instance.Instance, op *operations.
 		if err != nil {
 			return err
 		}
+	}
+
+	if !cleanupDependencies {
+		return nil
 	}
 
 	err = src.ForEachDependentDiskType(func(dev deviceConfig.DeviceNamed) error {
