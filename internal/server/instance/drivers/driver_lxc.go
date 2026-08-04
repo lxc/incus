72fe048beda95dc3d4a982be4c09321eb392aa18
René Jochum <rene@jochum.dev>
2026-08-03 22:24:34 -0400

    incusd/instance/lxc: Enable edns0 in OCI resolv.conf
    Signed-off-by: René Jochum <rene@jochum.dev>

--- internal/server/instance/drivers/driver_lxc.go
+++ internal/server/instance/drivers/driver_lxc.go
@@ -2696,6 +2696,8 @@ ff02::2 ip6-allrouters
 			fmt.Fprintf(&resolvConf, "domain %s\n", d.expandedConfig["oci.dns.domain"])
 		}
 
+		resolvConf.WriteString("options edns0\n")
+
 		err = instRoot.WriteFile("network/resolv.conf", []byte(resolvConf.String()), 0o644)
 		if err != nil {
 			return "", nil, err
