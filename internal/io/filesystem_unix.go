90d12822e398db152c0510e78cf2a33724b24905
Stéphane Graber <stgraber@stgraber.org>
2026-08-28 22:13:47 -0400

    internal/io: Add GetUmask and Lchtimes helpers
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/io/filesystem_unix.go
+++ internal/io/filesystem_unix.go
@@ -5,6 +5,9 @@ package io
 import (
 	"os"
 	"syscall"
+	"time"
+
+	"golang.org/x/sys/unix"
 )
 
 // GetOwnerMode returns the file mode, owner UID, and owner GID for the given file.
@@ -13,4 +16,17 @@ func GetOwnerMode(fInfo os.FileInfo) (os.FileMode, int, int) {
 	uid := int(fInfo.Sys().(*syscall.Stat_t).Uid)
 	gid := int(fInfo.Sys().(*syscall.Stat_t).Gid)
 	return mode, uid, gid
+}
+
+// GetUmask returns the current process umask.
+func GetUmask() os.FileMode {
+	mask := syscall.Umask(0)
+	syscall.Umask(mask)
+
+	return os.FileMode(mask)
+}
+
+// Lchtimes sets the access and modification times of a path without following symlinks.
+func Lchtimes(path string, atime time.Time, mtime time.Time) error {
+	return unix.Lutimes(path, []unix.Timeval{unix.NsecToTimeval(atime.UnixNano()), unix.NsecToTimeval(mtime.UnixNano())})
 }
