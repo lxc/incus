08ab8fce351a8097ae10b5f8f11fa953057e757c
Stéphane Graber <stgraber@stgraber.org>
2026-08-28 22:13:47 -0400

    internal/io: Add GetUmask and Lchtimes helpers
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/io/filesystem_windows.go
+++ internal/io/filesystem_windows.go
@@ -4,8 +4,19 @@ package io
 
 import (
 	"os"
+	"time"
 )
 
 func GetOwnerMode(fInfo os.FileInfo) (os.FileMode, int, int) {
 	return fInfo.Mode(), -1, -1
+}
+
+// GetUmask returns the current process umask.
+func GetUmask() os.FileMode {
+	return 0
+}
+
+// Lchtimes sets the access and modification times of a path.
+func Lchtimes(path string, atime time.Time, mtime time.Time) error {
+	return os.Chtimes(path, atime, mtime)
 }
