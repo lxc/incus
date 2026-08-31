2b4a09a6ec737c8031dc515a924dd58a52b8de79
0xk1f0 <dev@k1f0.dev>
2026-08-31 14:55:33 +0200

    api: Add network_peer_groups extension
    Signed-off-by: 0xk1f0 <dev@k1f0.dev>

--- internal/version/api.go
+++ internal/version/api.go
@@ -578,6 +578,7 @@ var APIExtensions = []string{
 	"gpu_physical_clique",
 	"authorization_scriptlet_claims",
 	"instance_project_move_live",
+	"network_peer_groups",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
