eebssk1/incus2

incusd: Use QuotaWriter for backup and ISO uploads
Incus should always respect project disk limits when they are in place
as those are a very useful safety net to prevent users from causing a
denial of service by filling up the server's disk.

This addresses CVE-2026-41685

Reported-by: https://7asecurity.com
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

cmd/incusd/instances_post.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gorilla/websocket"
 
 	internalInstance "github.com/lxc/incus/v6/internal/instance"
+	internalIO "github.com/lxc/incus/v6/internal/io"
 	"github.com/lxc/incus/v6/internal/server/backup"
 	"github.com/lxc/incus/v6/internal/server/cluster"
 	"github.com/lxc/incus/v6/internal/server/db"
@@ -780,8 +781,23 @@ func createFromBackup(s *state.State, r *http.Request, projectName string, data 
 	defer func() { _ = os.Remove(backupFile.Name()) }()
 	reverter.Add(func() { _ = backupFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded backup data into temporary file.
-	_, err = util.SafeCopy(backupFile, data)
+	_, err = util.SafeCopy(internalIO.NewQuotaWriter(backupFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}

cmd/incusd/storage_buckets.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/lxc/incus/v6/internal/filter"
+	internalIO "github.com/lxc/incus/v6/internal/io"
 	"github.com/lxc/incus/v6/internal/server/auth"
 	"github.com/lxc/incus/v6/internal/server/backup"
 	"github.com/lxc/incus/v6/internal/server/db"
@@ -1415,8 +1416,23 @@ func createStoragePoolBucketFromBackup(s *state.State, r *http.Request, requestP
 	defer func() { _ = os.Remove(backupFile.Name()) }()
 	reverter.Add(func() { _ = backupFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded backup data into temporary file.
-	_, err = util.SafeCopy(backupFile, data)
+	_, err = util.SafeCopy(internalIO.NewQuotaWriter(backupFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}

cmd/incusd/storage_volumes.go
@@ -2552,8 +2552,23 @@ func createStoragePoolVolumeFromISO(s *state.State, r *http.Request, requestProj
 	defer func() { _ = os.Remove(isoFile.Name()) }()
 	reverter.Add(func() { _ = isoFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded ISO data into temporary file.
-	size, err := util.SafeCopy(isoFile, data)
+	size, err := util.SafeCopy(internalIO.NewQuotaWriter(isoFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}
@@ -2605,8 +2620,23 @@ func createStoragePoolVolumeFromBackup(s *state.State, r *http.Request, requestP
 	defer func() { _ = os.Remove(backupFile.Name()) }()
 	reverter.Add(func() { _ = backupFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded backup data into temporary file.
-	_, err = util.SafeCopy(backupFile, data)
+	_, err = util.SafeCopy(internalIO.NewQuotaWriter(backupFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}

lxc/incus

cmd/incusd/instances_post.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gorilla/websocket"
 
 	internalInstance "github.com/lxc/incus/v6/internal/instance"
+	internalIO "github.com/lxc/incus/v6/internal/io"
 	"github.com/lxc/incus/v6/internal/server/backup"
 	"github.com/lxc/incus/v6/internal/server/cluster"
 	"github.com/lxc/incus/v6/internal/server/db"
@@ -659,8 +660,23 @@ func createFromBackup(s *state.State, r *http.Request, projectName string, data 
 	defer func() { _ = os.Remove(backupFile.Name()) }()
 	reverter.Add(func() { _ = backupFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded backup data into temporary file.
-	_, err = io.Copy(backupFile, data)
+	_, err = io.Copy(internalIO.NewQuotaWriter(backupFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}

cmd/incusd/storage_buckets.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/lxc/incus/v6/internal/filter"
+	internalIO "github.com/lxc/incus/v6/internal/io"
 	"github.com/lxc/incus/v6/internal/server/auth"
 	"github.com/lxc/incus/v6/internal/server/backup"
 	"github.com/lxc/incus/v6/internal/server/db"
@@ -1415,8 +1416,23 @@ func createStoragePoolBucketFromBackup(s *state.State, r *http.Request, requestP
 	defer func() { _ = os.Remove(backupFile.Name()) }()
 	reverter.Add(func() { _ = backupFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded backup data into temporary file.
-	_, err = io.Copy(backupFile, data)
+	_, err = io.Copy(internalIO.NewQuotaWriter(backupFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}

cmd/incusd/storage_volumes.go
@@ -2531,8 +2531,23 @@ func createStoragePoolVolumeFromISO(s *state.State, r *http.Request, requestProj
 	defer func() { _ = os.Remove(isoFile.Name()) }()
 	reverter.Add(func() { _ = isoFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded ISO data into temporary file.
-	size, err := io.Copy(isoFile, data)
+	size, err := io.Copy(internalIO.NewQuotaWriter(isoFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}
@@ -2584,8 +2599,23 @@ func createStoragePoolVolumeFromBackup(s *state.State, r *http.Request, requestP
 	defer func() { _ = os.Remove(backupFile.Name()) }()
 	reverter.Add(func() { _ = backupFile.Close() })
 
+	// Get disk budget for the project if any.
+	var budget int64
+
+	err = s.DB.Cluster.Transaction(r.Context(), func(ctx context.Context, tx *db.ClusterTx) error {
+		budget, err = project.GetSpaceBudget(tx, projectName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return response.InternalError(err)
+	}
+
 	// Stream uploaded backup data into temporary file.
-	_, err = io.Copy(backupFile, data)
+	_, err = io.Copy(internalIO.NewQuotaWriter(backupFile, budget), data)
 	if err != nil {
 		return response.InternalError(err)
 	}