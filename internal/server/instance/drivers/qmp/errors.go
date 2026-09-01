6b6ca5d5dfe40c563531da0d78e67d8c3b99c700
Stéphane Graber <stgraber@stgraber.org>
2026-09-01 16:00:44 -0400

    incusd/instance/qmp: Split query and operational command timeouts
    Queries keep failing fast to bound state rendering while operational
    commands warn past their expected duration and only fail after two
    minutes. The command lock is acquired within the command's deadline.
    
    Closes #3933
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/instance/drivers/qmp/errors.go
+++ internal/server/instance/drivers/qmp/errors.go
@@ -2,6 +2,7 @@ package qmp
 
 import (
 	"errors"
+	"fmt"
 )
 
 // ErrMonitorDisconnect is returned when interacting with a disconnected Monitor.
@@ -9,6 +10,9 @@ var ErrMonitorDisconnect = errors.New("Monitor is disconnected")
 
 // ErrMonitorTimeout is returned when a command doesn't get a reply in time (e.g. unresponsive QEMU).
 var ErrMonitorTimeout = errors.New("Monitor command timed out")
+
+// ErrMonitorBusy is returned when a command couldn't be sent in time because another one is in flight.
+var ErrMonitorBusy = fmt.Errorf("%w while monitor busy", ErrMonitorTimeout)
 
 // ErrMonitorBadConsole is returned when the requested console doesn't exist.
 var ErrMonitorBadConsole = errors.New("Requested console couldn't be found")
