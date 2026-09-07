6590fd559958a5717a3f3917cd5430e5731ef191
Stéphane Graber <stgraber@stgraber.org>
2026-09-07 16:11:41 -0400

    incusd/storage/zfs: Use generic property cache
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/server/storage/drivers/init.go
+++ internal/server/storage/drivers/init.go
@@ -5,9 +5,6 @@ import (
 )
 
 func init() {
-	zfsCache = map[string]map[string]zfsCacheEntry{}
-	zfsCachePrefillQueue = []string{}
-
 	truenasCache = map[string]map[string]map[string]truenasCacheEntry{}
 	truenasCachePrefillQueue = map[string][]string{}
 	truenasCachePrefillRunning = map[string]bool{}
