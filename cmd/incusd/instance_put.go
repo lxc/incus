f9e243195569d073f0a9095d5e88e36272c1cc5f
Stéphane Graber <stgraber@stgraber.org>
2026-09-14 09:35:15 -0400

    incusd/project: Allow snapshot restore with changed volatile keys
    Closes #3993
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- cmd/incusd/instance_put.go
+++ cmd/incusd/instance_put.go
@@ -243,14 +243,14 @@ func instanceSnapRestore(s *state.State, projectName string, name string, snap s
 
 	source.SetOperation(op)
 
-	// Ensure restoring the snapshot's config doesn't violate project restrictions.
+	// Ensure restoring the snapshot's config doesn't violate project restrictions, adjusting its volatile.* keys as needed.
 	profiles := make([]string, 0, len(source.Profiles()))
 	for _, profile := range source.Profiles() {
 		profiles = append(profiles, profile.Name)
 	}
 
 	err = s.DB.Cluster.Transaction(context.TODO(), func(ctx context.Context, tx *db.ClusterTx) error {
-		return projecthelpers.AllowInstanceUpdate(tx, projectName, name, api.InstancePut{
+		return projecthelpers.AllowSnapshotRestore(tx, projectName, name, api.InstancePut{
 			Config:   source.LocalConfig(),
 			Devices:  source.LocalDevices().CloneNative(),
 			Profiles: profiles,
