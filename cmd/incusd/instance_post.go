d8dbf349d648e08861bc9018aa16e259c9811535
Piotr Resztak <piotr.resztak@futurfusion.io>
2026-09-15 10:59:19 +0000

    incusd/instance: Reject project moves when an attached volume can't follow
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- cmd/incusd/instance_post.go
+++ cmd/incusd/instance_post.go
@@ -217,6 +217,13 @@ func instancePost(d *Daemon, r *http.Request) response.Response {
 		req.Migration = true
 	}
 
+	if req.Project != "" {
+		err = checkProjectMove(s, inst, req.Project)
+		if err != nil {
+			return response.BadRequest(err)
+		}
+	}
+
 	// Handle simple instance renaming.
 	if !req.Migration {
 		run := func(op *operations.Operation) error {
@@ -1246,6 +1253,34 @@ func cleanupDependentDisks(s *state.State, inst instance.Instance, deviceOverrid
 	})
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// checkProjectMove validates that an instance can be moved to the given project.
+func checkProjectMove(s *state.State, inst instance.Instance, targetProject string) error {
+	srcVolProject, err := project.StorageVolumeProject(s.DB.Cluster, inst.Project().Name, db.StoragePoolVolumeTypeCustom)
+	if err != nil {
+		return err
+	}
+
+	dstVolProject, err := project.StorageVolumeProject(s.DB.Cluster, targetProject, db.StoragePoolVolumeTypeCustom)
+	if err != nil {
+		return err
+	}
+
+	if srcVolProject == dstVolProject {
+		return nil
+	}
+
+	// Attached custom volumes are left behind when the volume project changes.
+	for _, dev := range inst.ExpandedDevices().Sorted() {
+		if dev.Config["type"] != "disk" || dev.Config["path"] == "/" || dev.Config["pool"] == "" || dev.Config["source"] == "" {
+			continue
+		}
+
+		return fmt.Errorf("Device %q uses a custom volume which isn't available in project %q", dev.Name, targetProject)
 	}
 
 	return nil
