22162a5df793b50687c5a84158cf238d3b46de6c
Stéphane Graber <stgraber@stgraber.org>
2026-09-23 00:39:11 -0400

    api: Add disk_initial_copy extension
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- internal/version/api.go
+++ internal/version/api.go
@@ -581,6 +581,7 @@ var APIExtensions = []string{
 	"metrics_cluster_members",
 	"instance_security_tags",
 	"network_ovn_parent",
+	"disk_initial_copy",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
